cd50a8efa51fffb5f479d0a7a4ce8bfceb0715d3
jacob-morrison <jacobm@allenai.org>
2023-09-07 17:27:36 -0700

    test script

---
+++ scripts/launch_lora_gantry_job.sh
@@ -0,0 +1,41 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+MODEL_SIZE=7B
+NUM_GPUS=4
+BATCH_SIZE_PER_GPU=1
+TOTAL_BATCH_SIZE=128
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+
+# Lora training
+gantry run --beaker-image 'hamishivi/open-instruct-peft' \
+--workspace ai2/jacobm-default-workspace --cluster ai2/allennlp-cirrascale --cluster ai2/general-cirrascale --cluster  \
+--pip requirements.txt --gpus 4 --priority high \
+-- accelerate launch \
+    --mixed_precision bf16 \
+    --num_machines 1 \
+    --num_processes $NUM_GPUS \
+    open_instruct/finetune.py \
+    --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/${MODEL_SIZE} \
+    --use_lora \
+    --use_flash_attn \
+    --lora_rank 256 \
+    --lora_alpha 256 \
+    --lora_dropout 0.05 \
+    --tokenizer_name /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/${MODEL_SIZE} \
+    --use_slow_tokenizer \
+    --train_file /net/nfs.cirrascale/allennlp/hamishi/open-instruct/tulu_data/tulu_v1_mix.jsonl \
+    --max_seq_length 2048 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+    --learning_rate 2e-4 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.03 \
+    --weight_decay 0. \
+    --num_train_epochs 3 \
+    --output_dir /results \
+    --save_merged_lora_model \
+    --with_tracking \
+    --report_to tensorboard \
+    --logging_steps 1 &&
