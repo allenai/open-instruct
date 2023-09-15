1afa4b6b5898f0b1e4c13b13085d654566f7016f
Hamish Ivison <hamishivi@gmail.com>
2023-09-15 01:25:28 +0000

    update script

--- scripts/finetune_qlora_with_accelerate.sh
+++ scripts/finetune_qlora_with_accelerate.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1
 
-MODEL_SIZE=13B
-NUM_GPUS=4
+MODEL_SIZE=7B
+NUM_GPUS=2
 BATCH_SIZE_PER_GPU=1
 TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
@@ -9,25 +9,25 @@ echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_G
 
 # Lora training
 accelerate launch \
-    --mixed_precision bf16 \
+    --multi_gpu \
     --num_machines 1 \
     --num_processes $NUM_GPUS \
     open_instruct/finetune.py \
-    --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/${MODEL_SIZE} \
+    --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama_models/${MODEL_SIZE} \
+    --use_qlora \
     --use_lora \
     --use_flash_attn \
-    --lora_rank 64 \
-    --lora_alpha 16 \
+    --lora_rank 256 \
+    --lora_alpha 256 \
     --lora_dropout 0.1 \
-    --use_qlora \
-    --tokenizer_name /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/${MODEL_SIZE} \
+    --tokenizer_name /net/nfs.cirrascale/allennlp/yizhongw/hf_llama_models/${MODEL_SIZE} \
     --use_slow_tokenizer \
     --train_file tulu_data/tulu_v1_mix.jsonl \
     --max_seq_length 2048 \
     --preprocessing_num_workers 16 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-    --learning_rate 2e-4 \
+    --learning_rate 1e-4 \
     --lr_scheduler_type linear \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
@@ -36,13 +36,9 @@ accelerate launch \
     --save_merged_lora_model \
     --with_tracking \
     --report_to tensorboard \
-    --logging_steps 1 &&
+    --logging_steps 1
 
 python open_instruct/merge_lora.py \
     --base_model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/${MODEL_SIZE} \
     --lora_model_name_or_path output/tulu_${MODEL_SIZE}_qlora_exp/ \
-    --qlora \
     --output_dir output/tulu_${MODEL_SIZE}_qlora_merged_exp/
-
-#    --use_deepspeed \
-#    --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
