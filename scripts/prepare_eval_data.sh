59238d4909c31594be2822c8512bcff85e966e86
yizhongw <eastonwyz@gmail.com>
2023-10-09 21:47:11 -0700

    Clean evaluation data preparation.

--- scripts/prepare_eval_data.sh
+++ scripts/prepare_eval_data.sh
@@ -15,49 +15,26 @@ unzip data/downloads/bbh_data.zip -d data/downloads/bbh
 mv data/downloads/bbh/BIG-Bench-Hard-main/ data/eval/bbh && rm -r data/downloads/bbh data/downloads/bbh_data.zip
 
 
-# Super-NaturalInstructions dataset
-wget -O data/downloads/superni_data.zip https://github.com/allenai/natural-instructions/archive/refs/heads/master.zip
-mkdir -p data/downloads/superni
-unzip data/downloads/superni_data.zip -d data/downloads/superni
-mv data/downloads/superni/natural-instructions-master/ data/eval/superni && rm -r data/downloads/superni data/downloads/superni_data.zip
-
-
 # TyDiQA-GoldP dataset
 mkdir -p data/eval/tydiqa
 wget -P data/eval/tydiqa/ https://storage.googleapis.com/tydiqa/v1.1/tydiqa-goldp-v1.1-dev.json
 wget -P data/eval/tydiqa/ https://storage.googleapis.com/tydiqa/v1.1/tydiqa-goldp-v1.1-train.json
 
 
-# XOR-QA dataset
-wget -P data/eval/xorqa/ https://raw.githubusercontent.com/mia-workshop/MIA-Shared-Task-2022/main/data/eval/mia_2022_dev_xorqa.jsonl
-wget -P data/eval/xorqa/ https://github.com/mia-workshop/MIA-Shared-Task-2022/raw/main/data/train/mia_2022_train_data.jsonl.zip
-unzip data/eval/xorqa/mia_2022_train_data.jsonl.zip -d data/eval/xorqa/ && rm data/eval/xorqa/mia_2022_train_data.jsonl.zip
-
-
 # GSM dataset
 wget -P data/eval/gsm/ https://github.com/openai/grade-school-math/raw/master/grade_school_math/data/test.jsonl
-
-
-# Multilingual GSM dataset
-wget -O data/downloads/url-nlp.zip https://github.com/google-research/url-nlp/archive/refs/heads/main.zip
-mkdir -p data/downloads/url-nlp
-unzip data/downloads/url-nlp.zip -d data/downloads/url-nlp
-mv data/downloads/url-nlp/url-nlp-main/mgsm data/eval/mgsm && rm -r data/downloads/url-nlp data/downloads/url-nlp.zip
 
 
 # Codex HumanEval
 wget -P data/eval/codex_humaneval https://github.com/openai/human-eval/raw/master/data/HumanEval.jsonl.gz
 
 
+# Alpaca Farm reference
+wget -P data/eval/alpaca_farm https://huggingface.co/datasets/hamishivi/alpaca-farm-davinci-003-2048-token/resolve/main/davinci_003_outputs.json
+
+
 # TruthfulQA
 wget -P data/eval/truthfulqa https://github.com/sylinrl/TruthfulQA/raw/main/TruthfulQA.csv
-
-
-# Self-instruct eval, Vicuna eval, and Koala eval for creative instructions/tasks
-mkdir -p data/eval/creative_tasks 
-wget -O data/eval/creative_tasks/self_instruct_test.jsonl https://github.com/yizhongw/self-instruct/raw/main/human_eval/user_oriented_instructions.jsonl
-wget -O data/eval/creative_tasks/vicuna_test.jsonl https://github.com/lm-sys/FastChat/raw/main/fastchat/eval/table/question.jsonl
-wget -O data/eval/creative_tasks/koala_test.jsonl https://github.com/arnav-gudibande/koala-test-set/raw/main/koala_test_set.jsonl
 
 
 # Toxigen data
@@ -66,3 +43,9 @@ for minority_group in asian black chinese jewish latino lgbtq mental_disability 
 do
     wget -O data/eval/toxigen/hate_${minority_group}.txt https://raw.githubusercontent.com/microsoft/TOXIGEN/main/prompts/hate_${minority_group}_1k.txt
 done
+
+
+# we use self-instruct test set, and vicuna test set for our human evaluation
+mkdir -p data/eval/creative_tasks 
+wget -O data/eval/creative_tasks/self_instruct_test.jsonl https://github.com/yizhongw/self-instruct/raw/main/human_eval/user_oriented_instructions.jsonl
+wget -O data/eval/creative_tasks/vicuna_test.jsonl https://github.com/lm-sys/FastChat/raw/main/fastchat/eval/table/question.jsonl
