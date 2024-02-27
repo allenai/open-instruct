be4d9df605a2c18c1f1ed231d5e56646e506956b
dwadden <davidw@allenai.org>
2024-02-27 05:28:21 +0000

    Run all tasks in a single experiment.
    Cuts down on the number of Beaker jobs to look through.

--- scripts/submit_eval_jobs.py
+++ scripts/submit_eval_jobs.py
@@ -56,32 +56,27 @@ experiment_groups = [
     "alpaca_eval",
 ]
 
-# format: model name, their beaker id, checkpoint subfolder, tuned or base.
-# or: name, path, None, tuned or base
-models = [
-    # llama1 models
-    # ("llama1-7B", "01HCCBK1MYKXKQC0C6CSVW1F22", None, "vanilla_lm"),
-
-    # other causal models
-    # ("hf-opt-7B", "facebook/opt-6.7b", None, "vanilla_lm"),
-    # ("finetuned_opt", "01H13EBXSADXXJCRERART90ZKJ", None, "tuned_lm"),
-    (args.model_name, args.location, args.beaker_subfolder, model_type),
-]
+model_info = (args.model_name, args.location, args.beaker_subfolder, model_type)
 
 #--------------- experiments about number of supervision tasks -------------------------
 
 # for experiment_group, model_info in itertools.product(experiment_groups, models):
-for model_info, experiment_group in itertools.product(models, experiment_groups):
-    print(f"Submitting {experiment_group} for model: {model_info[0]}")
-    d = copy.deepcopy(d1)
 
-    model_name = model_info[0] + f"_{model_info[2]}" if model_info[2] is not None else model_info[0]
+d = copy.deepcopy(d1)
+model_name = model_info[0] + f"_{model_info[2]}" if model_info[2] is not None else model_info[0]
+
+# Create tasks for each evaluation.
+eval_task_specs = []
+
+for experiment_group in experiment_groups:
+    print(f"Submitting {experiment_group} for model: {model_info[0]}")
+    task_spec = copy.deepcopy(d1["tasks"][0])
+
     name = f"open_instruct_eval_{experiment_group}_{model_name}_{today}"
-    d['description'] = name
-    d['tasks'][0]['name'] = name
+    task_spec['name'] = name
 
     if experiment_group == "mmlu_0shot":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.mmlu.run_eval \
             --ntrain 0 \
             --data_dir /data/mmlu/ \
@@ -94,7 +89,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "mmlu_5shot":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.mmlu.run_eval \
             --ntrain 5 \
             --data_dir /data/mmlu/ \
@@ -107,7 +102,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "bbh_direct":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.bbh.run_eval \
             --data_dir /data/bbh \
             --save_dir /output/ \
@@ -120,7 +115,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "bbh_cot":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.bbh.run_eval \
             --data_dir /data/bbh \
             --save_dir /output/ \
@@ -132,7 +127,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "gsm_direct":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.gsm.run_eval \
             --data_dir /data/gsm/ \
             --max_num_examples 200 \
@@ -146,7 +141,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "gsm_cot":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.gsm.run_eval \
             --data_dir /data/gsm/ \
             --max_num_examples 200 \
@@ -159,7 +154,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         ''' 
     elif experiment_group == "tydiqa_goldp_1shot":
-        d["tasks"][0]["arguments"][0] = '''
+        task_spec["arguments"][0] = '''
             python -m eval.tydiqa.run_eval \
             --data_dir /data/tydiqa/ \
             --n_shot 1 \
@@ -173,7 +168,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "tydiqa_no_context_1shot":
-        d["tasks"][0]["arguments"][0] = '''
+        task_spec["arguments"][0] = '''
             python -m eval.tydiqa.run_eval \
             --data_dir /data/tydiqa/ \
             --no_context \
@@ -188,7 +183,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "codex_eval_temp_0.1":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.codex_humaneval.run_eval \
             --data_file /data/codex_humaneval/HumanEval.jsonl.gz \
             --eval_pass_at_ks 1 5 10 20 \
@@ -200,7 +195,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --tokenizer_name_or_path /model
         '''
     elif experiment_group == "codex_eval_temp_0.8":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
             python -m eval.codex_humaneval.run_eval \
             --data_file /data/codex_humaneval/HumanEval.jsonl.gz \
             --eval_pass_at_ks 1 5 10 20 \
@@ -212,7 +207,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --tokenizer_name_or_path /model
         '''
     elif experiment_group == "trutufulqa":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
         python -m eval.truthfulqa.run_eval \
             --data_dir /data/truthfulqa \
             --save_dir /output/ \
@@ -228,7 +223,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "toxigen":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
         python -m eval.toxigen.run_eval \
             --data_dir /data/toxigen/ \
             --save_dir /output/ \
@@ -240,7 +235,7 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
             --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
         '''
     elif experiment_group == "alpaca_eval":
-        d['tasks'][0]['arguments'][0] = '''
+        task_spec['arguments'][0] = '''
         python -m eval.alpaca_farm.run_eval \
             --use_vllm \
             --model_name_or_path /model \
@@ -253,109 +248,117 @@ for model_info, experiment_group in itertools.product(models, experiment_groups)
         raise ValueError("experiment_group not supported")
 
     if model_info[0].startswith("hf-"):  # if it's a huggingface model, load it from the model hub
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--model_name_or_path /model", "--model_name_or_path "+model_info[1])]
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--tokenizer_name_or_path /model", "--model_name_or_path "+model_info[1])]
+        task_spec['arguments'] = [task_spec['arguments'][0].replace("--model_name_or_path /model", "--model_name_or_path "+model_info[1])]
+        task_spec['arguments'] = [task_spec['arguments'][0].replace("--tokenizer_name_or_path /model", "--model_name_or_path "+model_info[1])]
     elif model_info[1].startswith("/"):  # if it's a local model, load it from the local directory
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--model_name_or_path /model", "--model_name_or_path "+model_info[1])]
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--tokenizer_name_or_path /model", "--model_name_or_path "+model_info[1])]
+        task_spec['arguments'] = [task_spec['arguments'][0].replace("--model_name_or_path /model", "--model_name_or_path "+model_info[1])]
+        task_spec['arguments'] = [task_spec['arguments'][0].replace("--tokenizer_name_or_path /model", "--model_name_or_path "+model_info[1])]
     else:  # if it's a beaker model, mount the beaker dataset to `/model`
-        d['tasks'][0]['datasets'][1]['source']['beaker'] = model_info[1]
+        task_spec['datasets'][1]['source']['beaker'] = model_info[1]
 
     # if a specific checkpoint is specified, load model from that checkpoint
     if model_info[2] is not None:
         # extract existing model path
-        model_name_or_path = re.search("--model_name_or_path (\S+)", d['tasks'][0]['arguments'][0]).group(1)
+        model_name_or_path = re.search("--model_name_or_path (\S+)", task_spec['arguments'][0]).group(1)
         # replace the model path with the checkpoint subfolder. 
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(model_name_or_path, model_name_or_path+"/"+model_info[2], 1)]
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(model_name_or_path, model_name_or_path+"/"+model_info[2], 1)]
         # NOTE: We don't change the tokenizer subfolder, because by default the
         # tokenizer is only saved to the top-level output dir. That's why we call
         # `str.replace(..., 1)` above; this only replaces the first match.
 
     # for vanilla_lm, remove the chat formatting function
     if model_info[3] == "vanilla_lm":
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--use_chat_format", "")]
+        task_spec['arguments'] = [task_spec['arguments'][0].replace("--use_chat_format", "")]
 
     if "13B" in model_info[0]:
         # find the batch size argument, and reduce by 4x
-        if "--eval_batch_size" in d['tasks'][0]['arguments'][0]:
-            original_batch_size = re.search("--eval_batch_size (\d+)", d['tasks'][0]['arguments'][0]).group(1)
+        if "--eval_batch_size" in task_spec['arguments'][0]:
+            original_batch_size = re.search("--eval_batch_size (\d+)", task_spec['arguments'][0]).group(1)
             new_batch_size = max(1, int(original_batch_size) // 2)
-            d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--eval_batch_size {}".format(original_batch_size), "--eval_batch_size {}".format(new_batch_size))]
+            task_spec['arguments'] = [task_spec['arguments'][0].replace("--eval_batch_size {}".format(original_batch_size), "--eval_batch_size {}".format(new_batch_size))]
 
 
     if "30B" in model_info[0] or "34B" in model_info[0]:
         # find the batch size argument, and reduce by 4x
-        if "--eval_batch_size" in d['tasks'][0]['arguments'][0]:
-            original_batch_size = re.search("--eval_batch_size (\d+)", d['tasks'][0]['arguments'][0]).group(1)
+        if "--eval_batch_size" in task_spec['arguments'][0]:
+            original_batch_size = re.search("--eval_batch_size (\d+)", task_spec['arguments'][0]).group(1)
             new_batch_size = max(1, int(original_batch_size) // 4)
-            d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--eval_batch_size {}".format(original_batch_size), "--eval_batch_size {}".format(new_batch_size))]
+            task_spec['arguments'] = [task_spec['arguments'][0].replace("--eval_batch_size {}".format(original_batch_size), "--eval_batch_size {}".format(new_batch_size))]
 
         if "codex_eval" in experiment_group:
             # request 2x more GPUs
-            d['tasks'][0]['resources']['gpuCount'] = 2 * d['tasks'][0]['resources']['gpuCount']
+            task_spec['resources']['gpuCount'] = 2 * task_spec['resources']['gpuCount']
     
     elif "70B" in model_info[0] or "65B" in model_info[0] or "40B" in model_info[0]:
         # find the batch size argument, and reduce by 4x
-        if "--eval_batch_size" in d['tasks'][0]['arguments'][0]:
-            original_batch_size = re.search("--eval_batch_size (\d+)", d['tasks'][0]['arguments'][0]).group(1)
+        if "--eval_batch_size" in task_spec['arguments'][0]:
+            original_batch_size = re.search("--eval_batch_size (\d+)", task_spec['arguments'][0]).group(1)
             new_batch_size = max(1, int(original_batch_size) // 4)
-            d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--eval_batch_size {}".format(original_batch_size), "--eval_batch_size {}".format(new_batch_size))]
+            task_spec['arguments'] = [task_spec['arguments'][0].replace("--eval_batch_size {}".format(original_batch_size), "--eval_batch_size {}".format(new_batch_size))]
 
         if "codex_eval" in experiment_group:
             # request 4x more GPUs
-            d['tasks'][0]['resources']['gpuCount'] = 4 * d['tasks'][0]['resources']['gpuCount']
+            task_spec['resources']['gpuCount'] = 4 * task_spec['resources']['gpuCount']
         else:
             # request 2x more GPUs
-            d['tasks'][0]['resources']['gpuCount'] = 2 * d['tasks'][0]['resources']['gpuCount']
+            task_spec['resources']['gpuCount'] = 2 * task_spec['resources']['gpuCount']
 
     # if using huggingface tokenizer template, replace the chat formatting function with hf tokenizer one
     if args.use_hf_tokenizer_template:
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(
             "--chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format", 
             "--chat_formatting_function eval.templates.create_prompt_with_huggingface_tokenizer_template")
         ]
     if "llama2-chat" in model_info[0]:
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(
             "--chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format", 
             "--chat_formatting_function eval.templates.create_prompt_with_llama2_chat_format")
         ]
     elif "code_llama_instruct" in model_info[0]:
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(
             "--chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format", 
             "--chat_formatting_function eval.templates.create_prompt_with_llama2_chat_format")
         ]
     elif "zephyr" in model_info[0]:
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(
             "--chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format", 
             "--chat_formatting_function eval.templates.create_prompt_with_zephyr_chat_format")
         ]
     elif "xwin" in model_info[0]:
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(
             "--chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format", 
             "--chat_formatting_function eval.templates.create_prompt_with_xwin_chat_format")
         ]
     elif "olmo" in model_info[0]:
-        d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace(
+        task_spec['arguments'] = [task_spec['arguments'][0].replace(
             "--chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format", 
             "--chat_formatting_function eval.templates.create_prompt_with_olmo_chat_format")
         ]
         # no vllm for olmo yet
-        if "--use_vllm" in d['tasks'][0]['arguments'][0]:
+        if "--use_vllm" in task_spec['arguments'][0]:
             print(f"Removing --use_vllm for {model_info[0]}")
-            d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--use_vllm", "")] 
+            task_spec['arguments'] = [task_spec['arguments'][0].replace("--use_vllm", "")] 
 
 
     if any([x in model_info[0] for x in ["opt", "pythia", "falcon"]]):
-        if "--use_vllm" in d['tasks'][0]['arguments'][0]:
+        if "--use_vllm" in task_spec['arguments'][0]:
             print(f"Removing --use_vllm for {model_info[0]}")
-            d['tasks'][0]['arguments'] = [d['tasks'][0]['arguments'][0].replace("--use_vllm", "")] 
+            task_spec['arguments'] = [task_spec['arguments'][0].replace("--use_vllm", "")] 
 
     # print(d)
 
-    fn = "beaker_configs/auto_created/{}.yaml".format(name)
-    file = open(fn, "w")
-    yaml.dump(d, file, default_flow_style=True)
-    file.close()
+    eval_task_specs.append(task_spec)
 
-    cmd = "beaker experiment create {} --workspace ai2/{}".format(fn, workspace)
-    subprocess.Popen(cmd, shell=True)
+
+# Create an experiment that runs all the eval tasks.
+
+experiment_name = f"open_instruct_eval_{model_name}_{today}" 
+d["description"] = experiment_name
+d["tasks"] = eval_task_specs
+fn = "beaker_configs/auto_created/{}.yaml".format(experiment_name)
+file = open(fn, "w")
+yaml.dump(d, file, default_flow_style=True)
+file.close()
+
+cmd = "beaker experiment create {} --workspace ai2/{}".format(fn, workspace)
+subprocess.Popen(cmd, shell=True)
