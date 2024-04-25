7fc1154d74a41243a587cf8ea38f08420a691ef6
Nathan Lambert <nathanl@allenai.org>
2024-04-25 22:10:49 +0000

    up

---
+++ scripts/finetune_with_accelerate_olmo.sh
@@ -0,0 +1,34 @@
+MODEL_SIZE=7B
+NUM_GPUS=8
+BATCH_SIZE_PER_GPU=1
+TOTAL_BATCH_SIZE=128
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+echo "Training OLMo model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+
+# You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory, 
+# but it will trade off speed.
+accelerate launch \
+    --mixed_precision bf16 \
+    --num_machines 1 \
+    --num_processes $NUM_GPUS \
+    --use_deepspeed \
+    --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+    open_instruct/finetune.py \
+    --model_name_or_path allenai/OLMo-1.7-7B \
+    --use_flash_attn \
+    --tokenizer_name allenai/OLMo-1.7-7B \
+    --dataset_name allenai/tulu-v2-sft-mixture \
+    --max_seq_length 4096 \
+    --preprocessing_num_workers 128 \
+    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+    --learning_rate 2e-6 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.03 \
+    --weight_decay 0. \
+    --num_train_epochs 3 \
+    --output_dir output/olmo_instruct/ \
+    --with_tracking \
+    --report_to wandb \
+    --logging_steps 1 \
+    --checkpointing_steps epoch
