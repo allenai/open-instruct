ef9438ada8d4f6412ff2b4d934f8fbe027a15208
jacob-morrison <jacobm@allenai.org>
2024-04-26 13:48:53 -0700

    .

--- scripts/submit_tulu_evals-science.sh
+++ scripts/submit_tulu_evals-science.sh
@@ -13,7 +13,41 @@ model_list=(
     # "llama_2_7b-tulu_266025-science_114010"
     # "llama_2_7b-tulu_304028-science_76007"
     # "llama_2_7b-tulu_380035-science_0"
-    "llama_2_7b-tulu_76007-science_304028"
+    # "llama_2_7b-tulu_76007-science_304028"
+
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_1.0"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.9"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.8"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.7"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.6"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.5"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.4"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.3"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.2"
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.1"
+
+    # "task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.19"
+    # "linear_weighted-llama_2_7b-tulu_all_0.84-science_2500_0.16"
+    # "linear_weighted-llama_2_7b-tulu_all_0.9-tulu_2_7b_science_2500_0.1"
+    # "linear_weighted-llama_2_7b-tulu_all_0.8-tulu_2_7b_science_2500_0.2"
+    # "linear_weighted-llama_2_7b-tulu_all_0.7-tulu_2_7b_science_2500_0.3"
+    # "linear_weighted-llama_2_7b-tulu_all_0.6-tulu_2_7b_science_2500_0.4"
+    # "linear_weighted-llama_2_7b-tulu_all_0.5-tulu_2_7b_science_2500_0.5"
+    # "linear_weighted-llama_2_7b-tulu_all_0.4-tulu_2_7b_science_2500_0.6"
+    # "linear_weighted-llama_2_7b-tulu_all_0.3-tulu_2_7b_science_2500_0.7"
+    # "linear_weighted-llama_2_7b-tulu_all_0.2-tulu_2_7b_science_2500_0.8"
+    # "linear_weighted-llama_2_7b-tulu_all_0.1-tulu_2_7b_science_2500_0.9"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.1"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.19"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.2"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.3"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.4"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.5"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.6"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.7"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.8"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_0.9"
+    "dare_task_arithmetic-llama_2_7b-tulu_all_1.0-science_2500_1.0"
 )
 
 # for tuple in "${model_list[@]}"
@@ -24,7 +58,7 @@ python scripts/submit_eval_jobs.py \
     --workspace modular-adaptation-science \
     --model_name ${MODEL} \
     --location jacobm \
-    --priority low \
+    --priority high \
     --is_tuned \
     --output_dir /net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/results/domain_addition/science/tulu_evals/${MODEL}/
 done
