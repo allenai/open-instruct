6daa436ef896619879e5dcabb42614a6253b4e43
jacob-morrison <jacobm@allenai.org>
2024-06-24 22:25:41 -0700

    .

---
+++ data_prep/format_codefeedback_single_turn.py
@@ -0,0 +1,16 @@
+import json
+import random
+
+data = []
+with open("/oe-adapt-default/jacobm/tulu-3-dev/data/tulu_v2_mix.jsonl") as f_in:
+    for line in f_in.readlines():
+        data.append(json.loads(line))
+
+with open("/oe-adapt-default/jacobm/tulu-3-dev/data/codefeedback-single-turn.jsonl") as f_in:
+    for line in f_in.readlines():
+        data.append(json.loads(line))
+
+random.shuffle(data)
+with open("/oe-adapt-default/jacobm/tulu-3-dev/data/tulu-and-codefeedback-single-turn.jsonl", "w") as f_out:
+    for elem in data:
+        print(json.dumps(elem), file=f_out)
