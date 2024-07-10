a7127d36fbb64d99d73192c1e738123a01f7d243
jacob-morrison <jacobm@allenai.org>
2024-07-10 11:21:36 -0700

    .

--- data_prep/format_math_coding_and_tulu.py
+++ data_prep/format_math_coding_and_tulu.py
@@ -10,11 +10,11 @@ with open("/oe-adapt-default/jacobm/tulu-3-dev/data/codefeedback-single-turn.jso
     for line in f_in.readlines():
         data.append(json.loads(line))
 
-with open("/oe-adapt-default/jacobm/tulu-3-dev/data/mathplus-200k.jsonl") as f_in:
+with open("/oe-adapt-default/jacobm/tulu-3-dev/data/mathplus-500k.jsonl") as f_in:
     for line in f_in.readlines():
         data.append(json.loads(line))
 
 random.shuffle(data)
-with open("/oe-adapt-default/jacobm/tulu-3-dev/data/tulu-and-mathplus_200k-and-codefeedback-single-turn.jsonl", "w") as f_out:
+with open("/oe-adapt-default/jacobm/tulu-3-dev/data/tulu-and-mathplus_500k-and-codefeedback-single-turn.jsonl", "w") as f_out:
     for elem in data:
         print(json.dumps(elem), file=f_out)
