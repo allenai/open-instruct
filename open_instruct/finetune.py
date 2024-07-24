f1cc48b4da79103fb9b26ec745666c0083a9976a
jacob-morrison <jacobm@allenai.org>
2024-07-24 16:32:32 -0700

    set in config

--- open_instruct/finetune.py
+++ open_instruct/finetune.py
@@ -249,6 +249,7 @@ def main():
             args.config_name,
             trust_remote_code=args.trust_remote_code,
             revision=args.model_revision,
+            output_router_logits=True,
             token=os.getenv("HF_TOKEN", None),
         )
     elif args.model_name_or_path:
@@ -256,6 +257,7 @@ def main():
             args.model_name_or_path,
             trust_remote_code=args.trust_remote_code,
             revision=args.model_revision,
+            output_router_logits=True,
             token=os.getenv("HF_TOKEN", None),
         )
     else:
