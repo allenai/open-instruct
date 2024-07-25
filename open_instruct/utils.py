d57be9ec5766dee57f3f2566578a036217932ea2
Nathan Lambert <nathanl@allenai.org>
2024-07-25 18:31:17 +0000

    remove pdb

--- open_instruct/utils.py
+++ open_instruct/utils.py
@@ -334,7 +334,6 @@ class ArgumentParserPlus(HfArgumentParser):
         return outputs
 
     def parse(self) -> DataClassType | Tuple[DataClassType]:
-        import pdb; pdb.set_trace()
         if len(sys.argv) == 2 and sys.argv[1].endswith(".yaml"):
             # If we pass only one argument to the script and it's the path to a YAML file,
             # let's parse it to get our arguments.
