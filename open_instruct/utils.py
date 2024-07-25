a715bba04b162ff06f9157619cbcf8aca074b341
Nathan Lambert <nathanl@allenai.org>
2024-07-25 18:30:53 +0000

    improvements

--- open_instruct/utils.py
+++ open_instruct/utils.py
@@ -334,6 +334,7 @@ class ArgumentParserPlus(HfArgumentParser):
         return outputs
 
     def parse(self) -> DataClassType | Tuple[DataClassType]:
+        import pdb; pdb.set_trace()
         if len(sys.argv) == 2 and sys.argv[1].endswith(".yaml"):
             # If we pass only one argument to the script and it's the path to a YAML file,
             # let's parse it to get our arguments.
