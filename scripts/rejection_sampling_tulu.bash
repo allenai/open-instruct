444517eba769712710e02508fe3a546382e3a636
Costa Huang <costa.huang@outlook.com>
2024-08-12 14:01:49 +0000

    push changes

--- scripts/rejection_sampling_tulu.bash
+++ scripts/rejection_sampling_tulu.bash
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 mkdir -p output/shards
-num_prompts=400
-num_shards=5
+num_prompts=326154
+num_shards=40
 prompts_per_shard=$((num_prompts / num_shards))
 shared_hf_repo_id=rejection_sampling_$RANDOM 
 num_generations=5
@@ -8,8 +10,10 @@ generation_model=allenai/llama-3-tulu-2-8b
 reward_model=allenai/llama-3-tulu-2-8b-uf-mean-rm
 sft_dataset=allenai/tulu-v2-sft-mixture
 num_gpus=1
-clusters=ai2/general-cirrascale-a5000 # ai2/allennlp-cirrascale ai2/general-cirrascale-a100-80g-ib
 mkdir -p output/shards/$shared_hf_repo_id
+
+# Prepare the command string
+command=""
 
 # Loop through shards
 for ((i=0; i<num_shards; i++))
@@ -20,32 +24,45 @@ do
     
     # Adjust the end index for the last shard to include any remaining prompts
     if [ $i -eq $((num_shards - 1)) ]; then
-        end_idx=$((num_prompts))
+        end_idx=$num_prompts
     fi
     
-    # Run the command for this shard
-    echo "Running shard $((i+1)) of $num_shards (indices $start_idx to $end_idx)"
-    python mason.py \
-        --cluster $clusters  \
-        --budget ai2/allennlp \
-        --priority low \
-        --gpus $num_gpus -- "python open_instruct/generation.py \
-        --dataset_name $sft_dataset \
-        --model_name_or_path $generation_model \
-        --dataset_start_idx $start_idx \
-        --dataset_end_idx $end_idx \
-        --save_filename output/shards/$shared_hf_repo_id/$i.jsonl \
-        --n $num_generations --tensor_parallel_size $num_gpus && python open_instruct/rejection_sampling.py \
-        --input_filename output/shards/$shared_hf_repo_id/$i.jsonl \
-        --model_names_or_paths $reward_model \
-        --save_filename output/shards/$shared_hf_repo_id/scores_$i.jsonl \
-        --hf_repo_id $shared_hf_repo_id \
-        --no_add_timestamp \
-        --n $num_generations \
-        --push_to_hub \
-        --num_gpus $num_gpus"
+    # Build the command string for this shard
+    shard_command="python open_instruct/generation.py \
+    --dataset_name $sft_dataset \
+    --model_name_or_path $generation_model \
+    --dataset_start_idx $start_idx \
+    --dataset_end_idx $end_idx \
+    --save_filename output/shards/$shared_hf_repo_id/$i.jsonl \
+    --n $num_generations --tensor_parallel_size $num_gpus && \
+    python open_instruct/rejection_sampling.py \
+    --input_filename output/shards/$shared_hf_repo_id/$i.jsonl \
+    --model_names_or_paths $reward_model \
+    --save_filename output/shards/$shared_hf_repo_id/scores_$i.jsonl \
+    --hf_repo_id $shared_hf_repo_id \
+    --no_add_timestamp \
+    --n $num_generations \
+    --push_to_hub \
+    --num_gpus $num_gpus && \
+    echo Finished shard $((i+1)) of $num_shards"
 
-    echo "Finished shard $((i+1)) of $num_shards"
-    echo
+    # Add the shard command to the main command string
+    if [ -z "$command" ]; then
+        command="$shard_command"
+    else
+        command="$command -- $shard_command"
+    fi
 done
+
+echo $command
+
+# Run the combined command
+echo "Submitting all shards in one command"
+python mason.py \
+    --cluster ai2/general-cirrascale-a5000 ai2/allennlp-cirrascale ai2/general-cirrascale-a100-80g-ib \
+    --budget ai2/allennlp \
+    --priority low \
+    --preemptible \
+    --gpus $num_gpus -- $command
+
 echo "All shards submitted"
