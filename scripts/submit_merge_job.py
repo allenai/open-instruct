f516abcc63cea420a422b2cbbf0b28cf428ff43a
jacob-morrison <jacobm@allenai.org>
2024-08-18 18:28:35 -0700

    .

--- scripts/submit_merge_job.py
+++ scripts/submit_merge_job.py
@@ -65,6 +65,9 @@ def main():
     beakerConfig = yaml.load(beaker_yaml, Loader=yaml.FullLoader)
 
     beakerConfig['tasks'][0]['image']['beaker'] = args.beaker_image
+    beakerConfig['tasks'][0]['context']['cluster'] = args.cluster
+    beakerConfig['tasks'][0]['context']['priority'] = args.priority
+    beakerConfig['tasks'][0]['context']['preemptible'] = args.preemptible # True requried for Jupiter/Pluto
     
     if len(beakerDatasets) > 0:
         beakerConfig["tasks"][0]["datasets"] = beakerDatasets
