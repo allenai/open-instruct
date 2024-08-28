ade34d0a486fce513139c5e2c49a716e68d2bec5
Costa Huang <costa.huang@outlook.com>
2024-08-28 12:03:17 -0400

    only upload the hf_metadata with beaker job (#305)

--- open_instruct/finetune.py
+++ open_instruct/finetune.py
@@ -1017,7 +1017,7 @@ def main(args: FlatArguments):
                 location=args.hf_repo_id,
                 hf_repo_revision=args.hf_repo_revision,
             )
-    if args.hf_metadata_dataset:
+    if args.hf_metadata_dataset and accelerator.is_main_process and is_beaker_job():
         # dpo script only supports these two options right now for datasets
         if args.dataset_mixer:
             dataset_list = args.dataset_mixer.keys()
@@ -1027,7 +1027,6 @@ def main(args: FlatArguments):
             dataset_list = [args.dataset_name]
         else:
             dataset_list = [args.train_file]
-        beaker_config = maybe_get_beaker_config()
         # mainly just focussing here on what would be useful for the leaderboard.
         # wandb will have even more useful information.
         metadata_blob = {
