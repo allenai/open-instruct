b7302bd7944c92b7d30442462a924bbfe92ee51d
nouhadziri <dziri@ualberta.ca>
2024-08-27 21:43:50 -0400

    fic key dict

--- eval/zero_scrolls/run_eval.py
+++ eval/zero_scrolls/run_eval.py
@@ -137,7 +137,7 @@ def main(args):
         nb_examples_more_seq_length = 0
         nb_examples_less_seq_length = 0
         for i, example in tqdm(enumerate(data["validation"]), desc="Reading data"):
-            task_name = example["dataset"]
+            task_name = example[dataset]
             if 0 < max_examples_per_task == i:
                 print(f"Reached {max_examples_per_task} for {dataset}. Breaking")
                 break
