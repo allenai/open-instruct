10ba18f3ae41fb64cd475c394e23e1803987122a
Niklas Muennighoff <n.muennighoff@gmail.com>
2024-08-27 22:10:57 -0700

    Update dpo_utils.py

--- open_instruct/dpo_utils.py
+++ open_instruct/dpo_utils.py
@@ -262,17 +262,19 @@ def concatenated_forward(
     We do this to avoid doing two forward passes, because it's faster for FSDP.
     """
     concatenated_batch = concatenated_inputs(batch)
-    all_logits = model(
+    outputs = model(
         input_ids=concatenated_batch["concatenated_input_ids"],
         attention_mask=concatenated_batch["concatenated_attention_mask"],
         output_router_logits=True,
-    ).logits.to(torch.float32)
+    )
+    logits = outputs.logits.to(torch.float32)
+    aux_loss = outputs.aux_loss
     all_logps = _get_batch_logps(
         all_logits, concatenated_batch["concatenated_labels"], average_log_prob=average_log_prob
     )
     chosen_logps = all_logps[: batch["chosen_input_ids"].shape[0]]
     rejected_logps = all_logps[batch["chosen_input_ids"].shape[0] :]
-    return chosen_logps, rejected_logps
+    return chosen_logps, rejected_logps, aux_loss
 
 
 def pad_to_length(tensor: torch.Tensor, length: int, pad_value: Union[int, float], dim: int = -1) -> torch.Tensor:
