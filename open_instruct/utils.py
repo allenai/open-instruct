5250a1ee0917979daf5de356979709fc5175a3af
Costa Huang <costa.huang@outlook.com>
2024-08-28 00:30:38 +0000

    make sure to actually remove checkpoints if keep_last_n_checkpoints=0

--- open_instruct/utils.py
+++ open_instruct/utils.py
@@ -565,13 +565,12 @@ def is_checkpoint_folder(dir: str, folder: str) -> bool:
 
 def clean_last_n_checkpoints(output_dir: str, keep_last_n_checkpoints: int) -> None:
     # remove the last checkpoint to save space
-    if keep_last_n_checkpoints > 0:
-        folders = [f for f in os.listdir(output_dir) if is_checkpoint_folder(output_dir, f)]
-        # find the checkpoint with the largest step
-        checkpoints = sorted(folders, key=lambda x: int(x.split("_")[-1]))
-        if len(checkpoints) > keep_last_n_checkpoints:
-            for checkpoint in checkpoints[:len(checkpoints) - keep_last_n_checkpoints]:
-                shutil.rmtree(os.path.join(output_dir, checkpoint))
+    folders = [f for f in os.listdir(output_dir) if is_checkpoint_folder(output_dir, f)]
+    # find the checkpoint with the largest step
+    checkpoints = sorted(folders, key=lambda x: int(x.split("_")[-1]))
+    if len(checkpoints) > keep_last_n_checkpoints:
+        for checkpoint in checkpoints[:len(checkpoints) - keep_last_n_checkpoints]:
+            shutil.rmtree(os.path.join(output_dir, checkpoint))
 
 
 # ----------------------------------------------------------------------------
