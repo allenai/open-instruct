93ac8c079446bb05504414ea70a93f3e362a376d
jacob-morrison <jacobm@allenai.org>
2024-09-04 18:16:17 -0700

    .

--- scripts/rejection_sampling_tulu_docker.bash
+++ scripts/rejection_sampling_tulu_docker.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # my variables
-reward_model_path=L3.18B-RM
+reward_model_path=L3.18B-base_rs_L3.18BI-static-valpy_dpo-RM
 generation_model_path=L3.18B-base_rs_L3.18BI-static-valpy_dpo
-num_completions=8
-priority=high
+num_completions=1
+priority=normal
 
 mkdir -p output/shards
 num_prompts=296461
