cf22a2295b9ff432bd7f18fa026e77f3b9219b0a
Costa Huang <costa.huang@outlook.com>
2024-09-10 13:32:19 -0400

    Allow custom leaderboard name (#342)
    * quick change
    
    * quick fix
    
    * Allow custom leaderboard name

--- scripts/add_metadata_from_wandb.py
+++ scripts/add_metadata_from_wandb.py
@@ -12,11 +12,15 @@ api = wandb.Api()
 python scripts/add_metadata_from_wandb.py --wandb_run_id ai2-llm/open_instruct_internal/runs/fjclmg47
 # or from a hf_revision (the name of the autoeval)
 python scripts/add_metadata_from_wandb.py --hf_repo_revision valpy_dpo_mix_uf_wc_regen_da_sftmix_v4.23___model__42__1725581304
+
+# from a wandb url to a particular name
+python scripts/add_metadata_from_wandb.py --wandb_run_id ai2-llm/open_instruct_internal/runs/87iqgc1t --leaderboard_model_name dpo_tune_faezeb_mix_uf_wc_da_math_numina_25k_sftmix_v4.23
 """
 @dataclass
 class Args:
     wandb_run_id: Optional[str] = None
     hf_repo_revision: Optional[str] = None
+    leaderboard_model_name: Optional[str] = None
 
 new_args = HfArgumentParser(Args).parse_args_into_dataclasses()[0]
 
@@ -55,10 +59,13 @@ metadata_blob = {
 
 # upload metadata to the dataset if set
 hf_metadata_dataset = "allenai/tulu-3-evals"
+leaderboard_model_name = args["hf_repo_revision"]
+if new_args.leaderboard_model_name is not None:
+    leaderboard_model_name = new_args.leaderboard_model_name
 if hf_metadata_dataset:
     upload_metadata_to_hf(
         metadata_blob,
         "metadata.json",
         hf_metadata_dataset,
-        "results/" + args["hf_repo_revision"],  # to match what the auto-evals name as.
+        "results/" + leaderboard_model_name,  # to match what the auto-evals name as.
     )
