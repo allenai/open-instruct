968938e21aa99ea1a0d9b4e83c27585214665549
jacob-morrison <jacobm@allenai.org>
2024-09-13 20:45:55 -0700

    quick and dirty hack to sometimes choose reference

--- open_instruct/rejection_sampling/rejection_sampling.py
+++ open_instruct/rejection_sampling/rejection_sampling.py
@@ -350,6 +350,11 @@ def main(args: Args):
         assert worst_completions[i]["messages"][:-1] == best_completions[i]["messages"][:-1]
         table["chosen_score"].append(best_completions[i]["score"])
         table["rejected_score"].append(worst_completions[i]["score"])
+        rm_key = table["reference_completion_score"][-1].keys()[0]
+        if table["chosen_score"][-1] < table["reference_completion_score"][rm_key]:
+            print("reference was best")
+            table["chosen_score"][-1] = table["reference_completion_score"][rm_key]
+            table["chosen"][-1][-1] = table["reference_completion"][-1]
     save_jsonl(args.save_filename, table)
 
     table_scores = defaultdict(list)
