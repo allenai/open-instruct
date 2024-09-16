abd47557827bafbfc18509297c80e495dcfa5a95
Costa Huang <costa.huang@outlook.com>
2024-09-16 15:56:09 +0000

    add a quick comment

--- open_instruct/dpo_tune.py
+++ open_instruct/dpo_tune.py
@@ -1045,6 +1045,7 @@ def main(args: FlatArguments):
                 progress_bar.update(1)
                 completed_steps += 1
                 if args.logging_steps and completed_steps % (args.gradient_accumulation_steps * args.logging_steps) == 0:
+                    # single all reduce to save time, avoiding per metric all reduce
                     global_metrics = accelerator.reduce(local_metrics, reduction="mean")
                     global_metrics /= args.gradient_accumulation_steps * args.logging_steps
                     global_metrics = global_metrics.tolist()
