86c947a378cc1b9fd1748e87ba0f5ffeec984cb5
nouhadziri <dziri@ualberta.ca>
2024-09-24 16:35:18 -0700

    add safety RM data

---
+++ open_instruct/safety/process_beavertail.py
@@ -0,0 +1,34 @@
+
+
+import json
+from datasets import load_dataset
+from tqdm import tqdm
+
+
+def process_pku_saferlhf(output_file):
+
+    dataset = load_dataset("PKU-Alignment/PKU-SafeRLHF")
+
+    with open(output_file, "w") as outfile:
+        for split in dataset.keys():
+            for item in tqdm(dataset[split], desc=f"Processing {split}"):
+                if item["safer_response_id"]  == 0:
+                    chosen = item["response_0"]
+                    rejected = item["response_1"]
+                else:
+                    chosen = item["response_1"]
+                    rejected = item["response_0"]
+
+            preference_data = {
+                "chosen": chosen,
+                "rejected": rejected
+            }
+            json.dump(preference_data, outfile)
+            outfile.write("\n")
+
+    print("Processing done!")
+
+
+if __name__ == "__main__":
+    output_file = ""
+    process_pku_saferlhf(output_file)
