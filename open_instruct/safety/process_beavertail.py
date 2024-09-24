22b2aced11c558bff50c335a3ad38fec5a782b0d
nouhadziri <dziri@ualberta.ca>
2024-09-24 16:43:29 -0700

    fix data format

--- open_instruct/safety/process_beavertail.py
+++ open_instruct/safety/process_beavertail.py
@@ -1,5 +1,3 @@
-
-
 import json
 from datasets import load_dataset
 from tqdm import tqdm
@@ -12,22 +10,25 @@ def process_pku_saferlhf(output_file):
     with open(output_file, "w") as outfile:
         for split in dataset.keys():
             for item in tqdm(dataset[split], desc=f"Processing {split}"):
-                if item["safer_response_id"]  == 0:
+                if item["safer_response_id"] == 0:
+                    breakpoint()
                     chosen = item["response_0"]
                     rejected = item["response_1"]
                 else:
                     chosen = item["response_1"]
                     rejected = item["response_0"]
 
-            preference_data = {
-                "chosen": chosen,
-                "rejected": rejected
-            }
-            json.dump(preference_data, outfile)
-            outfile.write("\n")
+                prompt = item["prompt"]
+
+                preference_data = {
+                    "chosen": [{"role": "user", "content": prompt}, {"role": "assistant", "content": chosen}],
+                    "rejected": [{"role": "user", "content": prompt}, {"role": "assistant", "content": rejected}],
+                }
+                json.dump(preference_data, outfile)
+                outfile.write("\n")
 
     print("Processing done!")
-
+    print(f"Data save in {output_file}")
 
 if __name__ == "__main__":
     output_file = "/net/nfs.cirrascale/mosaic/nouhad/projects/open-instruct/open_instruct/safety/pku_saferlhf_preferences.jsonl"
