2383b3c62bf3fafb0504c8c6ba34bb66ea745e8c
nouhadziri <dziri@ualberta.ca>
2024-10-02 17:42:39 -0400

    add preference safety data to HF

--- open_instruct/safety/process_beavertail.py
+++ open_instruct/safety/process_beavertail.py
@@ -1,12 +1,27 @@
 import json
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
+from typing import List, Dict, Optional
 from tqdm import tqdm
 
+
+def upload_to_huggingface(data: List[dict], dataset_name: str):
+    dataset = Dataset.from_dict(
+        {
+            "chosen": [item["chosen"] for item in data],
+            "rejected": [item["rejected"] for item in data],
+            "source": [item["source"] for item in data],
+
+        }
+    )
+
+    dataset.push_to_hub(dataset_name)
+    print(f"Dataset uploaded to Hugging Face: https://huggingface.co/datasets/{dataset_name}")
 
 def process_pku_saferlhf(output_file):
     dataset = load_dataset("PKU-Alignment/PKU-SafeRLHF")
 
     with open(output_file, "w") as outfile:
+        preference  = []
         for split in dataset.keys():
             for item in tqdm(dataset[split], desc=f"Processing {split}"):
                 if item["safer_response_id"] == '0':  # Changed to string '0'
@@ -29,12 +44,14 @@ def process_pku_saferlhf(output_file):
                     ],
                     "source": "PKU-SafeRLHF"
                 }
-
                 json.dump(preference_data, outfile)
+                preference.append(preference_data)
                 outfile.write('\n')  # Add newline for JSONL format
 
     print("Processing done!")
     print(f"Data saved in {output_file}")
+
+    upload_to_huggingface(preference, "ai2-adapt-dev/PKU-SafeRLHF-processed")
 
 
 if __name__ == "__main__":
