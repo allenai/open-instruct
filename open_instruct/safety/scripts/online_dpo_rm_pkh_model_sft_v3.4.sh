2d39cce6daad24112c3815dc454428a0bad2c5c5
nouhadziri <dziri@ualberta.ca>
2024-10-05 10:26:50 -0400

    remove debug

--- open_instruct/safety/scripts/online_dpo_rm_pkh_model_sft_v3.4.sh
+++ open_instruct/safety/scripts/online_dpo_rm_pkh_model_sft_v3.4.sh
@@ -12,7 +12,7 @@ python /net/nfs.cirrascale/mosaic/nouhad/projects/open-instruct/mason.py \
     --exp_name "safety_online_dpo_vllm_thread_beta_0.03_rm_pkh_sft_v3.4" \
     --dataset_mixer '{"allenai/ultrafeedback_binarized_cleaned_train": 1.0: 1.0, "ai2-adapt-dev/DaringAnteater-prefs-RM-filter": 1.0, "ai2-adapt-dev/only_wildchat_aug28_regenerated_llama": 1.0, "ai2-adapt-dev/PKU-SafeRLHF-processed": 1.0}' \
     --sft_messages_key chosen \
-    --dataset_train_splits train_prefs \
+    --dataset_train_splits train \
     --dataset_eval_mixer '{"allenai/ultrafeedback_binarized_cleaned": 1.0}' \
     --dataset_eval_splits train \
     --max_token_length 1024 \
