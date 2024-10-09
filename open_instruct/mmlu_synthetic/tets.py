649693098945f08f397bd2491bbb8e12e3549ca8
nouhadziri <dziri@ualberta.ca>
2024-10-09 10:38:42 -0400

    avoid reusing the few shot examples

--- open_instruct/mmlu_synthetic/tets.py
+++ open_instruct/mmlu_synthetic/tets.py
@@ -24,7 +24,7 @@ class GenerationArgs:
     max_tokens: int = 500
     top_p: float = 0.95
     examples_per_subject: int = 1000
-    few_shot_examples: int = 3
+    few_shot_examples: int = 10
     similarity_threshold: float = 0.8  # Set a threshold to filter similar questions
 
 
