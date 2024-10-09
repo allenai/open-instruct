28292da25669d0e2a1e3b1d26a59aeee72f57dd2
nouhadziri <dziri@ualberta.ca>
2024-10-09 16:33:13 -0400

    mmlu SFT

---
+++ open_instruct/safety/scripts/sft/llama3-8b-mmlu36k-v3.4-100k.sh
@@ -0,0 +1,7 @@
+python scripts/submit_finetune_job.py \
+  --default_beaker_config configs/beaker_configs/default_finetune.yaml \
+  --config configs/train_configs/sft/mmlu_mix/llama3-8b-mmlu36k-v3.4-100k.yaml\
+  --cluster ai2/jupiter-cirrascale-2 \
+  --priority high \
+  --exp_name nd-SFT-llama3-8b-mmlu36k-v3.4-100k \
+  --num_gpus 8
