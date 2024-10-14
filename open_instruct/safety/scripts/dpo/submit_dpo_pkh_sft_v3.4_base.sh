67eb98f0fdd1e41cd6a49235340ac7765ac9bd73
nouhadziri <dziri@ualberta.ca>
2024-10-14 12:45:44 -0400

    fix dpo script

--- open_instruct/safety/scripts/dpo/submit_dpo_pkh_sft_v3.4_base.sh
+++ open_instruct/safety/scripts/dpo/submit_dpo_pkh_sft_v3.4_base.sh
@@ -3,7 +3,7 @@ python scripts/submit_dpo_job.py \
   --config configs/train_configs/dpo/llama-3.1-8b-dpo-pku.yaml\
   --cluster ai2/jupiter-cirrascale-2 \
   --priority high \
-  --exp_name nd-DPO-tulu3-sft-wildgaurdmixtrain-mixv3.4-valmix-pkh \
+  --experiment_name nd-DPO-tulu3-sft-wildgaurdmixtrain-mixv3.4-valmix-pkh \
   --num_gpus 8 \
   --datasets 01J9VR2PAE6V3Z4P7NPM3CSDFA:/model
 
