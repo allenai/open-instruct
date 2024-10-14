3b1c2870912143d26591a15966d2fb1dd1e0ca87
nouhadziri <dziri@ualberta.ca>
2024-10-14 12:45:02 -0400

    fix dpo script

--- open_instruct/safety/scripts/dpo/submit_dpo_pkh_sft_v3.4_base.sh
+++ open_instruct/safety/scripts/dpo/submit_dpo_pkh_sft_v3.4_base.sh
@@ -1,5 +1,5 @@
 python scripts/submit_dpo_job.py \
-  --default_beaker_config configs/beaker_configs/default_finetune.yaml \
+  --default_beaker_config configs/beaker_configs/default_dpo.yaml \
   --config configs/train_configs/dpo/llama-3.1-8b-dpo-pku.yaml\
   --cluster ai2/jupiter-cirrascale-2 \
   --priority high \
