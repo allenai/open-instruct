723017007bab3568bba6fc8ebf7538e8a5f91f38
nouhadziri <dziri@ualberta.ca>
2024-10-14 22:14:43 -0400

    ablations wildguardMix

---
+++ open_instruct/safety/scripts/sft/llama3.1-8b-NoCot-150k-wildguard-Wildjail-train-tulu3.4sftmix.sh
@@ -0,0 +1,14 @@
+python scripts/submit_finetune_job.py \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --priority high \
+  --num_nodes 8 \
+  --image nathanl/open_instruct_auto-f0b3def-11239828100 \
+  --reduce_loss sum \
+  --default_beaker_config configs/beaker_configs/default_finetune_multinode.yaml \
+  --config configs/train_configs/sft/safety_mix/llama3-8b-200k-wildgaurdmix-WildJail-mixv3.4.yaml \
+  --exp_name  nd-SFT-llama3-8b-200k-wildgaurdmix-Wildjail-mixv3.4
+
+
+
+
+
