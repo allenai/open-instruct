2be2852d542bd76543923ea7a00a7b93710622fc
nouhadziri <dziri@ualberta.ca>
2024-10-15 14:40:28 -0400

    200K ablations

--- open_instruct/safety/scripts/sft/llama3-8b-mmlu36k-v3.4-full.sh
+++ open_instruct/safety/scripts/sft/llama3-8b-mmlu36k-v3.4-full.sh
@@ -1,8 +1,16 @@
+
+
+
 python scripts/submit_finetune_job.py \
-  --default_beaker_config configs/beaker_configs/default_finetune.yaml \
-  --config configs/train_configs/sft/mmlu_mix/llama3-8b-mmlu36k-v3.4-full.yaml\
   --cluster ai2/jupiter-cirrascale-2 \
   --priority high \
-  --exp_name nd-SFT-llama3-8b-mmlu36k-v3.4-full \
-  --num_gpus 8
+  --num_nodes 8 \
+  --image nathanl/open_instruct_auto-f0b3def-11239828100 \
+  --reduce_loss sum \
+  --default_beaker_config configs/beaker_configs/default_finetune_multinode.yaml \
+  --config configs/train_configs/sft/mmlu_mix/llama3-8b-mmlu36k-v3.4-full.yaml \
+  --exp_name nd-SFT-llama3-8b-mmlu36k-v3.4-full
+
+
+
 
