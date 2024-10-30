74d0fa591795ef39c5b9b8ee9db8526392b4714d
nouhadziri <dziri@ualberta.ca>
2024-10-30 16:51:01 -0400

    fix the generate function and debug

--- eval/mmlu/nouha_eval_cot.py
+++ eval/mmlu/nouha_eval_cot.py
@@ -147,7 +147,7 @@ def eval_hf_model(args, subject, model, tokenizer, dev_df, test_df, batch_size=1
         full_responses.append(decoded_response)
 
     # Rest of the function remains the same
-    breakpoint()
+    # breakpoint()
     parsed_answers = []
     for response in full_responses:
         answer = parse_cot_response(response)
