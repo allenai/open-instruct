bdbe9f3afe0ff93651e2413d95518996c8bd71dc
nouhadziri <dziri@ualberta.ca>
2024-11-04 17:57:21 -0500

    fix olmo image

--- open_instruct/olmo/scripts/olmo_test.sh
+++ open_instruct/olmo/scripts/olmo_test.sh
@@ -3,6 +3,6 @@ python scripts/submit_finetune_job.py \
   --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
   --cluster ai2/jupiter-cirrascale-2 ai2/allennlp-cirrascale ai2/mosaic-cirrascale\
   --priority high \
-  --image nouhad/open_instruct_olmo
+  --image nouhad/open_instruct_olmo \
   --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
   --num_gpus 6
