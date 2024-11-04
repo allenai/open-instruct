20a43fd36bdefa5c2d65869e616a13e19cda9c71
nouhadziri <dziri@ualberta.ca>
2024-11-04 17:36:37 -0500

    olmo config

---
+++ open_instruct/olmo/scripts/olmo_test.sh
@@ -0,0 +1,7 @@
+python scripts/submit_finetune_job.py \
+  --default_beaker_config configs/beaker_configs/default_olmo_finetune.yaml \
+  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --priority high \
+  --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
+  --num_gpus 8
