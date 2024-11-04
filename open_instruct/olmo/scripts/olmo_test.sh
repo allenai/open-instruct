11fa6f5b910bc876ce30e79d931d2daa28968590
nouhadziri <dziri@ualberta.ca>
2024-11-04 18:36:30 -0500

    update olmo image

--- open_instruct/olmo/scripts/olmo_test.sh
+++ open_instruct/olmo/scripts/olmo_test.sh
@@ -1,8 +1,8 @@
 python scripts/submit_finetune_job.py \
   --default_beaker_config configs/beaker_configs/default_olmo_finetune.yaml \
   --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
-  --cluster  ai2/mosaic-cirrascale\
+  --cluster  ai2/jupiter-cirrascale-2\
   --priority high \
-  --image nouhad/open_instruct_olmo \
+  --image nathanl/open_instruct_olmo_13 \
   --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
   --num_gpus 6
