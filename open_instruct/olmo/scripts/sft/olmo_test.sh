abd25bf81a5194b28cbd56ee3af8a7ffe7a5d46b
nouhadziri <dziri@ualberta.ca>
2024-11-04 16:04:59 -0500

    rollback my changes

--- open_instruct/olmo/scripts/sft/olmo_test.sh
+++
@@ -1,7 +0,0 @@
-python scripts/submit_finetune_job.py \
-  --default_beaker_config configs/beaker_configs/default_finetune_offloading.yaml \
-  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
-  --cluster ai2/jupiter-cirrascale-2 \
-  --priority high \
-  --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
-  --num_gpus 8
