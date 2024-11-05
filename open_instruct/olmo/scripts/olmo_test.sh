badd3f2bb42991c9d5030d1b455ea170a5905093
nouhadziri <dziri@ualberta.ca>
2024-11-04 21:47:32 -0500

    new config olmo

--- open_instruct/olmo/scripts/olmo_test.sh
+++ open_instruct/olmo/scripts/olmo_test.sh
@@ -1,8 +1,9 @@
 python scripts/submit_finetune_job.py \
   --default_beaker_config configs/beaker_configs/default_olmo_finetune.yaml \
-  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
+  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9-noncommercial.yaml \
   --cluster  ai2/jupiter-cirrascale-2\
   --priority high \
+  --num_nodes 4  \
   --image nathanl/open_instruct_olmo_13 \
   --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
-  --num_gpus 6
+  --num_gpus 8
