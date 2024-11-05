9b41a8e189dddbedfa29a3848ce493f6fd415d4e
nouhadziri <dziri@ualberta.ca>
2024-11-04 23:03:36 -0500

    new config olmo

--- open_instruct/olmo/scripts/olmo_7b_multinode.sh
+++ open_instruct/olmo/scripts/olmo_7b_multinode.sh
@@ -4,6 +4,7 @@ python scripts/submit_finetune_job.py \
   --cluster  ai2/jupiter-cirrascale-2\
   --priority urgent \
   --workspace ai2/tulu-3-dev \
+  --mount_on_weka=oe-training-default \
   --num_nodes 4  \
   --image nathanl/open_instruct_olmo_13 \
   --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
