4f724e5fb39c36389a5221c1019a2f19d9e38331
nouhadziri <dziri@ualberta.ca>
2024-11-05 13:13:21 -0500

    new config

--- open_instruct/olmo/scripts/olmo7b_old_lr2e6_ep3.sh
+++ open_instruct/olmo/scripts/olmo7b_old_lr2e6_ep3.sh
@@ -1,6 +1,6 @@
 python scripts/submit_finetune_job.py \
   --default_beaker_config configs/beaker_configs/default_finetune_multinode.yaml \
-  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
+  --config configs/train_configs/sft/olmo/olmo_7b_old_lr2e6_ep3.yaml\
   --cluster  ai2/jupiter-cirrascale-2\
   --priority urgent \
   --workspace ai2/tulu-3-dev \
