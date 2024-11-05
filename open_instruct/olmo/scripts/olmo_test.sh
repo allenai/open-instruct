941f0dbd449aa95ed1e47f9033faf45d0d6db3a2
nouhadziri <dziri@ualberta.ca>
2024-11-04 22:43:46 -0500

    new config olmo

--- open_instruct/olmo/scripts/olmo_test.sh
+++ open_instruct/olmo/scripts/olmo_test.sh
@@ -1,6 +1,6 @@
 python scripts/submit_finetune_job.py \
   --default_beaker_config configs/beaker_configs/default_olmo_finetune.yaml \
-  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9-noncommercial.yaml \
+  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9_safety.yaml \
   --cluster  ai2/jupiter-cirrascale-2\
   --priority high \
   --num_nodes 4  \
