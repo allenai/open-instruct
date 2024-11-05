91097fb917958f8338b019e15ff24980af373ce1
nouhadziri <dziri@ualberta.ca>
2024-11-04 21:53:44 -0500

    new config olmo

---
+++ open_instruct/olmo/scripts/olmo_7b_multinode.sh
@@ -0,0 +1,9 @@
+python scripts/submit_finetune_job.py \
+  --default_beaker_config configs/beaker_configs/default_finetune_multinode.yaml \
+  --config configs/train_configs/sft/olmo/olmo_7b_0924_v3.9-noncommercial.yaml \
+  --cluster  ai2/jupiter-cirrascale-2\
+  --priority urgent \
+  --num_nodes 4  \
+  --image nathanl/open_instruct_olmo_13 \
+  --exp_name nd-SFT-olmo_7b_0924_v3.9_safety \
+  --num_gpus 8
