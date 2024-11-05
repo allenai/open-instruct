1b29ea84c7cbeda651b19512ce3fe228866994ef
nouhadziri <dziri@ualberta.ca>
2024-11-04 23:46:48 -0500

    new config

--- open_instruct/olmo/scripts/olmo7b_old_lr4e6_ep3.sh
+++ open_instruct/olmo/scripts/olmo7b_old_lr4e6_ep3.sh
@@ -7,6 +7,6 @@ python scripts/submit_finetune_job.py \
   --mount_on_weka=oe-training-default \
   --num_nodes 4  \
   --image nathanl/open_instruct_olmo_13 \
-  --exp_name nd-SFT-olmo7b_old_lr1e6_ep3_v3.9-mix \
+  --exp_name nd-SFT-olmo7b_old_lr4e6_ep3_v3.9-mix \
   --num_gpus 8
 
