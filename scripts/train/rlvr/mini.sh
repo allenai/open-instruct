2bdb3146bc69779301f796de40bf4ca5ced5a734
Costa Huang <costa.huang@outlook.com>
2025-02-01 06:12:26 -0800

    update mini.sh

--- scripts/train/rlvr/mini.sh
+++ scripts/train/rlvr/mini.sh
@@ -1,13 +1,13 @@
 python open_instruct/ppo_vllm_thread_ray_gtrl.py \
-    --dataset_mixer '{"ai2-adapt-dev/gsm8k_math_ifeval_ground_truth_mixed": 1.0}' \
+    --dataset_mixer '{"allenai/RLVR-GSM": 1.0}' \
     --dataset_train_splits train \
-    --dataset_eval_mixer '{"ai2-adapt-dev/gsm8k_math_ground_truth": 1.0}' \
+    --dataset_eval_mixer '{"allenai/RLVR-GSM": 1.0}' \
     --dataset_eval_splits test \
-    --max_token_length 2048 \
-    --max_prompt_token_length 2048 \
-    --response_length 2048 \
-    --model_name_or_path HuggingFaceTB/SmolLM2-360M-Instruct \
-    --reward_model_path HuggingFaceTB/SmolLM2-360M-Instruct \
+    --max_token_length 1023 \
+    --max_prompt_token_length 1024 \
+    --response_length 1024 \
+    --model_name_or_path HuggingFaceTB/SmolLM-135M-Instruct \
+    --reward_model_path HuggingFaceTB/SmolLM-135M-Instruct \
     --non_stop_penalty \
     --stop_token eos \
     --temperature 1.0 \
@@ -15,13 +15,13 @@ python open_instruct/ppo_vllm_thread_ray_gtrl.py \
     --chat_template tulu \
     --sft_messages_key messages \
     --learning_rate 3e-7 \
-    --total_episodes 1000 \
+    --total_episodes 10000 \
     --penalty_reward_value -10.0 \
     --deepspeed_stage 3 \
-    --per_device_train_batch_size 2 \
-    --local_rollout_forward_batch_size 2 \
-    --local_mini_batch_size 32 \
-    --local_rollout_batch_size 32 \
+    --per_device_train_batch_size 1 \
+    --local_rollout_forward_batch_size 1 \
+    --local_mini_batch_size 4 \
+    --local_rollout_batch_size 4 \
     --num_epochs 1 \
     --actor_num_gpus_per_node 1 \
     --vllm_tensor_parallel_size 1 \
@@ -33,4 +33,5 @@ python open_instruct/ppo_vllm_thread_ray_gtrl.py \
     --save_freq 100 \
     --reward_model_multiplier 0.0 \
     --gradient_checkpointing \
+    --vllm_enforce_eager \
     --with_tracking
