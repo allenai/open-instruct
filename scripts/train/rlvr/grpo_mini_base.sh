b8bd23c54894e5e0ea42e490005b60b42f19d7c1
Costa Huang <costa.huang@outlook.com>
2025-02-24 09:31:11 -0500

    push changes

--- scripts/train/rlvr/grpo_mini_base.sh
+++ scripts/train/rlvr/grpo_mini_base.sh
@@ -1,4 +1,7 @@
-python open_instruct/grpo_vllm_thread_ray_gtrl.py \
+exp_name="base_grpo_${RANDOM}"
+python open_instruct/grpo_vllm_thread_ray_gtrl1.py \
+    --exp_name $exp_name \
+    --output_dir /weka/oe-adapt-default/costah/models/$exp_name \
     --dataset_mixer_list ai2-adapt-dev/rlvr_gsm8k_zs 1.0 \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list ai2-adapt-dev/rlvr_gsm8k_zs 1.0 \
@@ -6,35 +9,37 @@ python open_instruct/grpo_vllm_thread_ray_gtrl.py \
     --max_token_length 512 \
     --max_prompt_token_length 512 \
     --response_length 512 \
-    --model_name_or_path EleutherAI/pythia-14m \
     --number_samples_per_prompt 4 \
-    --non_stop_penalty \
+    --model_name_or_path EleutherAI/pythia-14m \
+    --stop_strings "</answer>" \
+    --add_r1_style_format_reward \
+    --non_stop_penalty False \
     --stop_token eos \
-    --temperature 1.0 \
+    --penalty_reward_value 0.0 \
+    --temperature 0.7 \
     --ground_truths_key ground_truth \
-    --chat_template_name tulu \
+    --chat_template_name r1_simple_chat_postpend_think \
     --sft_messages_key messages \
     --learning_rate 3e-7 \
-    --total_episodes 10000 \
-    --penalty_reward_value -10.0 \
+    --total_episodes 1000000 \
     --deepspeed_stage 3 \
     --per_device_train_batch_size 1 \
     --local_rollout_forward_batch_size 1 \
-    --local_mini_batch_size 4 \
-    --local_rollout_batch_size 4 \
+    --local_mini_batch_size 16 \
+    --local_rollout_batch_size 16 \
     --num_epochs 1 \
     --actor_num_gpus_per_node 1 \
     --vllm_tensor_parallel_size 1 \
-    --beta 0.05 \
+    --beta 0.0 \
     --apply_verifiable_reward true \
-    --output_dir output/rlvr_1b \
     --seed 3 \
-    --num_evals 3 \
+    --num_evals 100 \
     --save_freq 100 \
     --reward_model_multiplier 0.0 \
-    --gradient_checkpointing \
+    --no_try_launch_beaker_eval_jobs \
     --single_gpu_mode \
     --vllm_sync_backend gloo \
     --vllm_gpu_memory_utilization 0.5 \
     --vllm_enforce_eager \
+    --gradient_checkpointing \
     # --with_tracking
