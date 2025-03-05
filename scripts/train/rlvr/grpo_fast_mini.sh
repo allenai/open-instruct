69ad9f01a331128532ff7dd89f8e79a6dc78a5db
Costa Huang <costa.huang@outlook.com>
2025-03-05 16:55:15 -0500

    quick change

--- scripts/train/rlvr/grpo_fast_mini.sh
+++ scripts/train/rlvr/grpo_fast_mini.sh
@@ -5,7 +5,7 @@ python open_instruct/grpo_fast.py \
     --dataset_mixer_list ai2-adapt-dev/rlvr_gsm8k_zs 1.0 \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list ai2-adapt-dev/rlvr_gsm8k_zs 1.0 \
-    --dataset_mixer_eval_list_splits test \
+    --dataset_mixer_eval_list_splits train \
     --max_token_length 512 \
     --max_prompt_token_length 256 \
     --response_length 256 \
