1ab9622243fd3e5cad72584ca2736b59c4a68c7e
Hamish Ivison <hamishivi@gmail.com>
2025-03-05 17:08:13 -0800

    fix

--- open_instruct/grpo_vllm_thread_ray_gtrl.py
+++ open_instruct/grpo_vllm_thread_ray_gtrl.py
@@ -1168,7 +1168,7 @@ class PolicyTrainerRayProcess(RayProcess):
                             reward_mult=args.verification_reward,
                         )
                         verifiable_reward = np.array(verifiable_reward)
-                        verifiable_count = (verifiable_reward > 0).to(np.float32)
+                        verifiable_count = (verifiable_reward > 0)
                         score += verifiable_reward
                         # record reward for each verifier function sep.
                         for reward_dict in per_func_reward:
