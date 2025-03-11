957a70c4ce4c96bfef8a665f91ed83f335cc0a15
ljvmiranda921 <ljvmiranda@gmail.com>
2025-03-11 15:28:12 -0700

    Update training scripts

---
+++ toolu_training/toolu_8b.sh
@@ -0,0 +1,13 @@
+python3 -m scripts.submit_finetune_job \
+    --cluster ai2/jupiter-cirrascale-2 \
+    --priority high \
+    --workspace ai2/tulu-3-dev \
+    --num_nodes 4 \
+    --default_beaker_config configs/beaker_configs/default_finetune_multinode.yaml \
+    --config toolu_training/train_configs/toolu_8b_sft.yaml \
+    --exp_name "toolu_test" \
+    --seed $seed \
+    --reduce_loss $loss_type \
+    --learning_rate $lr \
+    --num_train_epochs $epoch \
+    --hf_metadata_dataset allenai/olmo-instruct-evals
