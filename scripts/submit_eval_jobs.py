6be1b233fa07dd0f0a6e0495d62b6ebf05d75aff
saumyamalik <90475752+saumyamalik@users.noreply.github.com>
2025-03-11 13:56:32 -0700

    delete default model mount when not using beaker models (#603)

--- scripts/submit_eval_jobs.py
+++ scripts/submit_eval_jobs.py
@@ -664,13 +664,15 @@ VLLM_WORKER_MULTIPROC_METHOD=spawn PYTHONPATH=. python evaluation/run_all_genera
     --save_individual_results_path /output/all.json \
 '''
     # some copied logic
-    if model_info[0].startswith("hf-"):  # if it's a huggingface model, load it from the model hub
+    if model_info[0].startswith("hf-"):  # if it's a huggingface model, load it from the model hub and delete mount `/model`
         task_spec['arguments'] = [task_spec['arguments'][0].replace("--model_name_or_path /model", f"--model_name_or_path {model_info[1]} --hf_revision {args.hf_revision}")]
         task_spec['arguments'] = [task_spec['arguments'][0].replace("--tokenizer_name_or_path /model", f"--tokenizer_name_or_path {model_info[1]}")]
-    elif model_info[1].startswith("/"):  # if it's a local model, load it from the local directory
+        del task_spec['datasets'][1]
+    elif model_info[1].startswith("/"):  # if it's a local model, load it from the local directory and delete mount `/model`
         assert weka_available, "NFS / Weka is required for path-based models."  # to be safe.
         task_spec['arguments'] = [task_spec['arguments'][0].replace("--model_name_or_path /model", "--model_name_or_path "+model_info[1])]
         task_spec['arguments'] = [task_spec['arguments'][0].replace("--tokenizer_name_or_path /model", "--tokenizer_name_or_path "+model_info[1])]
+        del task_spec['datasets'][1]
     else:  # if it's a beaker model, mount the beaker dataset to `/model`
         task_spec['datasets'][1]['source']['beaker'] = model_info[1]
 
