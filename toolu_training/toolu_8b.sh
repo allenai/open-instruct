96e935ae210ceba764ef4e5c7731458907c3f1c1
ljvmiranda921 <ljvmiranda@gmail.com>
2025-03-13 10:33:32 -0700

    Fix problem on using incorrect image
    Turns out, the scripts.submit_finetune_job script should explicitly
    indicate --image as ljm/open_instruct_toolu. Turns out it overrides
    whatever you put in the beaker_configs/default_finetune_multinode.yaml
    setting.

--- toolu_training/toolu_8b.sh
+++ toolu_training/toolu_8b.sh
@@ -4,6 +4,7 @@ python3 -m scripts.submit_finetune_job \
     --workspace ai2/tulu-3-dev \
     --num_nodes 4 \
     --default_beaker_config toolu_training/beaker_configs/default_finetune_multinode.yaml \
+    --image ljm/open_instruct_toolu \
     --config toolu_training/train_configs/toolu_8b_sft.yaml \
     --exp_name "Llama3.1_toolu" \
     --reduce_loss sum \
