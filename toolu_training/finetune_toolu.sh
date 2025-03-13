80ee66efcffc9ed71620015a47252cbce63d4872
ljvmiranda921 <ljvmiranda@gmail.com>
2025-03-13 14:36:44 -0700

    Update all configurations for Qwen

--- toolu_training/finetune_toolu.sh
+++ toolu_training/finetune_toolu.sh
@@ -5,7 +5,7 @@ python3 -m scripts.submit_finetune_job \
     --num_nodes 4 \
     --default_beaker_config toolu_training/beaker_configs/default_finetune_multinode.yaml \
     --image ljm/open_instruct_toolu \
-    --config toolu_training/train_configs/toolu_8b_sft.yaml \
-    --exp_name "Llama3.1_toolu" \
+    --config toolu_training/train_configs/toolu_sft.yaml \
+    --exp_name "toolu-qwen2.5-7B" \
     --reduce_loss sum \
     --hf_metadata_dataset allenai/olmo-instruct-evals
