9e41273e8444e9683e2d8ca7dc32bf8c86715d95
Costa Huang <costa.huang@outlook.com>
2025-03-14 13:46:23 -0700

    push changes

--- scripts/train/benchmark.sh
+++ scripts/train/benchmark.sh
@@ -1,1 +1,4 @@
 WANDB_PROJECT=open_instruct_public CLUSTER=ai2/augusta-google-1 IMAGE=costah/open_instruct_dev0314 uv run bash scripts/train/tulu3/finetune_8b.sh
+
+
+WANDB_PROJECT=open_instruct_public CLUSTER=ai2/augusta-google-1 IMAGE=costah/open_instruct_dev0314 uv run bash scripts/train/tulu3/finetune_8b.sh
