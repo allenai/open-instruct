00e2100cc78b41b9f3251c42039ef21eb479fcd2
Costa Huang <costa.huang@outlook.com>
2025-03-13 23:30:03 -0400

    Add quick comment

--- open_instruct/dataset_transformation.py
+++ open_instruct/dataset_transformation.py
@@ -355,6 +355,7 @@ def get_tokenizer_tulu_v2_1(tc: "TokenizerConfig"):
 
 def get_tokenizer_tulu_v2_2(tc: "TokenizerConfig"):
     config = AutoConfig.from_pretrained(tc.model_name_or_path, revision=tc.revision)
+    # @vwxyzjn: "olmo" handles both `olmo2` and `olmoe`.
     if "olmo" in config.model_type:
         assert tc.add_bos, "For OLMo, you must run with `--add_bos`."
         assert tc.use_fast, "For OLMo, you must use fast tokenizer."
