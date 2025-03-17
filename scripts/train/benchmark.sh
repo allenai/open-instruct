ea28f3c3d1a464e8e84145c82c54f97607b610f8
Costa Huang <costa.huang@outlook.com>
2025-03-17 05:52:26 -0700

    push changes

--- scripts/train/benchmark.sh
+++ scripts/train/benchmark.sh
@@ -1,19 +1,53 @@
+# 8 nodes
 python launch.py scripts/train/tulu3/finetune_8b.sh \
     --cluster ai2/augusta-google-1 \
     --wandb_project_name open_instruct_public \
     --priority normal \
     --image costah/open_instruct_dev0316 | uv run bash
 
+# 4 nodes
 python launch.py scripts/train/tulu3/dpo_8b.sh \
     --cluster ai2/augusta-google-1 \
     --wandb_project_name open_instruct_public \
     --priority normal \
-    --image costah/open_instruct_dev0316 | uv run bash
+    --image costah/open_instruct_dev0316_7 | uv run bash
 
+# 2 nodes
 python launch.py scripts/train/tulu3/grpo_8b.sh \
     --cluster ai2/augusta-google-1 \
     --wandb_project_name open_instruct_public \
     --priority normal \
     --image costah/open_instruct_dev0316 | uv run bash
 
+# 2 nodes
+python launch.py scripts/train/tulu3/grpo_fast_8b.sh \
+    --cluster ai2/augusta-google-1 \
+    --wandb_project_name open_instruct_public \
+    --priority normal \
+    --image costah/open_instruct_dev0316_8 | uv run bash
 
+
+# (not part of the benchmark) 2 nodes
+python launch.py scripts/train/tulu3/grpo_fast_8b.sh \
+    --cluster ai2/augusta-google-1 \
+    --priority normal \
+    --wandb_project_name open_instruct_public \
+    --exp_name tulu3.1_8b_grpo_fast_axis_1_mean \
+    --masked_mean_axis 1 \
+    --image costah/open_instruct_dev0316_11 | uv run bash
+
+
+# ------------------------------------------------------------
+# single gpu experiments
+python launch.py scripts/train/qwen/grpo_fast_3b_single_node.sh \
+    --cluster ai2/augusta-google-1 \
+    --wandb_project_name open_instruct_public \
+    --priority normal \
+    --image costah/open_instruct_dev0316_10 | uv run bash
+
+
+python launch.py scripts/train/tulu3/grpo_fast_8b_single_node.sh \
+    --cluster ai2/augusta-google-1 \
+    --wandb_project_name open_instruct_public \
+    --priority normal \
+    --image costah/open_instruct_dev0316_11 | uv run bash
