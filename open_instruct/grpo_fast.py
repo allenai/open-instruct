e414d09e5d3c5c9600f32448947604056a184724
Hamish Ivison <hamishivi@gmail.com>
2025-03-18 11:09:32 -0700

    fix epoch logging in grpo_fast (#612)
    * fix epoch logging
    
    * Update open_instruct/grpo_fast.py
    
    Co-authored-by: Costa Huang <costa.huang@outlook.com>
    
    ---------
    
    Co-authored-by: Costa Huang <costa.huang@outlook.com>

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -1504,7 +1504,7 @@ def main(args: Args, model_config: ModelConfig, reward_fn: Callable):
                     "episode": episode,
                     "training_step": training_step,
                     "val/num_total_tokens": num_total_tokens,
-                    "epoch": episode / len(train_dataset),
+                    "epoch": episode / args.num_samples_per_prompt_rollout / len(train_dataset),
                     "tokens_per_second": num_total_tokens / (time.time() - start_time),
                     **data_thread_metrics,
                     **average_metrics,
