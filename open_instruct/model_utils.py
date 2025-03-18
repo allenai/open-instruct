0d1529227d296efbe473f1092efba9e2ef538fd4
Valentina Pyatkin <valentinapy@Valentinas-MacBook-Pro-3.local>
2025-03-18 11:37:50 -0700

    better if stuff

--- open_instruct/model_utils.py
+++ open_instruct/model_utils.py
@@ -241,6 +241,8 @@ def apply_verifiable_reward(
             dataset_list = [dataset]
         else:
             dataset_list = dataset
+        print(ground_truth_list)
+        print(dataset_list)
         assert len(ground_truth_list) == len(dataset_list), "Ground truth and dataset list lengths do not match."
         # for now, we just assume rewards are additive, rather than more complex functions.
         reward = 0
