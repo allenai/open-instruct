038af7aadc0b21dbda0dd4798aba251b6575d416
Costa Huang <costa.huang@outlook.com>
2025-03-18 05:36:17 -0700

    update qwen / olmo recipes

--- scripts/train/benchmark.sh
+++ scripts/train/benchmark.sh
@@ -24,9 +24,39 @@ python launch.py scripts/train/tulu3/grpo_fast_8b.sh \
     --cluster ai2/augusta-google-1 \
     --wandb_project_name open_instruct_public \
     --priority normal \
-    --set_weight_decay_on_bias_and_norm False \
-    --exp_name tulu3.1_8b_grpo_fast_no_weight_decay \
-    --image costah/open_instruct_dev0316_12 | uv run bash
+    --image costah/open_instruct_dev0316_16 | uv run bash
+
+# 2 nodes
+python launch.py scripts/train/qwen/grpo_fast_7b.sh \
+    --cluster ai2/augusta-google-1 \
+    --wandb_project_name open_instruct_public \
+    --priority normal \
+    --image costah/open_instruct_dev0316_16 | uv run bash
+
+
+# 2 nodes
+python launch.py scripts/train/qwen/grpo_7b.sh \
+    --cluster ai2/augusta-google-1 \
+    --wandb_project_name open_instruct_public \
+    --priority normal \
+    --image costah/open_instruct_dev0316_16 | uv run bash
+
+
+# 8 nodes
+python launch.py scripts/train/olmo2/grpo_fast_32b.sh \
+    --cluster ai2/augusta-google-1 \
+    --priority high \
+    --image costah/open_instruct_dev0316_16 | uv run bash
+
+
+python launch.py scripts/train/tulu3/grpo_fast_8b.sh \
+    --cluster ai2/augusta-google-1 \
+    --wandb_project_name open_instruct_public \
+    --priority normal \
+    --seed 3 \
+    --beta 0.00 \
+    --masked_mean_axis 1 \
+    --image costah/open_instruct_dev0316_16 | uv run bash
 
 # (not part of the benchmark) 2 nodes
 python launch.py scripts/train/tulu3/grpo_fast_8b.sh \
@@ -35,7 +65,7 @@ python launch.py scripts/train/tulu3/grpo_fast_8b.sh \
     --wandb_project_name open_instruct_public \
     --exp_name tulu3.1_8b_grpo_fast_axis_1_mean \
     --masked_mean_axis 1 \
-    --image costah/open_instruct_dev0316_11 | uv run bash
+    --image costah/open_instruct_dev0316_15 | uv run bash
 
 
 # ------------------------------------------------------------
