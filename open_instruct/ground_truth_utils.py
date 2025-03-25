5c2956e1a9c6517858876e97ad15f0a50efb2a0f
Valentina Pyatkin <valentinapy@ValentinasMBP3.hsd1.wa.comcast.net>
2025-03-25 12:27:52 -0700

    better if stuff

--- open_instruct/ground_truth_utils.py
+++ open_instruct/ground_truth_utils.py
@@ -183,6 +183,8 @@ class IFEvalVerifier(VerifierFunction):
 
     def __call__(self, tokenized_prediction: List[int], prediction: str, label: Union[str, Dict]) -> bool:
         instruction_dict = instructions_registry.INSTRUCTION_DICT
+        print("DEBUGGING")
+        print(label)
         constraint_dict = label
         answer = prediction.split("<|assistant|>\n")[-1].strip()
         instruction_key = constraint_dict["instruction_id"][0]
