2b51a4e75371c8ca3f37853903532c5161403df1
Valentina Pyatkin <valentinapy@Valentinas-MacBook-Pro-3.local>
2025-04-04 16:26:14 -0700

    better if stuff

--- open_instruct/grpo_vllm_thread_ray_gtrl.py
+++ open_instruct/grpo_vllm_thread_ray_gtrl.py
@@ -1159,6 +1159,7 @@ class PolicyTrainerRayProcess(RayProcess):
                         )
                     # Response Processing 2. run reward model on the truncated responses
                     postprocessed_query_response = torch.cat((query, postprocessed_response), 1)
+                    print(data)
                     messages = data["messages"]
                     messages = messages + messages
                     sequence_length = first_true_indices(postprocessed_response == tokenizer.pad_token_id) - 1
