21202306070db6b80845ebd911df9a8b3e755cf3
Michael Noukhovitch <noukhovm@mila.quebec>
2025-04-06 15:57:10 -0400

    orz 0.5

--- grpo_0.5b.sh
+++ grpo_0.5b.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --gres=gpu:a100l:1
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --mem=24G
 #SBATCH -c 4
-#SBATCH --time=8:00:00
-#SBATCH -p main
+#SBATCH --time=2:00:00
+#SBATCH -p long
 
 source mila.sh
-exp_name="grpo_qwen3b_base"
+exp_name="grpo_orz0.5b_base"
 
 uv run open_instruct/grpo_tinyzero.py \
     --exp_name $exp_name \
@@ -19,10 +19,10 @@ uv run open_instruct/grpo_tinyzero.py \
     --response_length 1024 \
     --pack_length 4096 \
     --per_device_train_batch_size 1 \
-    --num_unique_prompts_rollout 8 \
-    --num_samples_per_prompt_rollout 8 \
+    --num_unique_prompts_rollout 4 \
+    --num_samples_per_prompt_rollout 16 \
     --total_episodes 64000 \
-    --model_name_or_path Qwen/Qwen2.5-3B \
+    --model_name_or_path Open-Reasoner-Zero/Open-Reasoner-Zero-0.5B \
     --base_prompt \
     --stop_strings "<|endoftext|>" "User:" "</answer>" "Assistant:" \
     --apply_r1_style_format_reward \
@@ -30,30 +30,27 @@ uv run open_instruct/grpo_tinyzero.py \
     --apply_verifiable_reward \
     --verification_reward 0.9 \
     --non_stop_penalty False \
-    --temperature 0.7 \
+    --temperature 1.0 \
     --learning_rate 1e-6 \
     --num_epochs 1 \
-    --beta 0.001 \
-    --seed 3 \
-    --num_evals 5 \
-    --save_freq 50 \
     --num_learners_per_node 1 \
     --vllm_tensor_parallel_size 1 \
-    --vllm_gpu_memory_utilization 0.55 \
     --vllm_enable_prefix_caching \
-    --deepspeed_stage 2 \
+    --beta 0.001 \
+    --seed 3 \
+    --num_evals 4 \
+    --save_freq 1000 \
+    --vllm_gpu_memory_utilization 0.35 \
     --single_gpu_mode \
+    --deepspeed_stage 2 \
     --async_mode False \
-    --liger_kernel \
     --vllm_sync_backend gloo \
-    --gradient_checkpointing \
-    --vllm_sleep_level 2 \
-    --offload_ref \
     --fused_optimizer \
     --with_tracking $@
 
+    # --liger_kernel \
     # --vllm_enforce_eager \
-    # --vllm_enable_prefix_caching \
+    # --gradient_checkpointing \
     # --dataset_mixer_list_splits train \
     # --dataset_mixer_eval_list ai2-adapt-dev/rlvr_gsm8k_zs 1.0 \
     # --dataset_mixer_eval_list_splits train \
