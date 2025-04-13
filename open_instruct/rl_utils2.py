4098e444a4d553af831903ac9a4693b794cc0cdd
Costa Huang <costa.huang@outlook.com>
2025-04-12 22:54:24 -0400

    Debug change

--- open_instruct/rl_utils2.py
+++ open_instruct/rl_utils2.py
@@ -51,6 +51,13 @@ class PackedSequences(Generic[T]):
     """packed position ids (batch_size, pack_length)"""
     packed_seq_lens: Optional[np.ndarray] = None
     """packed sequence lengths (batch_size, pack_length)"""
+    dones: Optional[np.ndarray] = None
+    """packed dones (batch_size, pack_length), specifies the sequence boundaries
+    E.g., [0, 0, 0, 0, 1, 0, 0, 0, 0, 2] means the first sequence ends at index 4, and the 
+    second sequence ends at index 9
+    """
+    rewards: Optional[np.ndarray] = None
+    """packed rewards (batch_size, pack_length)"""
 
 
 def reset_position_ids(attention_mask):
@@ -78,6 +85,7 @@ def pack_sequences(
     query_responses = []
     attention_masks = []
     response_masks = []
+    dones = []
     num_actions = []
     packed_seq_lens = []
     cur_data = []
@@ -85,6 +93,7 @@ def pack_sequences(
     cur_num_actions = []
     cur_packed_seq_lens = []
     cur_attention_mask = []
+    cur_dones = []
     offset = 0
     for i in range(len(queries)):
         query = queries[i]
@@ -99,11 +108,13 @@ def pack_sequences(
             attention_masks.append(cur_attention_mask)
             num_actions.append(cur_num_actions)
             packed_seq_lens.append(cur_packed_seq_lens)
+            dones.append(cur_dones)
             cur_data = []
             cur_response_mask = []
             cur_attention_mask = []
             cur_num_actions = []
             cur_packed_seq_lens = []
+            cur_dones = []
             offset = i
         cur_data.extend(query_response)
         cur_num_actions.append(len(response))
@@ -113,12 +124,14 @@ def pack_sequences(
         # the response's index
         cur_response_mask.extend([0 for _ in range(len(query))] + [i + 1 for _ in range(len(response))])
         cur_attention_mask.extend([i + 1 - offset for _ in range(len(query_response))])
+        cur_dones.extend([0 for _ in range(len(query) + len(response) - 1)] + [i + 1])
     if len(cur_data) > 0:
         query_responses.append(cur_data)
         response_masks.append(cur_response_mask)
         attention_masks.append(cur_attention_mask)
         num_actions.append(cur_num_actions)
         packed_seq_lens.append(cur_packed_seq_lens)
+        dones.append(cur_dones)
     attention_masks_list = [torch.tensor(t) for t in attention_masks]
     return PackedSequences(
         query_responses=[torch.tensor(t) for t in query_responses],
@@ -128,6 +141,7 @@ def pack_sequences(
         original_responses=responses,
         num_actions=[torch.tensor(t) for t in num_actions],
         packed_seq_lens=[torch.tensor(t) for t in packed_seq_lens],
+        dones=[torch.tensor(t) for t in dones],
     )
 
 
@@ -152,8 +166,6 @@ def get_test_data():
     responses = [tokenizer.encode(response) for response in outputs]
     prompt_max_len = 20
     generate_max_len = 20
-    for q, r in zip(queries, responses):
-        pprint([q, r])
     assert all(len(query) <= prompt_max_len for query in queries)
     assert all(len(response) <= generate_max_len for response in responses)
     return queries, responses, tokenizer.pad_token_id
@@ -166,7 +178,29 @@ def test_pack_sequences():
         packed_sequences = pack_sequences(
             queries=queries, responses=responses, pack_length=pack_length, pad_token_id=pad_token_id
         )
+
+    # uncomment to debug
+    for q, r in zip(queries, responses):
+        pprint([q, r])
     pprint(packed_sequences)
+
+
+    # assert first and second sequence is correct
+    offset = 0
+    np.testing.assert_allclose(
+        packed_sequences.query_responses[0][offset:offset + len(queries[0]) + len(responses[0])],
+        np.array(sum([queries[0], responses[0]], [])),
+    )
+    offset += len(queries[0]) + len(responses[0])
+    np.testing.assert_allclose(
+        packed_sequences.query_responses[0][offset:offset + len(queries[1]) + len(responses[1])],
+        np.array(sum([queries[1], responses[1]], [])),
+    )
+    offset = 0
+    np.testing.assert_allclose(
+        packed_sequences.query_responses[1][offset:offset + len(queries[2]) + len(responses[2])],
+        np.array(sum([queries[2], responses[2]], [])),
+    )
 
 
 def print_diff(actual: torch.Tensor, expected: torch.Tensor):
@@ -238,6 +272,98 @@ def test_pack_sequences_logits():
     pprint(logprobs)
 
 
+def calculate_advantages(values: np.ndarray, rewards: np.ndarray, gamma: float, lam: float):
+    """Vanilla implementation of GAE. Each row is a separate padded sequence."""
+    lastgaelam = 0
+    advantages_reversed = []
+    gen_length = values.shape[1]
+    for t in reversed(range(gen_length)):
+        nextvalues = values[:, t + 1] if t < gen_length - 1 else 0.0
+        delta = rewards[:, t] + gamma * nextvalues - values[:, t]
+        lastgaelam = delta + gamma * lam * lastgaelam
+        advantages_reversed.append(lastgaelam)
+    advantages = np.stack(advantages_reversed[::-1], axis=1)
+    returns = advantages + values
+    return advantages, returns
+
+
+def calculate_advantages_packed(values: torch.Tensor, rewards: torch.Tensor, gamma: float, lam: float, dones: torch.Tensor, response_masks: torch.Tensor):
+    """Packed implementation of GAE. Each row is a packed sequence.
+    The `dones` specifies the sequence boundaries, and the `response_masks` specifies the query boundaries.
+    """
+    lastgaelam = 0
+    advantages_reversed = []
+    gen_length = values.shape[1]
+    for t in reversed(range(gen_length)):
+        nonterminal = 1 - dones[:, t]
+        nonquery = response_masks[:, t]
+        nextvalues = values[:, t + 1] if t < gen_length - 1 else 0.0
+        delta = rewards[:, t] + gamma * nextvalues * nonterminal * nonquery - values[:, t]
+        lastgaelam = delta + gamma * lam * lastgaelam * nonterminal * nonquery
+        advantages_reversed.append(lastgaelam)
+    advantages = torch.stack(advantages_reversed[::-1], dim=1)
+    returns = advantages + values
+    return advantages, returns
+
+def test_calculate_advantages_packed():
+    gamma = 0.99
+    lam = 0.98
+    values = np.array([
+        [1, 1, 1, 1, 1, 0, 0, 0 ,0, 0, 0, 0],
+        [2, 2, 2, 2, 2, 2, 0, 0 ,0, 0, 0, 0],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    ])
+    rewards = np.array([
+        [0, 0, 0, 0, 10, 0, 0, 0 ,0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 20, 0, 0 ,0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30],
+    ])
+    adv, ret = calculate_advantages(values, rewards, gamma, lam)
+
+    # here we assume -1 is the prompt token that should be ignored
+    # 50256 is the pad token
+    packed_values = np.array([[
+        -1, -1, -1, -1, 1, 1, 1, 1, 1,
+        -1, -1, -1, 2, 2, 2, 2, 2, 2,
+        -1, -1, -1, -1, -1, -1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 50256,
+    ]])
+    packed_rewards = np.array([[
+        0, 0, 0, 0, 0, 0, 0, 0, 10,
+        0, 0, 0, 0, 0, 0, 0, 0, 20,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 50256,
+    ]])
+    packed_dones = np.array([[
+        0, 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 50256,
+    ]])
+    packed_response_masks = np.array([[
+        0, 0, 0, 0, 1, 1, 1, 1, 1,
+        0, 0, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 50256,
+    ]])
+    packed_adv, packed_ret = calculate_advantages_packed(packed_values,
+        packed_rewards,
+        gamma,
+        lam,
+        packed_dones,
+        packed_response_masks
+    )
+    # uncomment to debug
+    # print("vanilla GAE implementation")
+    # print(adv.round(2))
+    # print(ret.round(2))
+    # print("packed GAE implementation with masked prompts")
+    # print(packed_adv.round(2))
+    # print(packed_ret.round(2))
+
+    # actual test cases
+    np.testing.assert_allclose(adv[0, :5], packed_adv[0, 4:9])
+    np.testing.assert_allclose(ret[1, :6], packed_ret[0, 12:18])
+    np.testing.assert_allclose(adv[2, :12], packed_adv[0, 24:36])
+
+
 if __name__ == "__main__":
     test_pack_sequences()
-    test_pack_sequences_logits()
+    # test_pack_sequences_logits()
+    test_calculate_advantages_packed()
