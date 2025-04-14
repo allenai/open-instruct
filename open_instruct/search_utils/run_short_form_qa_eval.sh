7746b5c9f35255aa57f9d815f80228ad068678e4
Hamish Ivison <hamishivi@gmail.com>
2025-04-15 09:56:40 +1000

    fix

--- open_instruct/search_utils/run_short_form_qa_eval.sh
+++ open_instruct/search_utils/run_short_form_qa_eval.sh
@@ -6,5 +6,5 @@ for dataset in hotpotqa nq tqa 2wiki; do
     python -m open_instruct.search_utils.short_form_qa_eval \
         --dataset_name $dataset \
         --model_path ${MODEL_PATH} \
-        --output_dir ${1}_${dataset}
+        --output_dir ${MODEL_PATH}/${dataset}_results
 done
