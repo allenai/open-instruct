3a8d04cf682846f0ac0937bfadeb0c675c27e8f0
Hamish Ivison <hamishivi@gmail.com>
2025-04-15 11:58:51 +1000

    fixup eval

--- open_instruct/search_utils/run_short_form_qa_eval.sh
+++ open_instruct/search_utils/run_short_form_qa_eval.sh
@@ -2,7 +2,7 @@ set -ex
 
 MODEL_PATH=$1
 
-for dataset in hotpotqa nq tqa 2wiki; do
+for dataset in hotpotqa nq tqa 2wiki simpleqa; do
     python -m open_instruct.search_utils.short_form_qa_eval \
         --dataset_name $dataset \
         --model_path ${MODEL_PATH} \
