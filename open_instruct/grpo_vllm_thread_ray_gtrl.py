102562db4ebc515d2c1cee028a0bc4eac7df39e0
Hamish Ivison <hamishivi@gmail.com>
2025-04-23 08:50:07 +1000

    Length Control Reward + other cleanup (#657)
    * first pass at mult verifies + max length check
    
    * update
    
    * minor tweak
    
    * fix bug
    
    * fix
    
    * bug fixes
    
    * push changes
    
    * quick change
    
    * add sequence length eval
    
    * trying a new reward function
    
    * fix
    
    * fix
    
    * better logging
    
    * add tokens per second metric
    
    * allow training with mini batches
    
    * fix index out of bound issues
    
    * return to previous setting
    
    * change it back, but per_device_train_batch_size > 1 does not work.
    
    * ok now pdbs>1 should work, accumulation steps was wrong
    
    * update tokens per second calculation based on iteration instead
    
    * add data thread
    
    * graceful shutdown
    
    * making the save logic works
    
    * refactor
    
    * Fixes here
    
    * remove unused
    
    * add better traceback
    
    * pin collatoed tensors
    
    * send the queries data early, so as not to block the data preparation thread.
    
    * push GRPO scripts
    
    * Apply suggestions from code review
    
    Co-authored-by: Nathan Lambert <nathanl@allenai.org>
    
    * update docs, remove unused stuff
    
    * refactor
    
    * change how tps is calculated
    
    * auto set gpu memory utilization for single gpu
    
    * add a manual eos token for starting from base
    
    * try up to
    
    * fix
    
    * fix
    
    * make reward_fn more accessible
    
    * quick change
    
    * add arithmetic reward
    
    * push changes
    
    * initial stab at more advanced verifiers
    
    * fix
    
    * swap to numeric scores always
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * i am silly
    
    * fix max len func
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix logging?
    
    * fix logging?
    
    * fix logging?
    
    * fix logging?
    
    * lint
    
    * edits for grpo fast
    
    * lint
    
    * trying sometibg
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add comment
    
    * Update open_instruct/grpo_fast.py
    
    Co-authored-by: Costa Huang <costa.huang@outlook.com>
    
    * costa comment
    
    * edit ppo
    
    * fix
    
    * fix
    
    * fix
    
    * dbl len
    
    * higher weight
    
    * fix padding thing
    
    * fix padding thing
    
    * fix
    
    * try neg
    
    * try original stuff
    
    * fix epoch logging
    
    * mean instead of sum KL logging
    
    * fix
    
    * tulu thinker template
    
    * tulu thinker dataset
    
    * add thinker reward, edit base format reward
    
    * fix
    
    * update data creation scripts
    
    * new format for tulu thinker
    
    * some fixes
    
    * further data tweaks
    
    * some fixes
    
    * fix reward
    
    * fix reward
    
    * fix data formatting.
    
    * remove thinking removal in chat template
    
    * tryin something
    
    * new reewards, downweight
    
    * new reward stuff
    
    * fix
    
    * cleanup for pr
    
    * cleanup
    
    * lint
    
    * remove todo
    
    * feedback
    
    ---------
    
    Co-authored-by: Costa Huang <costa.huang@outlook.com>
    Co-authored-by: Nathan Lambert <nathanl@allenai.org>

--- open_instruct/grpo_vllm_thread_ray_gtrl.py
+++ open_instruct/grpo_vllm_thread_ray_gtrl.py
@@ -58,6 +58,7 @@ from collections import deque
 from dataclasses import asdict, dataclass, field
 from queue import Empty, Queue
 from typing import Any, Callable, Iterator, List, Literal, Optional, Tuple
+from itertools import chain
 
 import numpy as np
 import pandas as pd
@@ -799,7 +800,10 @@ class PolicyTrainerRayProcess(RayProcess):
 
         # get list of all reward types in dataset, used for logging
         # sorted to make sure the order is consistent
-        reward_types = sorted(list(set(train_dataset.unique("dataset"))))
+        # have to handle case where dataset is a list of values.
+        dataset_column = train_dataset["dataset"]
+        all_datasets = chain.from_iterable(d if isinstance(d, list) else [d] for d in dataset_column)
+        reward_types = sorted(set(all_datasets))
 
         args = self.args
         self.tokenizer = tokenizer
@@ -1172,7 +1176,7 @@ class PolicyTrainerRayProcess(RayProcess):
                         dataset = datasets[i : i + args.local_rollout_forward_batch_size]
                         decoded_response = tokenizer.batch_decode(postprocessed_response)
                         verifiable_reward, per_func_reward = apply_verifiable_reward(
-                            responses=postprocessed_response,
+                            responses=[seq[seq != tokenizer.pad_token_id].tolist() for seq in postprocessed_response],
                             decoded_responses=decoded_response,
                             ground_truths=ground_truth,
                             datasets=dataset,
@@ -1361,7 +1365,7 @@ class PolicyTrainerRayProcess(RayProcess):
                     "episode": episode,
                     "training_step": training_step,
                     "lr": self.scheduler.get_last_lr()[0],
-                    "epoch": episode / len(train_dataset),
+                    "epoch": episode / args.number_samples_per_prompt / len(train_dataset),
                     "time/from_scratch": time.time() - start_time,
                     "time/training": time.time() - training_time_start,
                     **local_metrics.get_reduced_metrics_correctness(),
