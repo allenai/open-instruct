70b4befdd4d4e378926690fa6b51fa904b7dc544
Hamish Ivison <hamishivi@gmail.com>
2025-05-02 11:46:22 -0700

    update serrch tool

--- open_instruct/search_utils/search_tool.py
+++ open_instruct/search_utils/search_tool.py
@@ -1,4 +1,5 @@
 import re
+import time
 from open_instruct.tool_utils.tool_vllm import Tool, ToolOutput
 from open_instruct.search_utils.massive_ds import get_snippets_for_query
 
@@ -18,21 +19,24 @@ class SearchTool(Tool):
         query_blocks = re.findall(re_str, prompt, re.DOTALL)
         
         if len(query_blocks) == 0:
-            return ToolOutput(output="", called=False, success=False)
+            return ToolOutput(output="", called=False, error="", timeout=False, runtime=0)
 
         # Only execute the last code block
         query_string = query_blocks[-1]
 
         if not query_string:
-            return ToolOutput(output="Empty Query.", called=True, success=False, start_str="<document>\n", end_str="\n</document>")
+            return ToolOutput(output="", error="Empty query. Please provide some text in the query.", called=True, timeout=False,  runtime=0)
         
+        start_time = time.time()
+        timeout = False
+        error = ""
         snippets = get_snippets_for_query(query_string, api_endpoint=self.api_endpoint, number_of_results=self.number_documents_to_search)
 
         if not snippets or len(snippets) == 0:
-            return ToolOutput(output="Query failed.", called=True, success=False, start_str="<document>\n", end_str="\n</document>")
+            return ToolOutput(output="", error="Query failed for unknown reason.", called=True, timeout=False,  runtime=time.time() - start_time)
 
         # for now, we just join all snippets.
         snippets = [snippet.strip() for snippet in snippets]
         all_snippets = "\n".join(snippets).strip()
 
-        return ToolOutput(output=all_snippets, called=True, success=True, start_str="<document>\n", end_str="\n</document>")
+        return ToolOutput(output=all_snippets, called=True, error=error, timeout=timeout, runtime=time.time() - start_time, start_str="<document>\n", end_str="\n</document>")
