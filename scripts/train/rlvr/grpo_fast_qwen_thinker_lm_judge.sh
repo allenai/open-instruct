162edba3e2a54003f732d9000c82f34d8ed958a1
Faeze Brahman <fbrahman@ucsc.edu>
2025-05-06 15:59:30 -0700

    eval task modification

--- scripts/train/rlvr/grpo_fast_qwen_thinker_lm_judge.sh
+++ scripts/train/rlvr/grpo_fast_qwen_thinker_lm_judge.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # older data:  faezeb/tulu-3-sft-t3-70b-thinker-sampled
+# rubric+ref+math+shortqa_ifeval: ai2-adapt-dev/tulu-3-sft-92k-criteria-gpt4o-classified-rewritten-math-shortqa-ifeval-v2
+# ref-based / ref+math+shortqa+ifeval: ai2-adapt-dev/tulu-3-sft-92k-classified-rewritten-math-shortqa-ifeval-ref-based
 
 # saurab command:
-exp_name="0504_qwen2.5_7B_thinker_grpo_fast_llm_judge__gpt-4o_quality_ref_${RANDOM}_large"
-
+# exp_name="0504_qwen2.5_7B_thinker_grpo_fast_llm_judge__gpt-4o_quality_ref_${RANDOM}_large"
+exp_name="0505_qwen2.5_7B_thinker_grpo_fast_llm_judge_gpt-4o_quality_ref_${RANDOM}_v2"
+exp_name="0505_qwen2.5_7B_thinker_grpo_fast_llm_judge_gpt-4o_quality_ref_3188_v2/resume_from_step_250/"
+model_weka_path="/weka/oe-adapt-default/faezeb/model_checkpoints/0505_qwen2.5_7B_thinker_grpo_fast_llm_judge_gpt-4o_quality_ref_3188_v2/grpo_fast_wip__1__1746517521_checkpoints/step_250"
 python mason.py \
     --description $exp_name \
     --cluster ai2/jupiter-cirrascale-2 \
@@ -15,9 +19,9 @@ python mason.py \
     --budget ai2/oe-adapt \
     --image ai2/cuda11.8-cudnn8-dev-ubuntu20.04 \
     --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast_wip.py \
-    --dataset_mixer_list ai2-adapt-dev/tulu-3-sft-92k-criteria-gpt4o-classified-rewritten-math-shortqa-ifeval 90000 \
+    --dataset_mixer_list ai2-adapt-dev/tulu-3-sft-92k-classified-rewritten-math-shortqa-ifeval-ref-based 90000 \
     --dataset_mixer_list_splits train \
-    --dataset_mixer_eval_list ai2-adapt-dev/tulu-3-sft-92k-criteria-gpt4o-classified-rewritten-math-shortqa-ifeval 128 \
+    --dataset_mixer_eval_list ai2-adapt-dev/tulu-3-sft-92k-classified-rewritten-math-shortqa-ifeval-ref-based 128 \
     --dataset_mixer_eval_list_splits train \
     --max_token_length 10240 \
     --max_prompt_token_length 2048 \
@@ -26,8 +30,8 @@ python mason.py \
     --per_device_train_batch_size 1 \
     --num_unique_prompts_rollout 128 \
     --num_samples_per_prompt_rollout 8 \
-    --model_name_or_path Qwen/Qwen2.5-7B \
-    --oe_eval_tasks minerva_math::hamish_zs_reasoning,bbh:cot::hamish_zs_reasoning,gsm8k::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,aime::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,alpaca_eval_v2::tulu,ifeval::tulu,popqa::tulu,drop::llama3 \
+    --model_name_or_path $model_weka_path \
+    --oe_eval_tasks minerva_math::hamish_zs_reasoning,bbh:cot::hamish_zs_reasoning,gsm8k::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,aime::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,alpaca_eval_v2::tulu,ifeval::tulu,popqa::tulu,drop::llama3,codex_humanevalplus::tulu,mmlu:cot::summarize \
     --output_dir /weka/oe-adapt-default/faezeb/model_checkpoints/${exp_name} \
     --apply_llm_verifier_reward true \
     --apply_verifiable_reward false \
@@ -53,6 +57,7 @@ python mason.py \
     --seed 1 \
     --num_evals 50 \
     --save_freq 50 \
+    --save_traces \
     --try_launch_beaker_eval_jobs_on_weka True \
     --gradient_checkpointing \
     --with_tracking \
@@ -63,6 +68,7 @@ python mason.py \
     # --num_mini_batches 1 \
     # --num_learners_per_node 8 8 \
     # --vllm_num_engines 16 \
+    # --model_name_or_path Qwen/Qwen2.5-7B \
 
     # --image faezeb/open_instruct_llm_judge_async --pure_docker_mode \
     # --beaker_datasets /models:01JQAWR48DRN9XCHX6YQCG9RF8 \
