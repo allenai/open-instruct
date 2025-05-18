3595200d14240e9d9609d2b50c053a7208b1d2c7
jacob-morrison <jacobm@allenai.org>
2025-05-18 14:32:16 -0700

    test

--- grpo_fast_script_rm.sh
+++ grpo_fast_script_rm.sh
@@ -5,8 +5,8 @@
 # tulu3-rewritten with ref base lm judge + ifeval: ai2-adapt-dev/tulu_3_rewritten_100k-v2-ifeval
 # general-thoughts-100k-rewritten with ref base lm judge + ifeval: ai2-adapt-dev/general-thoughts-100k-rewritten-v2-ifeval
 
-# reward_model_revision="rm_qwenbase_2e-5_1_skyworkstulufull__1__1745388738"
-reward_model_revision="main"
+reward_model_revision="rm_qwenbase_2e-5_1_skyworkstulufull__1__1745388738"
+# reward_model_revision="main"
 # exp_name="0504_qwen2.5_7B_thinker_grpo_fast_llm_judge__gpt-4o_quality_ref_${RANDOM}_large"
 # exp_name="0508_qwen2.5_7B_thinker_grpo_rm_judge_${RANDOM}_tulu_3_rewritten_no_verifiable"
 exp_name="test_rm_during_grpo"
@@ -21,7 +21,7 @@ python mason.py \
     --budget ai2/oe-adapt \
     --image ai2/cuda11.8-cudnn8-dev-ubuntu20.04 \
     --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
-    --dataset_mixer_list jacobmorrison/tulu_3_rewritten_53k_no_verifiable 53122 \
+    --dataset_mixer_list jacobmorrison/tulu_3_rewritten_53k_no_verifiable 5000 \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list jacobmorrison/tulu_3_rewritten_53k_no_verifiable 128 \
     --dataset_mixer_eval_list_splits train \
@@ -32,14 +32,14 @@ python mason.py \
     --per_device_train_batch_size 1 \
     --num_unique_prompts_rollout 16 \
     --num_samples_per_prompt_rollout 8 \
-    --model_name_or_path meta-llama/Llama-3.2-1B \
+    --model_name_or_path Qwen/Qwen2.5-0.5B \
     --oe_eval_tasks minerva_math::hamish_zs_reasoning,bbh:cot::hamish_zs_reasoning,gsm8k::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,aime::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,alpaca_eval_v2::tulu,ifeval::tulu,popqa::tulu,drop::llama3,codex_humanevalplus::tulu,mmlu:cot::summarize \
     --output_dir /weka/oe-adapt-default/jacobm/llm-as-a-judge/checkpoints/rm-vs-llm/${exp_name} \
     --apply_verifiable_reward false \
     --apply_r1_style_format_reward true \
-    --reward_model_path Ray2333/GRM-Llama3.2-3B-rewardmodel-ft \
+    --reward_model_path allenai/open_instruct_dev \
     --reward_model_revision $reward_model_revision \
-    --reward_model_tokenizer_path Ray2333/GRM-Llama3.2-3B-rewardmodel-ft \
+    --reward_model_tokenizer_path allenai/open_instruct_dev \
     --reward_model_tokenizer_revision $reward_model_revision \
     --non_stop_penalty True \
     --non_stop_penalty_value 0.0 \
