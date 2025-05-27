e1cf0f2f81fbb1dd12291e164039f048e8266ec5
jacob-morrison <jacobm@allenai.org>
2025-05-27 12:33:18 -0700

    test

--- backfill_reasoning.sh
+++ backfill_reasoning.sh
@@ -1,5 +1,5 @@
 for eval in minerva_math::hamish_zs_reasoning bbh:cot::hamish_zs_reasoning gsm8k::hamish_zs_reasoning minerva_math_500::hamish_zs_reasoning zebralogic::hamish_zs_reasoning aime::hamish_zs_reasoning agi_eval_english:0shot_cot::hamish_zs_reasoning gpqa:0shot_cot::hamish_zs_reasoning codex_humanevalplus:0-shot-chat::tulu-thinker ifeval::tulu popqa::tulu mmlu:mc::tulu bbh:cot-v1::tulu; do
-for step in 80 120 160 200 240; do
+for step in 280 320 360 400 440 480 520 560 600; do
 python oe_eval/launch.py \
   --model lmeval-test_grpo_rm_with_valpy_code__1__1748026885_step_${step} \
   --beaker-workspace ai2/tulu-3-results \
