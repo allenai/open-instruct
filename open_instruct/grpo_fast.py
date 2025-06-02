cf9efc4407050ca5acde822370e7190da1d8e4e9
Ashish Agrawal <ashish.agrawal9@ibm.com>
2025-06-02 06:51:27 +0000

    add whitespace

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -725,7 +725,7 @@ class PolicyTrainerRayProcess(RayProcess):
         to_device_inplace(collated_position_ids, self.device)
         to_device_inplace(collated_advantages, self.device)
         to_device_inplace(collated_response_masks, self.device)
-        accumulation_steps = math.ceil(len(collated_query_responses)/num_mini_batches - 0.5)
+        accumulation_steps = math.ceil(len(collated_query_responses) / num_mini_batches - 0.5)
         leftover = len(collated_query_responses) % accumulation_steps
         if leftover > 0:
             collated_query_responses = collated_query_responses[0:-leftover]
