fix ppo reward type calculation
take into account that individual entries can be lists
fixes #717

open_instruct/ppo_vllm_thread_ray_gtrl.py
@@ -847,7 +847,7 @@ class PolicyTrainerRayProcess(RayProcess):
 
         # get list of all reward types in dataset, used for logging
         # sorted to make sure the order is consistent
-        reward_types = sorted(list(set(train_dataset.unique("dataset"))))
+        reward_types = sorted(np.unique(np.array(train_dataset["dataset"]).flat))
 
         args = self.args
         self.tokenizer = tokenizer