8b35ad0751021826b142e785bf25fac4786f0ab0
ValentinaPy <valentina@posteo.de>
2025-06-15 10:02:47 -0700

    addressing Hamish's comments

---
+++ base_config.sh
@@ -0,0 +1,58 @@
+python mason.py \
+    --cluster ai2/jupiter-cirrascale-2 --image hamishivi/open_instruct_hamish_dev_1306_2 \
+    --pure_docker_mode \
+    --workspace ai2/tulu-thinker \
+    --priority high \
+    --preemptible \
+    --num_nodes ${num_nodes} \
+    --max_retries 0 \
+    --env HOSTED_VLLM_API_BASE=http://saturn-cs-aus-236.reviz.ai2.in:8000/v1 \
+    --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
+    --budget ai2/oe-adapt \
+    --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& source configs/beaker_configs/code_api_setup.sh \&\& python open_instruct/grpo_fast.py \
+    --exp_name ${exp_name} \
+    --beta 0.0 \
+    --add_bos ${add_bos} \
+    --num_samples_per_prompt_rollout 16 \
+    --num_unique_prompts_rollout 128 \
+    --num_mini_batches 1 \
+    --gather_whole_model False \
+    --num_epochs 1 \
+    --learning_rate 5e-7 \
+    --per_device_train_batch_size 1 \
+    --output_dir /output \
+    --kl_estimator kl3 \
+    --dataset_mixer_list ${split_value} \
+    --dataset_mixer_list_splits train \
+    --dataset_mixer_eval_list hamishivi/tulu_3_rewritten_100k 32 \
+    --dataset_mixer_eval_list_splits train \
+    --max_token_length 10240 \
+    --max_prompt_token_length 2048 \
+    --response_length 8192 \
+    --pack_length 16384 \
+    --model_name_or_path ${model_name_or_path} \
+    --model_revision ${model_revision} \
+    --tokenizer_name_or_path ${tokenizer_name_or_path} \
+    --chat_template_name ${chat_template_name} \
+    --stop_strings "</answer>" \
+    --non_stop_penalty False \
+    --temperature 1.0 \
+    --ground_truths_key ground_truth \
+    --sft_messages_key messages \
+    --total_episodes 10000000 \
+    --deepspeed_stage ${deepspeed_stage} \
+    --num_learners_per_node ${num_learners_per_node} \
+    --vllm_num_engines ${vllm_num_engines} \
+    --vllm_tensor_parallel_size 1 \
+    --lr_scheduler_type constant \
+    --apply_verifiable_reward true \
+    --seed 1 \
+    --num_evals 5 \
+    --save_freq 100 \
+    --try_launch_beaker_eval_jobs_on_weka True \
+    --gradient_checkpointing \
+    --llm_judge_timeout 600 \
+    --llm_judge_model "hosted_vllm/hamishivi/general-verifier" \
+    --with_tracking \
+    --oe_eval_max_length 32768 \
+    --oe_eval_tasks "minerva_math::hamish_zs_reasoning,bbh:cot::hamish_zs_reasoning,gsm8k::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,aime::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,codex_humanevalplus:0-shot-chat::tulu-thinker,ifeval::tulu,popqa::tulu,mmlu:mc::tulu,bbh:cot-v1::tulu"
