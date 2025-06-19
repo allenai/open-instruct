35a3cc90694bd5725e6d8aa92e4da620b0019cfc
ValentinaPy <valentina@posteo.de>
2025-06-19 12:18:39 -0700

    from base with reasoning

--- multiturn_config.sh
+++ multiturn_config.sh
@@ -18,10 +18,10 @@ python mason.py \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list valpy/IF_multiturn2 16 \
     --dataset_mixer_eval_list_splits train \
-    --max_token_length 10240 \
+    --max_token_length 2048 \
     --max_prompt_token_length 2048 \
-    --response_length 8192 \
-    --pack_length 16384 \
+    --response_length 2048 \
+    --pack_length 4096 \
     --model_name_or_path allenai/Llama-3.1-Tulu-3-8B-DPO \
     --apply_verifiable_reward True \
     --non_stop_penalty True \
@@ -29,7 +29,7 @@ python mason.py \
     --temperature 1.0 \
     --chat_template_name tulu \
     --oe_eval_tasks  ifeval::tulu \
-    --oe_eval_max_length 32768 \
+    --oe_eval_max_length 2048 \
     --total_episodes 2000000 \
     --deepspeed_stage 2 \
     --per_device_train_batch_size 1 \
