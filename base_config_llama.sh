01f524a2ef2d5a234417e64bd01894c8b6f207e8
ValentinaPy <valentina@posteo.de>
2025-06-22 13:14:00 -0700

    from base with reasoning

--- base_config_llama.sh
+++ base_config_llama.sh
@@ -8,7 +8,7 @@ python mason.py \
     --budget ai2/oe-adapt \
     --max_retries 0 \
     --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
-    --exp_name valpy_if_from_base_llama3.1_multi \
+    --exp_name valpy_if_from_base_llama3.1_single_nolang \
     --beta 0.0 \
     --add_bos False \
     --num_samples_per_prompt_rollout 16 \
@@ -20,9 +20,9 @@ python mason.py \
     --per_device_train_batch_size 1 \
     --try_launch_beaker_eval_jobs_on_weka \
     --kl_estimator kl3 \
-    --dataset_mixer_list valpy/IF_multiturn2 1.0 \
+    --dataset_mixer_list allenai/IF_multi_constraints_upto5_no_lang 1.0 \
     --dataset_mixer_list_splits train \
-    --dataset_mixer_eval_list valpy/IF_multiturn2 16 \
+    --dataset_mixer_eval_list allenai/IF_multi_constraints_upto5_no_lang 16 \
     --dataset_mixer_eval_list_splits train \
     --max_token_length 10240 \
     --max_prompt_token_length 2048 \
@@ -50,6 +50,5 @@ python mason.py \
     --seed 1 \
     --num_evals 5 \
     --save_freq 100 \
-    --save_traces True \
     --gradient_checkpointing \
     --with_tracking
