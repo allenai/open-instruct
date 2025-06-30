9fef84a7d044cf714e9c8d7f73c57528eac6144c
ValentinaPy <valentina@posteo.de>
2025-06-29 20:17:42 -0600

    from base with reasoning

--- base_config2.sh
+++ base_config2.sh
@@ -8,7 +8,7 @@ python mason.py \
     --budget ai2/oe-adapt \
     --max_retries 0 \
     --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
-    --exp_name valpy_if_from_base_qwen2.5_25old \
+    --exp_name valpy_if_from_base_qwen2.5_3constraint \
     --beta 0.0 \
     --add_bos False \
     --num_samples_per_prompt_rollout 16 \
@@ -20,9 +20,9 @@ python mason.py \
     --per_device_train_batch_size 1 \
     --try_launch_beaker_eval_jobs_on_weka \
     --kl_estimator kl3 \
-    --dataset_mixer_list valpy/new_25old_5 1.0 \
+    --dataset_mixer_list valpy/3constraint 1.0 \
     --dataset_mixer_list_splits train \
-    --dataset_mixer_eval_list valpy/new_25old_5 16 \
+    --dataset_mixer_eval_list valpy/3constraint 16 \
     --dataset_mixer_eval_list_splits train \
     --max_token_length 10240 \
     --max_prompt_token_length 2048 \
