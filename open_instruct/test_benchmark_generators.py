36c9fc8b097c4d42af686de6bd7f811974e84d97
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-07-11 15:13:43 -0600

    Added test for benchmark generator script.

---
+++ open_instruct/test_benchmark_generators.py
@@ -0,0 +1,18 @@
+import unittest
+
+import parameterized
+
+from open_instruct import benchmark_generators
+
+
+class TestBenchmark(unittest.TestCase):
+    @parameterized.parameterized.expand(
+        [("NVIDIA H100 80GB HBM3", "h100"), ("NVIDIA L40S", "l40s"), ("NVIDIA RTX A6000", "a6000")]
+    )
+    def test_get_device_name(self, device_name, expected):
+        result = benchmark_generators.get_device_name(device_name)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
