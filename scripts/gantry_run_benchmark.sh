d4616e8a9492fd2a0fc83f9eb6f5047ad0b17095
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-07-17 19:14:43 +0000

    Undid changes to benchmark script.

--- scripts/gantry_run_benchmark.sh
+++ scripts/gantry_run_benchmark.sh
@@ -21,7 +21,6 @@ gantry run \
        --beaker-image nathanl/open_instruct_auto \
        --cluster ai2/jupiter-cirrascale-2 \
        --budget ai2/oe-eval \
-       --env "VLLM_ALLOW_LONG_MAX_MODEL_LEN=1" \
        --install 'pip install --upgrade pip "setuptools<70.0.0" wheel 
 # TODO, unpin setuptools when this issue in flash attention is resolved
 pip install torch==2.7.0 torchvision==0.22.0 --index-url https://download.pytorch.org/whl/cu128
@@ -31,8 +30,8 @@ pip install -r requirements.txt
 pip install -e .
 python -m nltk.downloader punkt' \
        -- python -m open_instruct.benchmark_generators \
-    --model_name_or_path "allenai/OLMo-2-1124-7B" \
-    --tokenizer_name_or_path "allenai/OLMo-2-1124-7B" \
+    --model_name_or_path "hamishivi/qwen2_5_openthoughts2" \
+    --tokenizer_name_or_path "hamishivi/qwen2_5_openthoughts2" \
     --dataset_mixer_list "hamishivi/hamishivi_rlvr_orz_math_57k_collected_all_filtered_hamishivi_qwen2_5_openthoughts2" "1.0" \
     --dataset_mixer_list_splits "train" \
     --max_token_length 10240 \
@@ -41,7 +40,7 @@ python -m nltk.downloader punkt' \
     --response_length "$response_length" \
     --vllm_top_p 0.9 \
     --num_unique_prompts_rollout 32 \
-    --num_samples_per_prompt_rollout 8 \
+    --num_samples_per_prompt_rollout 16 \
     --vllm_num_engines 1 \
     --vllm_tensor_parallel_size 1 \
     --vllm_gpu_memory_utilization 0.9 \
@@ -51,4 +50,5 @@ python -m nltk.downloader punkt' \
     --seed 42 \
     --dataset_local_cache_dir "benchmark_cache" \
     --dataset_cache_mode "local" \
-    --dataset_transform_fn "rlvr_tokenize_v1" "rlvr_filter_v1"
+    --dataset_transform_fn "rlvr_tokenize_v1" "rlvr_filter_v1" \
+    "$@"
