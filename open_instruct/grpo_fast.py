1445b0c1949dc093fec42423427c8de0583123c5
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-07-22 16:57:23 -0600

    Ran linter.

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -30,6 +30,7 @@
 # isort: off
 import os
 import tqdm
+
 os.environ["NCCL_CUMEM_ENABLE"] = "0"  # NOQA
 try:
     import deepspeed
@@ -1129,10 +1130,12 @@ def accumulate_inference_batches(
     all_ground_truths = []
     all_datasets = []
 
-    for _ in tqdm.tqdm(range(expected_results),
-                       desc=f"Accumulating results for training step {training_step}",
-                       # Adds a new line after the progress bar so that logs look better.
-                       bar_format="{l_bar}{bar}{r_bar}\n"):
+    for _ in tqdm.tqdm(
+        range(expected_results),
+        desc=f"Accumulating results for training step {training_step}",
+        # Adds a new line after the progress bar so that logs look better.
+        bar_format="{l_bar}{bar}{r_bar}\n",
+    ):
         # Get individual result from queue
         result = inference_results_Q.get()
 
