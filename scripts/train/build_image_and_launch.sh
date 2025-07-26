f8239b1ed2bcfac9293b2b1a77c5c364f64d998c
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-07-26 15:29:19 -0600

    More attempts to fix the no space left on device error by mimicking push-image.yml.

--- scripts/train/build_image_and_launch.sh
+++ scripts/train/build_image_and_launch.sh
@@ -4,8 +4,7 @@ set -e
 image_name=open-instruct-dev
 
 # Build and push the Docker image to Beaker
-# Use --ssh default to forward SSH agent for git clone
-DOCKER_BUILDKIT=1 docker build --ssh default -f Dockerfile.uv --build-arg UV_CACHE_DIR=$UV_CACHE_DIR -t $image_name .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.uv --build-arg UV_CACHE_DIR=$UV_CACHE_DIR -t $image_name .
 
 beaker_user=$(beaker account whoami --format json | jq -r '.[0].name')
 
