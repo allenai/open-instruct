7cea66cb1643de45ce081a9c5c6af43965e5784f
Michael Noukhovitch <michaeln@allenai.org>
2025-07-29 03:31:32 -0700

    updated grpo_olmo3
    no add_bos
    olmo_thinker_r1_style template

--- scripts/train/debug/grpo_fast.sh
+++ scripts/train/debug/grpo_fast.sh
@@ -1,4 +1,4 @@
-uv run python open_instruct/grpo_fast.py \
+python open_instruct/grpo_fast.py \
     --dataset_mixer_list ai2-adapt-dev/rlvr_gsm8k_zs 64 \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list ai2-adapt-dev/rlvr_gsm8k_zs 16 \
@@ -8,28 +8,28 @@ uv run python open_instruct/grpo_fast.py \
     --response_length 512 \
     --pack_length 1024 \
     --per_device_train_batch_size 1 \
-    --num_unique_prompts_rollout 8 \
+    --num_unique_prompts_rollout 16 \
     --num_samples_per_prompt_rollout 4 \
     --model_name_or_path Qwen/Qwen3-0.6B \
     --stop_strings "</answer>" \
     --apply_r1_style_format_reward \
     --apply_verifiable_reward true \
-    --temperature 0.7 \
+    --temperature 1.0 \
     --ground_truths_key ground_truth \
     --chat_template_name r1_simple_chat_postpend_think \
     --learning_rate 3e-7 \
-    --total_episodes 200 \
+    --total_episodes 256 \
     --deepspeed_stage 2 \
     --num_epochs 1 \
     --num_learners_per_node 1 \
     --vllm_tensor_parallel_size 1 \
     --beta 0.01 \
     --seed 3 \
-    --num_evals 20 \
+    --num_evals 4 \
     --vllm_sync_backend gloo \
     --vllm_gpu_memory_utilization 0.3 \
     --save_traces \
     --vllm_enforce_eager \
     --gradient_checkpointing \
     --single_gpu_mode \
-    # --with_tracking
+    --push_to_hub False $@
--- scripts/train/rlvr/grpo_olmo3.sh
+++ scripts/train/rlvr/grpo_olmo3.sh
@@ -1,16 +1,36 @@
-exp_name="grpo_fast_olmo3"
+exp_name="grpo_orz_olmo3_fullmix"
+
+# full integration mix
 dataset_mix="saurabh5/rlvr_acecoder 56878 hamishivi/rlvr_orz_math_57k_collected 56878 hamishivi/tulu_3_rewritten_400k_string_f1_only_v2 56878 allenai/IF_multi_constraints_upto5 56878"
-evals="minerva_math::hamish_zs_reasoning,gsm8k::zs_cot_latex,gsm8k::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,aime::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,ifeval::hamish_zs_reasoning,popqa::hamish_zs_reasoning,mmlu:cot::hamish_zs_reasoning,alpaca_eval_v3::hamish_zs_reasoning,bbh:cot::hamish_zs_reasoning,mbppplus:0-shot-chat::tulu-thinker,codex_humanevalplus:0-shot-chat-v1::tulu-thinker"
+# math only mix
+# dataset_mix="hamishivi/rlvr_orz_math_57k_collected 56878"
+
+# all evals
+# evals="minerva_math::hamish_zs_reasoning,gsm8k::zs_cot_latex,gsm8k::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,aime::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,ifeval::hamish_zs_reasoning,popqa::hamish_zs_reasoning,mmlu:cot::hamish_zs_reasoning,alpaca_eval_v3::hamish_zs_reasoning,bbh:cot::hamish_zs_reasoning,mbppplus:0-shot-chat::tulu-thinker,codex_humanevalplus:0-shot-chat-v1::tulu-thinker"
+# math evals
+evals="minerva_math::hamish_zs_reasoning,minerva_math_500::hamish_zs_reasoning,aime::hamish_zs_reasoning"
+
 # all I've changed with the checkpoints is the config.json, model_type=olmo3 and architectures is OLMo3ForCausalLM 
 # jacob tulu sft
 # model_name_or_path="/weka/oe-adapt-default/michaeln/olmo3/olmo3_reasoning-anneal-tulu3sft-olmo2hparams__8__1751523764/"
 # midtraining no reasoning
 # model_name_or_path="/weka/oe-adapt-default/michaeln/olmo3/anneal-round1-100B-olmo3_7b_no-reasoning-anneal-3c193128_step47684"
 # midtraining with reasoning
-model_name_or_path="/weka/oe-adapt-default/michaeln/olmo3/anneal-round1-100B-olmo3_7b_with-reasoning-anneal-9d6f76b0_step47684"
+# model_name_or_path="/weka/oe-adapt-default/michaeln/olmo3/anneal-round1-100B-olmo3_7b_with-reasoning-anneal-9d6f76b0_step47684"
+# micro anneals
+# model_name_or_path="/weka/oe-adapt-default/allennlp/deletable_checkpoint/michaeln/olmo3_microanneal-finemath-643cecc4_step4769-hf"
+# model_name_or_path="/weka/oe-training-default/ai2-llm/checkpoints/OLMo3-midtraining/anneal-round1-100B-olmo3_7b_with-reasoning-anneal-12T-3d39e871/step47684-hf"
+# model_name_or_path="/weka/oe-training-default/ai2-llm/checkpoints/kylel/baseline-olmo2_7b-928646-anneal-100B-dolma2-round1-alldressed-17b22b3a/step47684-hf"
+# model_name_or_path="/weka/oe-training-default/ai2-llm/checkpoints/OLMo3-midtraining/anneal-round2-100B-olmo3_7b_with-reasoning-anneal-12T-53f443c7/step47684-hf"
+model_name_or_path="/weka/oe-training-default/ai2-llm/checkpoints/OLMo3-midtraining/anneal-round3-webround2-100B-olmo3_7b_with-reasoning-anneal-12T-302b1ae8/step47684-hf"
+gs_model_name="olmo3-midtraining-round3"
+# cluster
+cluster=ai2/augusta-google-1
+# cluster=ai2/jupiter-cirrascale-2
+
 python mason.py \
     --task_name ${exp_name} \
-    --cluster ai2/jupiter-cirrascale-2 \
+    --cluster ${cluster} \
     --workspace ai2/tulu-thinker \
     --priority high \
     --pure_docker_mode \
@@ -18,21 +38,20 @@ python mason.py \
     --preemptible \
     --num_nodes 1 \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
+    --gs_model_name $gs_model_name \
     --budget ai2/oe-adapt \
     --gpus 8 -- \
 source configs/beaker_configs/ray_node_setup.sh \&\& \
 source configs/beaker_configs/code_api_setup.sh \&\& \
 python open_instruct/grpo_fast.py \
-    --add_bos True \
     --exp_name ${exp_name} \
     --beta 0.0 \
     --num_samples_per_prompt_rollout 16 \
-    --num_unique_prompts_rollout 128 \
-    --num_mini_batches 2 \
+    --num_unique_prompts_rollout 64 \
+    --num_mini_batches 1 \
     --num_epochs 1 \
     --learning_rate 5e-7 \
     --per_device_train_batch_size 1 \
-    --output_dir overridenbymason \
     --kl_estimator kl3 \
     --dataset_mixer_list ${dataset_mix} \
     --dataset_mixer_list_splits train \
@@ -43,12 +62,12 @@ python open_instruct/grpo_fast.py \
     --response_length 6144 \
     --pack_length 8192 \
     --model_name_or_path ${model_name_or_path} \
-    --chat_template_name tulu_thinker_r1_style \
+    --chat_template_name olmo_thinker_r1_style \
     --stop_strings "</answer>" \
     --non_stop_penalty False \
     --temperature 1.0 \
-    --total_episodes 512000 \
-    --deepspeed_stage 3 \
+    --total_episodes 256000 \
+    --deepspeed_stage 2 \
     --num_learners_per_node 4 \
     --vllm_num_engines 4 \
     --lr_scheduler_type constant \
