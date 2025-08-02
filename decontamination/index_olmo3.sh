baccb1732940de1641cdd32a73776bbee91b4349
saumyamalik <saumyam@allenai.org>
2025-08-01 17:02:30 -0700

    script update

--- decontamination/index_olmo3.sh
+++ decontamination/index_olmo3.sh
@@ -10,11 +10,21 @@ usable_datasets=(
 )
 reasoner_datasets=(
     "allenai/OpenThoughts3-merged-cn-fltrd-final-ngram-filtered-chinese-filtered"
-    "allenai/tulu_v3.9_wildchat_100k_english-r1-final-content-filtered-chinese-filtered"
     "allenai/wildchat-r1-p2-repetition-filter-chinese-filtered"
+    "allenai/persona-precise-if-r1-final-content-filtered-chinese-filtered"
+    "allenai/valpy_if_qwq_reasoning_verified-keyword-filter-datecutoff-chinese-ngram-content-filtered"
+    "allenai/SYNTHETIC-2-SFT-cn-fltrd-final-ngram-filtered-chinese-filtered"
+    "allenai/the-algorithm-python-r1-format-filtered-keyword-filtered-filter-datecutoff"
+    "allenai/acecoder-r1-cn-fltrd-final-ngram-filtered-chinese-filtered"
+    "allenai/rlvr-code-data-python-r1-cn-fltrd-final-ngram-filtered-chinese-filtered"
+    "allenai/numinatmath-r1-format-filtered-keyword-filtered-filter-datecutoff-chinese-filtered"
 )
 
-for dataset in "${usable_datasets[@]}"; do
+python search.py --train_dataset_names "${usable_datasets[@]}" --ngram_size 8 --output_dir=olmo3_usable_decontam --match_threshold 0.5 --decontaminate --search_size 10000 
+
+for dataset in "${reasoner_datasets[@]}"; do
     echo "Processing dataset: $dataset"
     python index.py --dataset "$dataset"
 done
+
+python search.py --train_dataset_names "${reasoner_datasets[@]}" --ngram_size 8 --output_dir=olmo3_usable_decontam --match_threshold 0.5 --decontaminate --search_size 10000 
