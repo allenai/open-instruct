f7527fce3f4964a58403359f63ea3976d7ad2f27
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-08-11 13:19:18 -0600

    Updated script to include git hash.

--- scripts/train/build_image_and_launch.sh
+++ scripts/train/build_image_and_launch.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 # Get the current git commit hash (short version)
-git_hash=$(git rev-parse --short HEAD)
-image_name=open-instruct-integration-test-${git_hash}
+git_commit=$(git rev-parse --short HEAD)
+image_name=open-instruct-integration-test-$git_commit
 
 # Build the Docker image exactly like push-image.yml does
-docker build --platform=linux/amd64 . -t $image_name
+docker build --platform linux/amd64 . -t $image_name
 
 beaker_user=$(beaker account whoami --format json | jq -r '.[0].name')
 
