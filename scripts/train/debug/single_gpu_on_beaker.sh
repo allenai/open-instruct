02097dca4dc67fead9ea786cf6bff15f67d30de5
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-08-11 10:39:39 -0600

    Another tweak.

--- scripts/train/debug/single_gpu_on_beaker.sh
+++ scripts/train/debug/single_gpu_on_beaker.sh
@@ -52,7 +52,7 @@ uv run python mason.py \
     --seed 3 \
     --local_eval_every 1 \
     --vllm_sync_backend gloo \
-    --vllm_gpu_memory_utilization 0.3 \
+    --vllm_gpu_memory_utilization 0.2 \
     --save_traces \
     --vllm_enforce_eager \
     --gradient_checkpointing \
