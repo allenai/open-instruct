f330f83df7bf6ab2cc4b811d132a8ad3bf9e8a4e
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-08-12 12:30:00 -0600

    Clean up

--- scripts/train/build_image_and_launch.sh
+++ scripts/train/build_image_and_launch.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 # Get the current git commit hash (short version)
-git_commit=$(git rev-parse --short HEAD)
-image_name=open-instruct-integration-test-$git_commit
+git_hash=$(git rev-parse --short HEAD)
+image_name=open-instruct-integration-test-${git_hash}
 
 # Build the Docker image exactly like push-image.yml does
-docker build --platform linux/amd64 . -t $image_name
+docker build . -t "$image_name"
 
 beaker_user=$(beaker account whoami --format json | jq -r '.[0].name')
 
