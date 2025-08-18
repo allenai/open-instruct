4156bb1dc17928b798af58dd2b24a4a5887ee58a
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-08-18 09:18:11 -0600

    Set total episodes back to 10k.

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -42,7 +42,7 @@ uv run python mason.py \
         --temperature 1.0 \
         --ground_truths_key ground_truth \
         --sft_messages_key messages \
-        --total_episodes 10_0 \
+        --total_episodes 10_000 \
         --deepspeed_stage 2 \
         --num_learners_per_node 8 \
         --vllm_num_engines 8 \
