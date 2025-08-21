4e43ea4797a8c3f2c3df5360eee85879b78aafc2
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-08-21 15:51:31 -0600

    Adds a progress bar to the Wandb description (#929)
    * Added a progress bar to the description.
    
    * Fixed bug.
    
    * Updated scripts.
    
    * Cleaned up format_eta.
    
    * Updated code.
    
    * Removed some logging.
    
    * Now, we have a single function that touches the beaker description.
    
    * Fixed bug in code.
    
    * CLeaned up pr.
    
    * Cleaned up code.
    
    * Cleaned up code.
    
    * Fixed bug in description.
    
    * Remove wandb conditional.

--- open_instruct/ppo_fast.py
+++ open_instruct/ppo_fast.py
@@ -123,7 +123,7 @@ from open_instruct.utils import (
     is_beaker_job,
     launch_ai2_evals_on_weka,
     maybe_get_beaker_config,
-    maybe_update_beaker_description_with_wandb_url,
+    maybe_update_beaker_description,
     maybe_use_ai2_hf_entity,
     maybe_use_ai2_wandb_entity,
 )
@@ -1599,7 +1599,7 @@ def main(args: Args, tc: TokenizerConfig, model_config: ModelConfig, reward_fn: 
     inits = []
     policy_group = ModelGroup(pg, PolicyTrainerRayProcess, args.num_learners_per_node, args.single_gpu_mode)
     wandb_url = wandb.run.get_url() if args.with_tracking else None
-    maybe_update_beaker_description_with_wandb_url(wandb_url)
+    maybe_update_beaker_description(wandb_url=wandb_url)
     inits.extend(
         model.from_pretrained.remote(args, model_config, beaker_config, wandb_url, tokenizer)
         for model in policy_group.models
@@ -1735,8 +1735,22 @@ def main(args: Args, tc: TokenizerConfig, model_config: ModelConfig, reward_fn: 
 
     num_total_tokens = 0
     start_time = time.time()
+    training_start_time = time.time()  # Track overall training start time
     try:
         for training_step in range(resume_training_step, args.num_training_steps + 1):
+            # Update Beaker progress every 10 steps or on first/last step
+            if (
+                training_step == resume_training_step
+                or training_step % 10 == 0
+                or training_step == args.num_training_steps
+            ):
+                maybe_update_beaker_description(
+                    current_step=training_step,
+                    total_steps=args.num_training_steps,
+                    start_time=training_start_time,
+                    wandb_url=wandb_url,
+                )
+
             print("-" * 100)
             episode += (
                 args.num_unique_prompts_rollout * args.num_samples_per_prompt_rollout
