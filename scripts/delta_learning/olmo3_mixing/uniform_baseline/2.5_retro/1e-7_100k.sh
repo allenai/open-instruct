1a88ba4eadaa0964756eee54c421a89bb6ea53d4
Scott Geng <scottg@allenai.org>
2025-08-31 21:25:32 -0700

    delta learning olmo prompt scripts

---
+++ scripts/delta_learning/olmo3_mixing/uniform_baseline/2.5_retro/1e-7_100k.sh
@@ -0,0 +1,40 @@
+MODEL_NAME=/weka/oe-adapt-default/jacobm/checkpoints/olmo2-7B-sft/olmo3-hparam-search/olmo2.5-6T-R5-NO_LR_FIX-olmo2-tulu3-mix-num_3
+# MODEL_NAME=/weka/oe-adapt-default/jacobm/checkpoints/olmo2-7B-sft/usable-tulu/olmo2-7B-FINAL-LC-olmo2-tulu3-mix-num_3
+python /weka/oe-adapt-default/scottg/olmo/open-instruct/mason.py \
+    --cluster ai2/jupiter-cirrascale-2 ai2/ceres-cirrascale \
+    --workspace ai2/usable-olmo \
+    --priority high \
+    --image michaeln/open_instruct_olmo2_retrofit --pure_docker_mode \
+    --preemptible \
+    --num_nodes 2 \
+    --budget ai2/oe-base \
+    --no_auto_dataset_cache \
+    --gpus 8 -- accelerate launch \
+    --mixed_precision bf16 \
+    --num_processes 8 \
+    --use_deepspeed \
+    --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
+    --deepspeed_multinode_launcher standard \
+    open_instruct/dpo_tune_cache.py \
+    --exp_name retrojm-dpo-olmo3-qwen3delta-unif-lr1e-7_100k \
+    --model_name_or_path $MODEL_NAME \
+    --tokenizer_name $MODEL_NAME \
+    --use_slow_tokenizer False \
+    --dataset_mixer_list scottgeng00/olmo-3-preference-mix-deltas 1.0 \
+    --max_seq_length 2048 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 1e-7 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.1 \
+    --weight_decay 0.0 \
+    --num_train_epochs 1 \
+    --logging_steps 1 \
+    --dpo_loss_type dpo_norm \
+    --dpo_beta 5 \
+    --use_flash_attn \
+    --gradient_checkpointing \
+    --report_to wandb \
+    --chat_template_name olmo \
+    --with_tracking \
+    --max_train_samples 100000
