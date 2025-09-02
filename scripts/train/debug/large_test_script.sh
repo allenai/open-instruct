ce88c01850f5a28f41e5ac3b42c2b2812e1007f7
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-02 10:46:11 -0600

    Added preemptible flag

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -10,6 +10,7 @@ uv run python mason.py \
 	--pure_docker_mode \
         --workspace ai2/open-instruct-dev \
         --priority urgent \
+	--preemptible \
         --num_nodes 2 \
         --description "rlvr ace fn and og ocr stdio from base with perf penalty" \
         --max_retries 0 \
