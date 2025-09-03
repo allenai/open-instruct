81078bf575eef3faec0284422d2eda4c767af0c7
saumyamalik <saumyam@allenai.org>
2025-09-02 17:51:10 -0700

    script

--- parse_preferences.sh
+++ parse_preferences.sh
@@ -6,4 +6,19 @@ python mason.py \
     --preemptible \
     --num_nodes 1 \
     --budget ai2/oe-adapt \
-    --gpus 1 -- python scripts/data/preferences/olmo3/extract_responses.py
+    --gpus 1 -- python scripts/data/preferences/olmo3/extract_responses.py --parsed /weka/oe-adapt-default/jacobm/rl-sft/olmo3-preferences/grades/workspaces/qwq/32b/parsed_judgments_combined.jsonl \
+    --selected /weka/oe-adapt-default/jacobm/rl-sft/olmo3-preferences/selected_models_FINAL.jsonl \
+    --combined-dir /weka/oe-adapt-default/jacobm/rl-sft/olmo3-preferences/combined-outputs \
+
+python mason.py \
+    --cluster ai2/neptune-cirrascale \
+    --workspace ai2/usable-olmo \
+    --priority normal \
+    --image nathanl/rewardbench_auto \
+    --preemptible \
+    --num_nodes 1 \
+    --budget ai2/oe-adapt \
+    --gpus 1 -- python scripts/data/preferences/olmo3/extract_responses.py --parsed /weka/oe-adapt-default/jacobm/rl-sft/olmo3-preferences/grades/workspaces/gemma3/27b/parsed_judgments_combined.jsonl \
+    --selected /weka/oe-adapt-default/jacobm/rl-sft/olmo3-preferences/selected_models_FINAL.jsonl \
+    --combined-dir /weka/oe-adapt-default/jacobm/rl-sft/olmo3-preferences/combined-outputs \
+    
