8f7c17f50e4712732b4d249fa84f004e3bdcd899
Scott Geng <scottg@allenai.org>
2025-09-04 10:40:50 -0700

    more exps

---
+++ scripts/delta_learning/olmo3_mixing/chat35base_plus_domain/add_code50k_5e-7.sh
@@ -0,0 +1,40 @@
+MODEL_NAME=/weka/oe-adapt-default/jacobm/checkpoints/olmo2-7B-sft/olmo3-hparam-search/olmo2.5-6T-R5-NO_LR_FIX-olmo2-tulu3-mix-num_3
+# MODEL_NAME=/weka/oe-adapt-default/jacobm/checkpoints/olmo2-7B-sft/usable-tulu/olmo2-7B-FINAL-LC-olmo2-tulu3-mix-num_3
+python /weka/oe-adapt-default/scottg/olmo/open-instruct/mason.py \
+    --cluster ai2/jupiter-cirrascale-2 ai2/ceres-cirrascale ai2/saturn-cirrascale \
+    --workspace ai2/usable-olmo \
+    --priority high \
+    --image scottg/open_instruct_dev --pure_docker_mode \
+    --preemptible \
+    --num_nodes 1 \
+    --budget ai2/oe-base \
+    --no_auto_dataset_cache \
+    --gpus 8 -- accelerate launch \
+    --mixed_precision bf16 \
+    --num_processes 8 \
+    --use_deepspeed \
+    --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
+    --deepspeed_multinode_launcher standard \
+    open_instruct/dpo_tune_cache.py \
+    --exp_name retrojm-dpo-deltamix-add_code50k_lr5e-7 \
+    --model_name_or_path $MODEL_NAME \
+    --tokenizer_name $MODEL_NAME \
+    --use_slow_tokenizer False \
+    --dataset_mixer_list scottgeng00/olmo-3-preference-mix-deltas-100k_base 1.0 \
+        scottgeng00/olmo-3-preference-mix-deltas-100k_base_complement-only_code 50000 \
+    --max_seq_length 2048 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 5e-7 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.1 \
+    --weight_decay 0.0 \
+    --num_train_epochs 1 \
+    --logging_steps 1 \
+    --dpo_loss_type dpo_norm \
+    --dpo_beta 5 \
+    --use_flash_attn \
+    --gradient_checkpointing \
+    --report_to wandb \
+    --chat_template_name olmo \
+    --with_tracking
