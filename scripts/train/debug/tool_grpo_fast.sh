02a403b00e2757396d1c7bffb141acc7e82301c4
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-12 15:06:30 -0600

    Undid host network change to tool_grpo_fast.sh.

--- scripts/train/debug/tool_grpo_fast.sh
+++ scripts/train/debug/tool_grpo_fast.sh
@@ -23,6 +23,7 @@ uv run python mason.py \
        --max_retries 0 \
        --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
        --env GIT_COMMIT="$(git rev-parse --short HEAD)" \
+       --no-host_network \
        --budget ai2/oe-adapt \
        --gpus 1 \
 	   -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
