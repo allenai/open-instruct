e83fb31e73f6ac132f0030ee06902394d7ac77bd
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-16 12:24:28 -0600

    updated script

--- scripts/train/debug/single_gpu_on_beaker.sh
+++ scripts/train/debug/single_gpu_on_beaker.sh
@@ -14,6 +14,7 @@ uv run python mason.py \
        --description "Single GPU on Beaker test script." \
        --pure_docker_mode \
        --workspace ai2/open-instruct-dev \
+       --non_resumable \
        --preemptible \
        --priority urgent \
        --num_nodes 1 \
