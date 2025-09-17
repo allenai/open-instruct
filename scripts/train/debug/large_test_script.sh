6658cc1d32e28c30d3e4a7e377308f3882bf96a3
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-17 09:12:47 -0600

    Fixed logging levels

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -16,7 +16,6 @@ uv run python mason.py \
 	--description "Large (multi-node) test script." \
         --max_retries 0 \
         --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
-	--env VLLM_LOGGING_LEVEL=WARNING \
         --budget ai2/oe-adapt \
         --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& source configs/beaker_configs/code_api_setup.sh \&\&python open_instruct/grpo_fast.py \
         --exp_name ${exp_name} \
