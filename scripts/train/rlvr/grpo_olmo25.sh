f18842513a6f7bf12028de90a77c909df906e111
Michael Noukhovitch <michaeln@allenai.org>
2025-09-19 22:07:19 +0000

    shorter run

--- scripts/train/rlvr/grpo_olmo25.sh
+++ scripts/train/rlvr/grpo_olmo25.sh
@@ -6,6 +6,7 @@ GS_MODEL_NAME="olmo25_7b_lc_beta_740666e3"
 
 # english only DAPO
 DATASETS="mnoukhov/DAPO-Math-14k-Processed-RLVR 1.0"
+# DATASETS="TTTXXX01/MATH_3000_Filtered 1.0"
 
 # math evals
 EVALS="minerva_math::hamish_zs_reasoning_deepseek,minerva_math_500::hamish_zs_reasoning_deepseek,aime:zs_cot_r1::pass_at_32_2024_deepseek,aime:zs_cot_r1::pass_at_32_2025_deepseek"
@@ -13,8 +14,9 @@ EVALS="minerva_math::hamish_zs_reasoning_deepseek,minerva_math_500::hamish_zs_re
 # AIME 2024, 2025 local evals
 LOCAL_EVALS="mnoukhov/aime2024-25-rlvr 1.0 mnoukhov/aime2024-25-rlvr 1.0"
 LOCAL_EVAL_SPLITS="test_2024 test_2024 test_2025 test_2025"
-
-EXP_NAME="grpo_dapo14k_${gs_model_name}"
+# tengmath3k
+EXP_NAME="grpo_tengmath3k_k16_${GS_MODEL_NAME}"
+# EXP_NAME="grpo_dapo14k_${GS_MODEL_NAME}"
 
 cluster=ai2/augusta
 
@@ -26,7 +28,7 @@ python mason.py \
     --pure_docker_mode \
     --image ${1:-michaeln/open_instruct_olmo2_retrofit} \
     --preemptible \
-    --num_nodes 6 \
+    --num_nodes 4 \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --env VLLM_ATTENTION_BACKEND="FLASH_ATTN" \
     --gs_model_name $GS_MODEL_NAME \
@@ -39,9 +41,8 @@ python open_instruct/grpo_fast.py \
     --exp_name ${EXP_NAME} \
     --beta 0.0 \
     --num_samples_per_prompt_rollout 16 \
-    --num_unique_prompts_rollout 128 \
+    --num_unique_prompts_rollout 24 \
     --num_mini_batches 4 \
-    --num_epochs 2 \
     --learning_rate 1e-6 \
     --per_device_train_batch_size 1 \
     --kl_estimator kl3 \
@@ -49,26 +50,26 @@ python open_instruct/grpo_fast.py \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list $LOCAL_EVALS \
     --dataset_mixer_eval_list_splits $LOCAL_EVAL_SPLITS \
-    --max_token_length 18432 \
+    --max_token_length 2048 \
     --max_prompt_token_length 2048 \
-    --response_length 16384 \
+    --response_length 8192 \
     --pack_length 32768 \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --chat_template_name olmo_thinker_r1_style_nochat \
     --stop_strings "</answer>" \
     --non_stop_penalty False \
     --temperature 1.0 \
-    --total_episodes 2048000 \
+    --total_episodes 192000 \
     --deepspeed_stage 3 \
     --num_learners_per_node 8 \
-    --vllm_num_engines 40 \
+    --vllm_num_engines 24 \
     --vllm_tensor_parallel_size 1 \
     --lr_scheduler_type constant \
     --apply_verifiable_reward true \
     --seed 1 \
-    --local_eval_every 25 \
-    --save_freq 25 \
-    --checkpoint_state_freq 25 \
+    --local_eval_every 50 \
+    --save_freq 50 \
+    --checkpoint_state_freq 50 \
     --gradient_checkpointing \
     --with_tracking \
     --vllm_enable_prefix_caching \
