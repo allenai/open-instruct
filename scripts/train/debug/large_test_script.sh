2f917ded07e53888efadac7835dbf6aba2d933d7
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-24 13:38:48 -0600

    Use v1 engine

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -17,6 +17,7 @@ uv run python mason.py \
         --max_retries 0 \
         --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
 	--env VLLM_LOGGING_LEVEL=DEBUG \
+	--env VLLM_USE_V1=1 \
 	--budget ai2/oe-adapt \
         --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& source configs/beaker_configs/code_api_setup.sh \&\&python open_instruct/grpo_fast.py \
         --exp_name ${exp_name} \
