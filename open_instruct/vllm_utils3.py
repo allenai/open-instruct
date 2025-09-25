125a41c05fa538169e3bdc76846e75331d98854c
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-25 09:28:36 -0600

    Removed vllm_enable_V1_multiprocessing.

--- open_instruct/vllm_utils3.py
+++ open_instruct/vllm_utils3.py
@@ -1143,9 +1143,7 @@ def create_vllm_engines(
                 num_gpus=num_gpus,
                 scheduling_strategy=scheduling_strategy,
                 # VLLM v1 multiprocessing is required due to https://github.com/vllm-project/vllm/issues/15349
-                runtime_env=ray.runtime_env.RuntimeEnv(
-                    env_vars={"VLLM_ENABLE_V1_MULTIPROCESSING": "0", "TORCH_CUDA_ARCH_LIST": get_cuda_arch_list()}
-                ),
+                runtime_env=ray.runtime_env.RuntimeEnv(env_vars={"TORCH_CUDA_ARCH_LIST": get_cuda_arch_list()}),
             )
             .remote(
                 model=pretrain,
