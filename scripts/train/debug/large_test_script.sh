8205c7ba0d404b9cdfa533ce9277f1b2638684e8
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-25 12:54:11 -0600

    Set vllm_enforce_eager to large_test_script.

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -60,6 +60,7 @@ uv run python mason.py \
         --with_tracking \
 	--update_progress_every 1 \
         --vllm_enable_prefix_caching \
+        --vllm_enforce_eager \
         --oe_eval_max_length 32768 \
         --oe_eval_tasks "codex_humanevalplus:0-shot-chat-v1::tulu-thinker,mbppplus:0-shot-chat::tulu-thinker,livecodebench_codegeneration::tulu-thinker" \
         --dataset_skip_cache True \
