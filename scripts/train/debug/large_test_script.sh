898c04f1c848b334830fa7b8d22b46135a2338f2
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-09-25 13:42:18 -0600

    Set inflight updates to False

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -38,7 +38,7 @@ uv run python mason.py \
         --pack_length 20480 \
         --model_name_or_path Qwen/Qwen2.5-7B \
         --chat_template_name tulu_thinker \
-	--inflight_updates True \
+	--inflight_updates False \
         --stop_strings "</answer>" \
         --non_stop_penalty False \
         --temperature 1.0 \
