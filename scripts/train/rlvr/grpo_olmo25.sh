0c2a1c630c6f06f0ed227ecb25e532471b3b1097
Michael Noukhovitch <michaeln@allenai.org>
2025-09-27 05:56:07 +0000

    new hyperparams

--- scripts/train/rlvr/grpo_olmo25.sh
+++ scripts/train/rlvr/grpo_olmo25.sh
@@ -6,25 +6,27 @@ GS_MODEL_NAME="olmo25_7b_lc_final_fb13a737"
 
 # english only DAPO
 DATASETS="mnoukhov/DAPO-Math-14k-Processed-RLVR 1.0 TTTXXX01/MATH_3000_Filtered 1.0"
-# DATASETS="TTTXXX01/MATH_3000_Filtered 1.0"
+# DATASETS="mnoukhov/deepscaler_20k_medhard_nolatex_rlvr 1.0"
+# DATASETS=""
 
 # math evals
-EVALS="minerva_math_500::hamish_zs_reasoning_deepseek,aime:zs_cot_r1::pass_at_32_2024_deepseek,aime:zs_cot_r1::pass_at_32_2025_deepseek"
+# EVALS="minerva_math_500::hamish_zs_reasoning_deepseek"
+EVALS="aime:zs_cot_r1::pass_at_32_2024_dapo,aime:zs_cot_r1::pass_at_32_2025_dapo"
 
 # AIME 2024, 2025 local evals
 LOCAL_EVALS="mnoukhov/aime2024-25-rlvr 1.0 mnoukhov/aime2024-25-rlvr 1.0"
 LOCAL_EVAL_SPLITS="test_2024 test_2024 test_2025 test_2025"
 # tengmath3k
-EXP_NAME="grpo_dapoteng17k_k8_${GS_MODEL_NAME}"
-# EXP_NAME="grpo_dapo14k_${GS_MODEL_NAME}"
+# EXP_NAME="grpo_deepscaler20k_k8_${GS_MODEL_NAME}"
+EXP_NAME="grpo_dapoteng17k_3e6_${GS_MODEL_NAME}"
 
 cluster=ai2/augusta
 
 python mason.py \
     --task_name ${EXP_NAME} \
     --cluster ${cluster} \
-    --workspace ai2/oe-adapt-code \
-    --priority high \
+    --workspace ai2/olmo-instruct \
+    --priority urgent \
     --pure_docker_mode \
     --image michaeln/open_instruct_olmo2_retrofit \
     --preemptible \
@@ -40,10 +42,11 @@ source configs/beaker_configs/code_api_setup.sh \&\& \
 python open_instruct/grpo_fast.py \
     --exp_name ${EXP_NAME} \
     --beta 0.0 \
-    --num_samples_per_prompt_rollout 8 \
+    --num_samples_per_prompt_rollout 16 \
     --num_unique_prompts_rollout 32 \
     --num_mini_batches 1 \
-    --learning_rate 1e-6 \
+    --inflight_updates \
+    --learning_rate 3e-6 \
     --per_device_train_batch_size 1 \
     --kl_estimator kl3 \
     --dataset_mixer_list $DATASETS \
@@ -53,13 +56,13 @@ python open_instruct/grpo_fast.py \
     --max_token_length 2048 \
     --max_prompt_token_length 2048 \
     --response_length 16384 \
-    --pack_length 32768 \
+    --pack_length 32000 \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
-    --chat_template_name olmo_thinker_r1_style_nochat \
+    --chat_template_name olmo_thinker_dapo \
     --stop_strings "</answer>" \
     --non_stop_penalty False \
     --temperature 1.0 \
-    --total_episodes 256000 \
+    --total_episodes 512000 \
     --deepspeed_stage 3 \
     --num_learners_per_node 8 \
     --vllm_num_engines 32 \
@@ -67,15 +70,17 @@ python open_instruct/grpo_fast.py \
     --lr_scheduler_type constant \
     --apply_verifiable_reward true \
     --seed 1 \
-    --local_eval_every 100 \
-    --save_freq 100 \
+    --local_eval_every 25 \
+    --save_freq 25 \
     --checkpoint_state_freq 100 \
     --gradient_checkpointing \
     --with_tracking \
     --vllm_enable_prefix_caching \
     --clip_higher 0.272 \
-    --mask_truncated_completions True \
+    --mask_truncated_completions False \
     --oe_eval_max_length 32000 \
     --try_launch_beaker_eval_jobs_on_weka True \
+    --eval_priority high \
     --oe_eval_tasks $EVALS \
-    --oe_eval_beaker_image oe-eval-beaker/oe_eval_olmo2_retrofit_auto
+    --oe_eval_gpu_multiplier 4 \
+    --oe_eval_beaker_image michaeln/oe_eval_olmo2_retrofit
