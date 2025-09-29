7220b0ee880fc2ee16fc36dfb17e5a4da13cd2a2
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-09-29 13:09:44 -0700

    pip

--- run_eval_models.sh
+++ run_eval_models.sh
@@ -46,6 +46,7 @@ for i in "${!MODEL_NAMES[@]}"; do
         --beaker_image jacobm/flex-olmo-oe-eval-vllm \
         --skip_oi_evals \
         --gpu_multiplier 2 \
+        --install "pip install --no-cache-dir 'git+https://github.com/huggingface/transformers.git@main'" \
         --oe_eval_tasks mmlu:cot::hamish_zs_reasoning,popqa::hamish_zs_reasoning,simpleqa::tulu-thinker,bbh:cot::hamish_zs_reasoning,gpqa:0shot_cot::hamish_zs_reasoning,zebralogic::hamish_zs_reasoning,agi_eval_english:0shot_cot::hamish_zs_reasoning,minerva_math::hamish_zs_reasoning,gsm8k::zs_cot_latex,aime:zs_cot_r1::pass_at_32_2024_temp1,aime:zs_cot_r1::pass_at_32_2025_temp1,codex_humanevalplus:0-shot-chat::tulu-thinker,mbppplus:0-shot-chat::tulu-thinker,alpaca_eval_v3::hamish_zs_reasoning,ifeval::hamish_zs_reasoning \
         --s3_output_dir s3://ai2-sewonm/sanjaya/post_training_eval_results/${MODEL_NAME}/ \
         --location $MODEL_PATH \
