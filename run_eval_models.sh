0834599cccdb9de37889cca6b2de817fc24ba0d6
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-10-01 03:21:21 -0700

    router sft experts sft

--- run_eval_models.sh
+++ run_eval_models.sh
@@ -3,11 +3,11 @@
 # Define arrays for model names and paths
 PY="/opt/anaconda3/envs/python310/bin/python"
 MODEL_NAMES=(
-    "merged-RT-experts-sft-math-mixed-2-experts"
+    "merged-RT-router-sftexperts-sft-math-mixed"
 )
 
 MODEL_PATHS=(
-    "/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7b-flex-base-merged-math-code-RT-experts-sft-math-mixed-2-experts/step9537-hf"
+    "/weka/oe-training-default/sanjaya/flexolmo/checkpoints/router-sft-experts-sft-mixed/step1212-hf"
 )
 
 # Check that arrays have the same length
