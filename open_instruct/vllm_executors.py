4239ce9161cad14a37ff8964c5d5bf34b2682076
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-02 12:24:05 -0600

    Fixed issue

--- open_instruct/vllm_executors.py
+++ open_instruct/vllm_executors.py
@@ -14,6 +14,7 @@ class PauseExecutorMixin:
         super().__init__(*args, **kwargs)
         self._pause_lock: Optional[asyncio.Lock] = None
         self._pause_requested: bool = False
+        self._pause_lock_owned_by_pause: bool = False
         self._pause_ack_event: Optional[asyncio.Event] = None
         self._pause_resume_event: Optional[asyncio.Event] = None
 
@@ -77,24 +78,28 @@ class PauseExecutorMixin:
 
         if not lock.locked():
             await lock.acquire()
-            try:
-                await self.stop_remote_worker_execution_loop_no_lock()
-            finally:
-                ack_event.set()
-                lock.release()
+            await self.stop_remote_worker_execution_loop_no_lock()
+            self._pause_requested = False
+            self._pause_lock_owned_by_pause = True
+            ack_event.set()
             return
 
         self._pause_requested = True
         await ack_event.wait()
+        ack_event.clear()
 
     async def acquire_pause_lock(self) -> None:
         await self._ensure_sync_primitives()
         lock = self._pause_lock
+        if self._pause_lock_owned_by_pause:
+            self._pause_lock_owned_by_pause = False
+            return
         await lock.acquire()
 
     async def release_pause_lock(self) -> None:
         await self._ensure_sync_primitives()
         resume_event = self._pause_resume_event
+        self._pause_lock_owned_by_pause = False
         resume_event.set()
         lock = self._pause_lock
         if lock.locked():
