361618cd43e5ec9342a6d988baa7bffb42bf23de
Michael Noukhovitch <michaeln@allenai.org>
2025-10-02 05:39:40 +0000

    final script maybe

--- scripts/train/rlvr/grpo_olmo25.sh
+++ scripts/train/rlvr/grpo_olmo25.sh
@@ -5,7 +5,8 @@ MODEL_NAME_OR_PATH="/weka/oe-training-default/ai2-llm/checkpoints/tylerr/long-co
 GS_MODEL_NAME="olmo25_7b_lc_final_fb13a737"
 
 # english only DAPO
-DATASETS="mnoukhov/DAPO-Math-14k-Processed-RLVR 1.0 TTTXXX01/MATH_3000_Filtered 1.0"
+# DATASETS="mnoukhov/DAPO-Math-14k-Processed-RLVR 1.0 TTTXXX01/MATH_3000_Filtered 1.0"
+DATASETS="saurabh5/DAPO-Math-17k-Processed_filtered_olmo_completions_new_template_filtered 1.0 saurabh5/MATH_3000_Filtered_olmo_completions_new_template_filtered 1.0"
 # DATASETS="mnoukhov/deepscaler_20k_medhard_nolatex_rlvr 1.0"
 # DATASETS=""
 
@@ -18,7 +19,7 @@ LOCAL_EVALS="mnoukhov/aime2024-25-rlvr 1.0 mnoukhov/aime2024-25-rlvr 1.0"
 LOCAL_EVAL_SPLITS="test_2024 test_2024 test_2025 test_2025"
 # tengmath3k
 # EXP_NAME="grpo_deepscaler20k_k8_${GS_MODEL_NAME}"
-EXP_NAME="grpo_dapoteng17k_3e6_${GS_MODEL_NAME}"
+EXP_NAME="grpo_dapoteng17kfilter_${GS_MODEL_NAME}"
 
 cluster=ai2/augusta
 
@@ -28,9 +29,9 @@ python mason.py \
     --workspace ai2/olmo-instruct \
     --priority urgent \
     --pure_docker_mode \
-    --image michaeln/open_instruct_olmo2_retrofit \
+    --image michaeln/open_instruct_2.5-rl0 \
     --preemptible \
-    --num_nodes 5 \
+    --num_nodes 9 \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --env VLLM_ATTENTION_BACKEND="FLASH_ATTN" \
     --gs_model_name $GS_MODEL_NAME \
@@ -42,11 +43,10 @@ source configs/beaker_configs/code_api_setup.sh \&\& \
 python open_instruct/grpo_fast.py \
     --exp_name ${EXP_NAME} \
     --beta 0.0 \
-    --num_samples_per_prompt_rollout 16 \
-    --num_unique_prompts_rollout 32 \
+    --num_samples_per_prompt_rollout 8 \
+    --num_unique_prompts_rollout 64 \
     --num_mini_batches 1 \
-    --inflight_updates \
-    --learning_rate 3e-6 \
+    --learning_rate 1e-6 \
     --per_device_train_batch_size 1 \
     --kl_estimator kl3 \
     --dataset_mixer_list $DATASETS \
@@ -56,29 +56,28 @@ python open_instruct/grpo_fast.py \
     --max_token_length 2048 \
     --max_prompt_token_length 2048 \
     --response_length 16384 \
-    --pack_length 32000 \
+    --pack_length 32768 \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --chat_template_name olmo_thinker_dapo \
-    --stop_strings "</answer>" \
     --non_stop_penalty False \
     --temperature 1.0 \
     --total_episodes 512000 \
     --deepspeed_stage 3 \
     --num_learners_per_node 8 \
-    --vllm_num_engines 32 \
+    --vllm_num_engines 64 \
     --vllm_tensor_parallel_size 1 \
     --lr_scheduler_type constant \
     --apply_verifiable_reward true \
     --seed 1 \
-    --local_eval_every 25 \
-    --save_freq 25 \
+    --local_eval_every 100 \
+    --save_freq 100 \
     --checkpoint_state_freq 100 \
     --gradient_checkpointing \
     --with_tracking \
     --vllm_enable_prefix_caching \
     --clip_higher 0.272 \
-    --mask_truncated_completions False \
-    --oe_eval_max_length 32000 \
+    --mask_truncated_completions True \
+    --oe_eval_max_length 32768 \
     --try_launch_beaker_eval_jobs_on_weka True \
     --eval_priority high \
     --oe_eval_tasks $EVALS \
