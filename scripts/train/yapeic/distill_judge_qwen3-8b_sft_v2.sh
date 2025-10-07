d4a9dd40e9fc35fe2ce01239fdcde18e611fb467
lilakk <monku1002@gmail.com>
2025-10-06 19:23:55 -0700

    v2 data

--- scripts/train/yapeic/distill_judge_qwen3-8b_sft_v2.sh
+++ scripts/train/yapeic/distill_judge_qwen3-8b_sft_v2.sh
@@ -1,7 +1,7 @@
 python mason.py \
     --cluster ai2/jupiter \
-    --task_name distill_judge_qwen3-8b_sft \
-    --description "distill judge into Qwen3-8B via SFT" \
+    --task_name distill_judge_qwen3-8b_sft_v2 \
+    --description "distill judge into Qwen3-8B via SFT (v2 data)" \
     --workspace ai2/oe-data \
     --priority high \
     --image nathanl/open_instruct_auto \
@@ -16,24 +16,25 @@ python mason.py \
     --deepspeed_multinode_launcher standard \
     open_instruct/finetune.py \
     --hf_entity yapeichang \
-    --hf_repo_id distill_judge_qwen3-8b_sft \
-    --exp_name distill_judge_qwen3-8b_sft \
+    --hf_repo_id distill_judge_qwen3-8b_sft_v2 \
+    --exp_name distill_judge_qwen3-8b_sft_v2 \
     --model_name_or_path Qwen/Qwen3-8B \
     --model_revision main \
     --tokenizer_name Qwen/Qwen3-8B \
     --tokenizer_revision main \
     --use_slow_tokenizer \
-    --dataset_mixer_list /weka/oe-training-default/yapeic/proc-data/data/distillation/gpt-5_v5_train_15000.jsonl 1.0 \
+    --dataset_transform_fn sft_qwen3_tokenize_and_truncate_no_thinking_v1 sft_tulu_filter_v1 \
+    --dataset_mixer_list /weka/oe-training-default/yapeic/proc-data/data/distillation_v2/gpt-5_v5_train_42621.jsonl 1.0 \
     --max_seq_length 8192 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 2 \
     --learning_rate 5e-06 \
     --lr_scheduler_type linear \
     --warmup_ratio 0.03 \
     --weight_decay 0.0 \
-    --num_train_epochs 5 \
+    --num_train_epochs 3 \
     --checkpointing_steps epoch \
-    --keep_last_n_checkpoints 5 \
+    --keep_last_n_checkpoints 3 \
     --use_flash_attn \
     --gradient_checkpointing \
     --report_to wandb \
