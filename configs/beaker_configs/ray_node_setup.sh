9dbaa1a78497781cde1e9da23a77637b10ad0d7e
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-09 14:15:33 -0600

    Creates triton cache, silencing warning

--- configs/beaker_configs/ray_node_setup.sh
+++ configs/beaker_configs/ray_node_setup.sh
@@ -15,6 +15,7 @@ echo PATH=$PATH
 BEAKER_LEADER_REPLICA_IP=$(getent hosts ${BEAKER_LEADER_REPLICA_HOSTNAME} | awk '{print $1}')
 
 RAY_NODE_PORT=8888
+mkdir -p /root/.triton/autotune
 ray stop --force
 
 if [ "$BEAKER_REPLICA_RANK" == "0" ]; then
