2bbff11ffce13bbf3a0e59961d135bb9b929c151
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-09 15:45:01 -0600

    Added tests

---
+++ open_instruct/test_math_utils.py
@@ -0,0 +1,19 @@
+import unittest
+
+import parameterized
+
+import open_instruct.math_utils
+
+
+class TestStripString(unittest.TestCase):
+    @parameterized.parameterized.expand([
+        (r"50\% of the total", "50 of the total"),
+        (r"50% of the total", "50% of the total"),
+    ])
+    def test_percentage_handling(self, input_string, expected_output):
+        result = input_string.replace("\\%", "")
+        self.assertEqual(result, expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
