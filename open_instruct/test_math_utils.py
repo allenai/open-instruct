dc195255b61951f8f2c785c177e2e2a2dac76e28
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-09 15:50:12 -0600

    Fixed improt

--- open_instruct/test_math_utils.py
+++ open_instruct/test_math_utils.py
@@ -2,15 +2,17 @@ import unittest
 
 import parameterized
 
+from open_instruct import math_utils
+
 
 class TestStripString(unittest.TestCase):
     @parameterized.parameterized.expand(
-        [(r"50\% of the total", "50 of the total"), (r"50% of the total", "50% of the total")]
-    @parameterized.parameterized.expand([
-        (r"50\% of the total", "50ofthetotal"),
-        (r"50% of the total", "50%ofthetotal"),
-    ])
+        [(r"50\% of the total", "50ofthetotal"), (r"50% of the total", "50%ofthetotal")]
+    )
     def test_percentage_handling(self, input_string, expected_output):
-        result = open_instruct.math_utils.strip_string(input_string)
+        result = math_utils.strip_string(input_string)
         self.assertEqual(result, expected_output)
+
+
+if __name__ == "__main__":
     unittest.main()
