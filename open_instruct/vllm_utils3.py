4270093b68ea1da74d6117b3be7c7623b33076dd
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-10 11:22:18 -0600

    Updated code

--- open_instruct/vllm_utils3.py
+++ open_instruct/vllm_utils3.py
@@ -952,7 +952,8 @@ class LLMRayActor:
 
     def get_kv_cache_info(self):
         """Get KV cache max concurrency from the vLLM engine."""
-        kv_cache_specs = self.llm_engine.model_executor.get_kv_cache_specs()
+        # For UniProcExecutor, access through driver_worker
+        kv_cache_specs = self.llm_engine.model_executor.driver_worker.get_kv_cache_specs()
         kv_cache_spec = kv_cache_specs[0]
         # Group layers by their attention type (type_id) to handle models
         # with sliding attention in some layers but not others
