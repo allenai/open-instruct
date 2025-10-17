93acd525e91faf2d4b9a84868df03f29e39daed4
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-17 16:20:14 -0600

    compile model

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -701,6 +701,7 @@ class PolicyTrainerRayProcess(RayProcess):
             lr_scheduler=scheduler,
             dist_init_required=True,
         )
+        self.model.compile()
         optimization_steps_done = 0
         if args.checkpoint_state_dir:
             # check if the dir exists
