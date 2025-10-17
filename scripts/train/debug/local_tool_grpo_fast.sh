1ecfac9b7a39e43430d23d6745e886307858c2d2
Hamish Ivison <hamishivi@gmail.com>
2025-10-17 13:05:59 -0700

    new debug system prompt + debug script

--- scripts/train/debug/local_tool_grpo_fast.sh
+++ scripts/train/debug/local_tool_grpo_fast.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 uv run open_instruct/grpo_fast.py \
-    --dataset_mixer_list hamishivi/tulu_3_rewritten_100k_with_tool_prompt 64 \
+    --dataset_mixer_list ai2-adapt-dev/rlvr_gsm8k_zs 64 \
     --dataset_mixer_list_splits train \
-    --dataset_mixer_eval_list hamishivi/tulu_3_rewritten_100k_with_tool_prompt 16 \
+    --dataset_mixer_eval_list ai2-adapt-dev/rlvr_gsm8k_zs 16 \
     --dataset_mixer_eval_list_splits train \
     --max_token_length 512 \
     --max_prompt_token_length 512 \
@@ -11,11 +11,12 @@ uv run open_instruct/grpo_fast.py \
     --per_device_train_batch_size 1 \
     --num_unique_prompts_rollout 16 \
     --num_samples_per_prompt_rollout 4 \
-    --model_name_or_path Qwen/Qwen3-1.7B \
+    --model_name_or_path Qwen/Qwen3-0.6B \
     --apply_verifiable_reward true \
     --temperature 0.7 \
     --ground_truths_key ground_truth \
     --chat_template_name r1_simple_chat_postpend_think_tools \
+    --system_prompt_override_file open_instruct/tools/system_prompts/debug_system_prompt.txt \
     --learning_rate 3e-7 \
     --total_episodes 200 \
     --deepspeed_stage 2 \
@@ -31,7 +32,6 @@ uv run open_instruct/grpo_fast.py \
     --save_traces \
     --vllm_enforce_eager \
     --gradient_checkpointing \
-    --tools search code \
-    --search_api_endpoint "http://saturn-cs-aus-232.reviz.ai2.in:44177/search" \
+    --tools code \
     --code_tool_api_endpoint https://open-instruct-tool-server-10554368204.us-central1.run.app/execute \
     --push_to_hub false
