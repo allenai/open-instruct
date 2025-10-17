64c8caabe76a7ba18e653e504a2e44a8179b3467
Hamish Ivison <hamishivi@gmail.com>
2025-10-17 15:15:17 -0700

    lint

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -3091,7 +3091,7 @@ def main(args: Args, tc: TokenizerConfig, model_config: ModelConfig):
     pprint([args, model_config])
 
     # Initialize Ray before creating Ray objects
-    ray.init(dashboard_host="0.0.0.0", runtime_env={'excludes': ['.git/']})
+    ray.init(dashboard_host="0.0.0.0", runtime_env={"excludes": [".git/"]})
 
     # Create Ray queues.
     # Since we now send/receive individual prompts, queue size should accommodate
