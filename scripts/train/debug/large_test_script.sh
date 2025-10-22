1454912b6bb19959f5fda6dae3d9adf9427aa00b
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-22 09:43:40 -0600

    Added clusters.

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -6,6 +6,8 @@ exp_name=rlvr_ace_fn_and_og_ocr_stdio_from_base_with_perf_penalty
 BEAKER_IMAGE="${1:-${BEAKER_USER}/open-instruct-integration-test}"
 uv run python mason.py \
         --cluster ai2/jupiter \
+        --cluster ai2/ceres \
+        --cluster ai2/titan \
         --image "$BEAKER_IMAGE" \
 	--pure_docker_mode \
         --workspace ai2/open-instruct-dev \
