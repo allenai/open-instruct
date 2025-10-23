72846a6cca4c3ced8799e0160623b4b982786cc4
lilakk <monku1002@gmail.com>
2025-10-23 12:09:03 -0700

    save current scripts

--- scripts/train/yapeic/convert_then_upload.sh
+++ scripts/train/yapeic/convert_then_upload.sh
@@ -7,3 +7,14 @@ python scripts/convert_and_upload_checkpoints.py \
   --max_shard_size 5GB \
   --save_format safetensors \
   --trust_remote_code
+
+
+python scripts/convert_and_upload_checkpoints.py \
+  --model_name_or_path Qwen/Qwen2.5-7B-Instruct \
+  --checkpoint_path /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen25-7b-inst_v3_ratio_10k_with_stop_fixed_template_checkpoint_states/global_step140 \
+  --revision step140 \
+  --hf_repo_id yapeichang/grpo_qwen25-7b-inst_v3_ratio_10k_with_stop_fixed_template \
+  --upload_dtype bf16 \
+  --max_shard_size 5GB \
+  --save_format safetensors \
+  --trust_remote_code
