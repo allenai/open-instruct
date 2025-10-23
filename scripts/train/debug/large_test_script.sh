d30d5e62c522859d67312ceaab7c594902283dcc
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-23 18:51:01 +0000

    Updated script

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -34,7 +34,7 @@ uv run python mason.py \
         --max_prompt_token_length 2048 \
         --response_length 4096 \
         --pack_length 20480 \
-        --model_name_or_path /weka/oe-adapt-default/finbarrt/stego32/step17000-hf \
+        --model_name_or_path /weka/oe-adapt-default/finbarrt/stego32/step358000-hf \
 	--tokenizer_name_or_path "allenai/OLMo-2-1124-7B" \
         --chat_template_name tulu_thinker \
 	--inflight_updates True \
