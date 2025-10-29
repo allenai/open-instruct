52fb84d7b8c5c5d3a3c3bd2d72c7e734df1c9af7
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-10-29 09:34:46 -0600

    removed flag

--- scripts/benchmarking/launch_benchmark_single_node.sh
+++ scripts/benchmarking/launch_benchmark_single_node.sh
@@ -73,7 +73,6 @@ for model_name_or_path in "$@"; do
 	    --inflight_updates True \
             --vllm_num_engines 1 \
             --vllm_tensor_parallel_size 8 \
-            --vllm_pipeline_parallel_size 1 \
             --vllm_enable_prefix_caching \
             --vllm_gpu_memory_utilization 0.9 \
             --pack_length 40000 \
