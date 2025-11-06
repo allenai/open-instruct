13bd0d3aa6b38301ef2281bb7202c50ea5f86d31
Tyler Romero <tylerr@allenai.org>
2025-11-06 14:25:27 -0800

    add nccl env vars for debugging

--- scripts/train/debug/large_test_script.sh
+++ scripts/train/debug/large_test_script.sh
@@ -16,6 +16,8 @@ uv run python mason.py \
         --timeout 3600 \
         --max_retries 0 \
         --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
+        --env NCCL_DEBUG="INFO" \
+        --env NCCL_DEBUG_SUBSYS="INIT,NET,COLL" \
         --budget ai2/oe-adapt \
         --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& source configs/beaker_configs/code_api_setup.sh \&\&python open_instruct/grpo_fast.py \
         --exp_name ${exp_name} \
