a490a51f7dae9ed72035c7541db35a0a516d51ca
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-11-10 18:30:04 +0000

    updated script

--- scripts/train/olmo3/32b_dpo_smoke_test.sh
+++ scripts/train/olmo3/32b_dpo_smoke_test.sh
@@ -29,7 +29,7 @@ do
        --exp_name $EXP_NAME \
        --model_name_or_path $MODEL_NAME \
        --use_slow_tokenizer False \
-       --dataset_mixer_list allenai/olmo-3-preference-mix-deltas-complement2-grafmix-DECON-qwen32b-keyword-filtered 1000 \
+       --dataset_mixer_list allenai/olmo-3-preference-mix-deltas_reasoning-scottmix-DECON-keyword-filtered 10000       
        allenai/dpo-yolo1-200k-gpt4.1-2w2s-maxdelta_rejected-DECON-rm-gemma3-kwd-ftd 1000 \
        --dataset_skip_cache \
        --zero_stage 2 \
