a23445e65cd71f32ae9fc451058afdae28aa2bda
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-11-10 15:59:15 +0000

    now uses olmo-instruct

--- scripts/train/olmo3/dpo_sweep.sh
+++ scripts/train/olmo3/dpo_sweep.sh
@@ -9,7 +9,7 @@ do
     uv run python mason.py \
         --cluster ai2/augusta \
         --gs_model_name $EXP_NAME \
-        --workspace ai2/open-instruct-dev \
+        --workspace ai2/olmo-instruct \
         --priority urgent \
         --image scottg/open_instruct_dev_11092025 --pure_docker_mode \
         --preemptible \
