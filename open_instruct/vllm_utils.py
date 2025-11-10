81056258d3e71838fa8226f9927261d6c6b88771
Hamish Ivison <hamishivi@gmail.com>
2025-11-10 15:53:30 -0800

    longer timeout on capturing cuda

--- open_instruct/vllm_utils.py
+++ open_instruct/vllm_utils.py
@@ -881,8 +881,6 @@ def create_vllm_engines(
             )
         )
 
-    ray_get_with_progress(
-        [engine.ready.remote() for engine in vllm_engines], "Initializing vLLM engines", timeout=1200
-    )
+    ray_get_with_progress([engine.ready.remote() for engine in vllm_engines], "Initializing vLLM engines", timeout=1200)
 
     return vllm_engines
