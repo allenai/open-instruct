792adb628bcab3a4504a1f52572c4dabaef9d9b1
Hamish Ivison <hamishivi@gmail.com>
2025-11-10 16:49:55 -0800

    fix

--- open_instruct/search_rewards/utils/rubric_utils.py
+++ open_instruct/search_rewards/utils/rubric_utils.py
@@ -385,6 +385,7 @@ async def generate_instance_wise_adaptive_rubrics(question, response_list, exist
     prompt = INSTANCE_WISE_RUBRIC_GENERATION_PROMPT + prompt_suffix
     
     try:        
+        print("model name: ", model_name)
         resp = await run_litellm_async(
                 model_name=model_name,
                 user_prompt=prompt,
