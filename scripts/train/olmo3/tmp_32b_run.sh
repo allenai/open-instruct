b441c9c915cc6d9c0583643d746a390493b27ab3
Hamish Ivison <hamishivi@gmail.com>
2025-11-10 21:08:07 -0800

    working script

--- scripts/train/olmo3/tmp_32b_run.sh
+++ scripts/train/olmo3/tmp_32b_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-export exp_name=olmo3_32b_rl_run_${RANDOM}
+export exp_name=test_olmo3_32b_rl_run_${RANDOM}
 export data_mix="hamishivi/math_rlvr_mixture_dpo 1.0 hamishivi/code_rlvr_mixture_dpo 1.0 hamishivi/IF_multi_constraints_upto5_filtered_dpo_0625_filter 30186 allenai/rlvr_general_mix-keyword-filtered 21387"
-export beaker_image=TODO
-export model_path=TODO
+export beaker_image=hamishivi/open_instruct_rl32_test6
+export model_path=/weka/oe-adapt-default/hamishi/model_checkpoints/olmo3-merge-32b-1e-4-5e-5/olmo3-merge-32b-1e-4-5e-5/
 
 
 python mason.py \
@@ -14,13 +14,14 @@ python mason.py \
     --pure_docker_mode \
     --workspace ai2/olmo-instruct \
     --priority urgent \
-    --gs_model_name "dpo_olmo3_32b_rl_run" \
+    --gs_model_name "sft_olmo3_32b_rl_run_testing" \
     --preemptible \
-    --num_nodes 16 \
+    --num_nodes 12 \
     --gpus 8 \
     --max_retries 0 \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --env LD_LIBRARY_PATH=/var/lib/tcpxo/lib64 \
+    --env NCCL_LIB_DIR=/var/lib/tcpxo/lib64 \
     --env HOSTED_VLLM_API_BASE=http://ceres-cs-aus-447.reviz.ai2.in:8001/v1 \
     -- source configs/beaker_configs/ray_node_setup.sh \&\& source configs/beaker_configs/code_api_setup.sh \&\& python open_instruct/grpo_fast.py \
         --exp_name ${exp_name} \
@@ -37,7 +38,6 @@ python mason.py \
         --dataset_mixer_list_splits train \
         --dataset_mixer_eval_list hamishivi/omega-combined 8 allenai/IF_multi_constraints_upto5 8 saurabh5/rlvr_acecoder_filtered 8 hamishivi/tulu_3_rewritten_400k_string_f1_only_v2_nocode_all_filtered_qwen2_5_openthoughts2 4 hamishivi/virtuoussy_multi_subject_rlvr 4 \
         --dataset_mixer_eval_list_splits train \
-        --max_token_length 10240 \
         --max_prompt_token_length 2048 \
         --response_length 32768 \
         --pack_length 35840 \
@@ -50,8 +50,9 @@ python mason.py \
         --sft_messages_key messages \
         --total_episodes 10000000 \
         --deepspeed_stage 3 \
-        --num_learners_per_node 8 8 8 8 \
-        --vllm_num_engines 12 \
+        --num_learners_per_node 8 8 8 8 8 8 \
+        --vllm_num_engines 6 \
+        --gather_whole_model False \
         --vllm_tensor_parallel_size 8 \
         --lr_scheduler_type constant \
         --apply_verifiable_reward true \
