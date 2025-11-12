877755df0881e0cae4f5e4bd3fcd9f7791342757
lilakk <monku1002@gmail.com>
2025-11-12 10:43:03 -0800

    new scripts

--- scripts/train/yapeic/grpo_qwen3-8b-inst_v1_binary.sh
+++ scripts/train/yapeic/grpo_qwen3-8b-inst_v1_binary.sh
@@ -1,24 +1,24 @@
-# beaker experiment create configs/judge_configs/proc_judge.yaml \
-#     --name vllm_judge_qwen3-4b_v1_binary \
-#     --workspace ai2/oe-data
+beaker experiment create configs/judge_configs/proc_judge.yaml \
+    --name vllm_judge_qwen3-8b_v1_binary \
+    --workspace ai2/oe-data
 
 JUDGE_BASE_URL=http://saturn-cs-aus-253.reviz.ai2.in:8001/v1
 
 python mason.py \
     --cluster ai2/jupiter \
-    --task_name grpo_qwen3-4b-inst_v1_binary \
-    --description "GRPO Qwen3-4B, v1 binary" \
+    --task_name grpo_qwen3-8b-inst_v1_binary \
+    --description "GRPO Qwen3-8B, v1 binary" \
     --workspace ai2/oe-data \
     --priority high \
     --image nathanl/open_instruct_auto \
     --preemptible \
-    --num_nodes 1 \
+    --num_nodes 2 \
     --budget ai2/oe-base \
     --env HOSTED_VLLM_API_BASE=${JUDGE_BASE_URL} \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --secret HF_TOKEN=yapeic_HF_TOKEN \
     --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
-    --exp_name grpo_qwen3-4b-inst_v1_binary \
+    --exp_name grpo_qwen3-8b-inst_v1_binary \
     --beta 0.0 \
     --num_unique_prompts_rollout 4 \
     --num_samples_per_prompt_rollout 8 \
@@ -29,9 +29,9 @@ python mason.py \
     --dataset_mixer_eval_list /weka/oe-training-default/yapeic/proc-data/data/dclm/tutorial_subset/batch_runs_prefiltered_4_15_new_filtered_v14_pp_v11_tools_v5_ff_v2/grpo_data/v1_binary-format_100k_test.jsonl 1.0 \
     --dataset_mixer_eval_list_splits train \
     --max_prompt_token_length 1024 \
-    --response_length 2048 \
-    --pack_length 3072 \
-    --model_name_or_path Qwen/Qwen3-4B \
+    --response_length 5120 \
+    --pack_length 6144 \
+    --model_name_or_path Qwen/Qwen3-8B \
     --apply_verifiable_reward True \
     --llm_judge_model "hosted_vllm/yapeichang/distill_judge_qwen3-8b_sft_v6" \
     --non_stop_penalty True \
@@ -41,20 +41,20 @@ python mason.py \
     --deepspeed_stage 3 \
     --per_device_train_batch_size 1 \
     --num_mini_batches 1 \
-    --num_learners_per_node 6 \
+    --num_learners_per_node 4 \
     --num_epochs 1 \
     --vllm_tensor_parallel_size 1 \
-    --vllm_num_engines 4 \
+    --vllm_num_engines 12 \
     --lr_scheduler_type linear \
     --seed 1 \
     --local_eval_every 40 \
     --checkpoint_state_freq 20 \
-    --checkpoint_state_dir /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen3-4b-inst_v1_binary_checkpoint_states \
+    --checkpoint_state_dir /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen3-8b-inst_v1_binary_checkpoint_states \
     --save_freq 100 \
     --keep_last_n_checkpoints 10 \
     --gradient_checkpointing \
     --with_tracking \
     --hf_entity yapeichang \
-    --hf_repo_id grpo_qwen3-4b-inst_v1_binary \
+    --hf_repo_id grpo_qwen3-8b-inst_v1_binary \
     --wandb_project_name yapeic-exp \
     --wandb_entity ai2-llm
