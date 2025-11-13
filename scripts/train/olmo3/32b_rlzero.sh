55a054997d251d415939eba2e3bc362ad5b666e1
Saurabh Shah <saurabhs@allenai.org>
2025-11-13 05:32:39 +0000

    script change

--- scripts/train/olmo3/32b_rlzero.sh
+++ scripts/train/olmo3/32b_rlzero.sh
@@ -25,7 +25,7 @@ python mason.py \
     --task_name ${EXP_NAME} \
     --cluster ${cluster} \
     --workspace ai2/olmo-instruct \
-    --priority high \
+    --priority urgent \
     --pure_docker_mode \
     --image ${BEAKER_IMAGE} \
     --preemptible \
@@ -33,10 +33,9 @@ python mason.py \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --env VLLM_ATTENTION_BACKEND="FLASH_ATTN" \
     --gpus 8 \
+    --no_auto_dataset_cache \
     --budget ai2/oe-adapt \
-    -- \
-source configs/beaker_configs/ray_node_setup.sh \&\& \
-python open_instruct/grpo_fast.py \
+    -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
     --exp_name ${EXP_NAME} \
     --beta 0.0 \
     --async_steps 4 \
@@ -45,8 +44,8 @@ python open_instruct/grpo_fast.py \
     --truncated_importance_sampling_ratio_cap 2.0 \
     --advantage_normalization_type centered \
     --active_sampling \
-    --num_samples_per_prompt_rollout 16 \
-    --num_unique_prompts_rollout 16 \
+    --num_samples_per_prompt_rollout 8 \
+    --num_unique_prompts_rollout 32 \
     --num_mini_batches 1 \
     --learning_rate 1e-6 \
     --per_device_train_batch_size 1 \
@@ -56,8 +55,8 @@ python open_instruct/grpo_fast.py \
     --dataset_mixer_eval_list $LOCAL_EVALS \
     --dataset_mixer_eval_list_splits $LOCAL_EVAL_SPLITS \
     --max_prompt_token_length 2048 \
-    --response_length 16000 \
-    --pack_length 18048 \
+    --response_length 16384 \
+    --pack_length 18432 \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --chat_template_name olmo_thinker_dapo \
     --non_stop_penalty False \
@@ -77,6 +76,8 @@ python open_instruct/grpo_fast.py \
     --checkpoint_state_freq 100 \
     --gradient_checkpointing \
     --with_tracking \
+    --checkpoint_state_dir /output \
+    --gs_checkpoint_state_dir gs://ai2-llm/checkpoints/rlzero/olmo3-32b_rlzero/ \
     --vllm_enable_prefix_caching \
     --clip_higher 0.272 \
     --mask_truncated_completions True \
