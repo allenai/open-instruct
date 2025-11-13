92a49e88e6debb47dfcd543ee463d3131fc760a6
Michael Noukhovitch <michaeln@allenai.org>
2025-11-13 17:21:12 +0000

    fix is not None

--- open_instruct/model_utils.py
+++ open_instruct/model_utils.py
@@ -67,10 +67,10 @@ class Batch:
                 queries=self.queries[key],
                 ground_truths=self.ground_truths[key],
                 datasets=self.datasets[key],
-                raw_queries=self.raw_queries[key] if self.raw_queries else None,
-                decoded_responses=self.decoded_responses[key] if self.decoded_responses else None,
-                indices=self.indices[key] if self.indices else None,
-                scores=self.scores[key] if self.scores else None,
+                raw_queries=self.raw_queries[key] if self.raw_queries is not None else None,
+                decoded_responses=self.decoded_responses[key] if self.decoded_responses is not None else None,
+                indices=self.indices[key] if self.indices is not None else None,
+                scores=self.scores[key] if self.scores is not None else None,
             )
         elif isinstance(key, int):
             # Handle single index: batch[5]
@@ -78,10 +78,10 @@ class Batch:
                 queries=[self.queries[key]],
                 ground_truths=[self.ground_truths[key]],
                 datasets=[self.datasets[key]],
-                raw_queries=[self.raw_queries[key]] if self.raw_queries else None,
-                decoded_responses=[self.decoded_responses[key]] if self.decoded_responses else None,
-                indices=[self.indices[key]] if self.indices else None,
-                scores=[self.scores[key]] if self.scores else None,
+                raw_queries=[self.raw_queries[key]] if self.raw_queries is not None else None,
+                decoded_responses=[self.decoded_responses[key]] if self.decoded_responses is not None else None,
+                indices=[self.indices[key]] if self.indices is not None else None,
+                scores=[self.scores[key]] if self.scores is not None else None,
             )
         else:
             # Handle list of indices: batch[[1,3,5]]
@@ -89,10 +89,12 @@ class Batch:
                 queries=[self.queries[i] for i in key],
                 ground_truths=[self.ground_truths[i] for i in key],
                 datasets=[self.datasets[i] for i in key],
-                raw_queries=[self.raw_queries[i] for i in key] if self.raw_queries else None,
-                decoded_responses=[self.decoded_responses[i] for i in key] if self.decoded_responses else None,
-                indices=[self.indices[i] for i in key] if self.indices else None,
-                scores=[self.scores[i] for i in key] if self.scores else None,
+                raw_queries=[self.raw_queries[i] for i in key] if self.raw_queries is not None else None,
+                decoded_responses=[self.decoded_responses[i] for i in key]
+                if self.decoded_responses is not None
+                else None,
+                indices=[self.indices[i] for i in key] if self.indices is not None else None,
+                scores=[self.scores[i] for i in key] if self.scores is not None else None,
             )
 
 
