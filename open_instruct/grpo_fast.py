b1ccc5d40c4e78b98889a6eb350212415ce10866
Hamish Ivison <hamishivi@gmail.com>
2025-11-15 11:18:32 -0800

    fixes to gs checkpoint logic

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -507,7 +507,7 @@ class Args:
             else:
                 self.gs_checkpoint_state_dir = f"{self.gs_bucket_path}/{checkpoint_dir_name}"
             if not checkpoint_dir_name.startswith("/filestore"):
-                self.gs_checkpoint_state_dir = f"/filestore{self.gs_checkpoint_state_dir}"
+                self.checkpoint_state_dir = f"/filestore{self.checkpoint_state_dir}"
 
         if self.checkpoint_state_dir is not None:
             if self.gs_checkpoint_state_dir is not None:
