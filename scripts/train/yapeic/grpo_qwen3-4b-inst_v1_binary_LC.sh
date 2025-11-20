d9abf277a8289f03043bbe6e41565c48432ff72a
lilakk <monku1002@gmail.com>
2025-11-20 15:02:23 -0800

    relax lc

--- scripts/train/yapeic/grpo_qwen3-4b-inst_v1_binary_LC.sh
+++ scripts/train/yapeic/grpo_qwen3-4b-inst_v1_binary_LC.sh
@@ -6,8 +6,8 @@ JUDGE_BASE_URL=http://saturn-cs-aus-242.reviz.ai2.in:8001/v1
 
 python mason.py \
     --cluster ai2/jupiter \
-    --task_name grpo_qwen3-4b-inst_v1_binary_LC_sym \
-    --description "GRPO Qwen3-4B, v1 binary (length control symmetric)" \
+    --task_name grpo_qwen3-4b-inst_v1_binary_LC_sym_r2 \
+    --description "GRPO Qwen3-4B, v1 binary (length control symmetric) r2" \
     --workspace ai2/oe-data \
     --priority high \
     --image nathanl/open_instruct_auto \
@@ -18,7 +18,7 @@ python mason.py \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --secret HF_TOKEN=yapeic_HF_TOKEN \
     --gpus 8 -- source configs/beaker_configs/ray_node_setup.sh \&\& python open_instruct/grpo_fast.py \
-    --exp_name grpo_qwen3-4b-inst_v1_binary_LC_sym \
+    --exp_name grpo_qwen3-4b-inst_v1_binary_LC_sym_r2 \
     --beta 0.0 \
     --num_unique_prompts_rollout 4 \
     --num_samples_per_prompt_rollout 8 \
@@ -49,14 +49,14 @@ python mason.py \
     --seed 1 \
     --local_eval_every 110 \
     --checkpoint_state_freq 40 \
-    --checkpoint_state_dir /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen3-4b-inst_v1_binary_LC_sym_checkpoint_states \
-    --output_dir /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen3-4b-inst_v1_binary_LC_sym_checkpoints \
+    --checkpoint_state_dir /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen3-4b-inst_v1_binary_LC_sym_r2_checkpoint_states \
+    --output_dir /weka/oe-adapt-default/allennlp/deletable_checkpoint/yapeic/grpo_qwen3-4b-inst_v1_binary_LC_sym_r2_checkpoints \
     --save_freq 100 \
     --keep_last_n_checkpoints 10 \
     --gradient_checkpointing \
     --vllm_gpu_memory_utilization 0.7 \
     --with_tracking \
     --hf_entity yapeichang \
-    --hf_repo_id grpo_qwen3-4b-inst_v1_binary_LC_sym \
+    --hf_repo_id grpo_qwen3-4b-inst_v1_binary_LC_sym_r2 \
     --wandb_project_name yapeic-exp \
     --wandb_entity ai2-llm
