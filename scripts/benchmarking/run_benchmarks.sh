e909e65e1fb0faa20d187627a659be29bf09183f
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-11-30 17:26:39 +0000

    changed config to use 7b

--- scripts/benchmarking/run_benchmarks.sh
+++ scripts/benchmarking/run_benchmarks.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-MODEL_PATH="/weka/oe-adapt-default/finbarrt/stego32/step17000-hf"
+#MODEL_PATH="/weka/oe-adapt-default/finbarrt/stego32/step17000-hf"
+MODEL_PATH="/weka/oe-adapt-default/finbarrt/olmo25_7b-hf-olmo3-test"
 
-for seq_len in 4096 8192 16000 32000; do
+for seq_len in 1024 4096 8192 16000 32000; do
     echo "Running benchmark with sequence length: $seq_len"
     ./scripts/train/build_image_and_launch.sh scripts/benchmarking/launch_benchmark_single_node.sh "$seq_len" "$MODEL_PATH"
 done
