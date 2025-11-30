6e382974487026e25422f3c774a7578c50f957eb
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-11-30 17:22:15 +0000

    removed 1024

--- scripts/benchmarking/run_benchmarks.sh
+++ scripts/benchmarking/run_benchmarks.sh
@@ -2,7 +2,7 @@
 
 MODEL_PATH="/weka/oe-adapt-default/finbarrt/stego32/step17000-hf"
 
-for seq_len in 1024 4096 8192 16000 32000; do
+for seq_len in 4096 8192 16000 32000; do
     echo "Running benchmark with sequence length: $seq_len"
     ./scripts/train/build_image_and_launch.sh scripts/benchmarking/launch_benchmark_single_node.sh "$seq_len" "$MODEL_PATH"
 done
