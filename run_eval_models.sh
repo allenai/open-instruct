6799085c8307578c93adbe1ba66f93f818f5fba8
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-11-30 16:45:42 -0800

    eval

--- run_eval_models.sh
+++ run_eval_models.sh
@@ -3,11 +3,11 @@
 # Define arrays for model names and paths
 PY="/opt/anaconda3/envs/python310/bin/python"
 MODEL_NAMES=(
-    "Merged-Supervied-Router-SourceLabels"
+    "Merged-Supervised-Router-ExpertsSFTed-SourceLabels"
 )
 
 MODEL_PATHS=(
-    "/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7b-flex-base-merged-math-code-RT-supervised-router-basic/step750-hf"
+    "/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7b-flex-base-merged-math-code-experts-sft-math-mixed-RT-supervised-router-basic/step10500-hf"
 )
 
 # Check that arrays have the same length
