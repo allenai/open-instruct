6c493cc84013f80fac1d8380b1cfc2e2d31cf693
Michael Noukhovitch <michaeln@allenai.org>
2025-12-03 19:26:03 +0000

    filestore fix

--- open_instruct/grpo_fast.py
+++ open_instruct/grpo_fast.py
@@ -499,6 +499,9 @@ class Args:
             else:
                 self.gs_checkpoint_state_dir = f"{self.gs_bucket_path}/{checkpoint_dir_name}"
 
+        if not checkpoint_dir_name.startswith("/filestore"):
+            self.checkpoint_state_dir = f"/filestore{self.checkpoint_state_dir}"
+
         if self.checkpoint_state_dir is not None:
             if self.gs_checkpoint_state_dir is not None:
                 download_latest_checkpoint_from_gs(self.gs_checkpoint_state_dir, self.checkpoint_state_dir)
