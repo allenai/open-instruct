9834e2e857dd952a70e2a8bb202c504390f830d3
Finbarr Timbers <finbarrtimbers@gmail.com>
2025-12-03 09:42:17 -0700

    Fix test pattern to match all TestGeneration tests

--- scripts/train/debug/run_gpu_generation_tests.sh
+++ scripts/train/debug/run_gpu_generation_tests.sh
@@ -14,4 +14,4 @@ uv run python mason.py \
     --timeout 30m \
     --budget ai2/oe-adapt \
     --gpus 1 \
-    -- 'source configs/beaker_configs/ray_node_setup.sh && pytest open_instruct/test_grpo_fast_gpu.py::TestGeneration::test_generation_deterministic -xvs'
+    -- 'source configs/beaker_configs/ray_node_setup.sh && pytest open_instruct/test_grpo_fast_gpu.py::TestGeneration -xvs'
