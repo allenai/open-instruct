c7e1c3998673d4c768085fc329c0bd9b309dce58
jacob-morrison <jacobm@allenai.org>
2025-12-05 11:16:46 -0800

    push script

---
+++ get-dpo-model-responses.sh
@@ -0,0 +1,15 @@
+# need to fix
+python get-responses-from-model.py \
+  --dataset_name "jacobmorrison/social-rl-eval-prompts-100" \
+  --model_name "allenai/Olmo-3-7B-Instruct-DPO" \
+  --tokenizer_name "allenai/Olmo-3-7B-Instruct-DPO" \
+  --output_path "responses-from-olmo-3-instruct-dpo.jsonl" \
+  --num_gpus 1 \
+  --num_responses 1 \
+  --apply_chat_template \
+  --temperature 1.0 \
+  --top_p 1.0 \
+  --max_tokens 4096 \
+  --max_model_len 32768 \
+  --apply_chat_template \
+  --messages_column "messages"
