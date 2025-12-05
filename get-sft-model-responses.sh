f52b050ce0fd0c396fc673f1c0da5c48529a17f9
jacob-morrison <jacobm@allenai.org>
2025-12-04 16:42:29 -0800

    update

--- get-sft-model-responses.sh
+++ get-sft-model-responses.sh
@@ -1,9 +1,9 @@
 # need to fix
 python get-responses-from-model.py \
   --dataset_name "jacobmorrison/social-rl-eval-prompts-100" \
-  --model_name "allenai/Olmo-3-1025-7B" \
-  --tokenizer_name "/weka/oe-adapt-default/jacobm/social-rl/checkpoints/baseline_random_all/test_exp__1__1761341180_checkpoints/step_1000/" \
-  --output_path "responses-from-olmo-3-base-model.jsonl" \
+  --model_name "allenai/Olmo-3-7B-Instruct-SFT" \
+  --tokenizer_name "allenai/Olmo-3-7B-Instruct-SFT" \
+  --output_path "responses-from-olmo-3-instruct-sft.jsonl" \
   --num_gpus 1 \
   --num_responses 1 \
   --apply_chat_template \
