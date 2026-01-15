943caa0524c5cb6af07e9d79464943293a49ba29
Finbarr Timbers <finbarrtimbers@gmail.com>
2026-01-14 17:55:36 -0700

    updated to num_epochs

--- scripts/train/debug/large_dpo.sh
+++ scripts/train/debug/large_dpo.sh
@@ -32,7 +32,7 @@ uv run python mason.py \
     --lr_scheduler_type linear \
     --warmup_ratio 0.1 \
     --weight_decay 0.0 \
-    --num_train_epochs 1 \
+    --num_epochs 1 \
     --output_dir output/dpo_olmo3_debug/ \
     --dataset_mixer_list allenai/tulu-3-wildchat-reused-on-policy-8b 1000 \
     --seed 123 \
