86ccd12d5d4cca8dd35f63520a0fde16145bfaeb
Hamish Ivison <hamishivi@gmail.com>
2026-01-19 14:07:37 -0800

    Refactor dense comprehension into readable for loop

--- open_instruct/dataset_transformation.py
+++ open_instruct/dataset_transformation.py
@@ -946,9 +946,12 @@ def validate_dataset_tools(dataset: Dataset, configured_tool_names: list[str], d
     if TOOLS_COLUMN_KEY not in dataset.column_names:
         return
 
-    dataset_tool_names = {
-        tool_name for tools in dataset[TOOLS_COLUMN_KEY] if tools for tool_name in tools if tool_name
-    }
+    dataset_tool_names: set[str] = set()
+    for tools in dataset[TOOLS_COLUMN_KEY]:
+        if tools:
+            for tool_name in tools:
+                if tool_name:
+                    dataset_tool_names.add(tool_name)
     configured_set = set(configured_tool_names) if configured_tool_names else set()
 
     unconfigured_tools = dataset_tool_names - configured_set
