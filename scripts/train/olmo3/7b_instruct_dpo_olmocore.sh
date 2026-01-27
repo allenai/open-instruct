e7be6e5f8bc23bede3809bfde415ae08552c00ab
Finbarr Timbers <finbarrtimbers@gmail.com>
2026-01-27 08:20:53 -0700

    removed eval from script

--- scripts/train/olmo3/7b_instruct_dpo_olmocore.sh
+++ scripts/train/olmo3/7b_instruct_dpo_olmocore.sh
@@ -5,23 +5,17 @@ for LR in 1e-6
 do
     EXP_NAME=olmo3-7b-DPO-olmocore-${LR}
     uv run python mason.py \
-        --cluster ai2/augusta \
+        --cluster ai2/jupiter \
         --description "OLMo3-7B DPO with OLMo-core, 4 nodes, 16k seq len" \
-        --gs_model_name olmo3-7b-instruct-SFT-1115 \
         --workspace ai2/olmo-instruct \
         --priority urgent \
         --max_retries 2 \
         --preemptible \
         --image $BEAKER_IMAGE --pure_docker_mode \
-        --env NCCL_LIB_DIR=/var/lib/tcpxo/lib64 \
-        --env LD_LIBRARY_PATH=/var/lib/tcpxo/lib64 \
-        --env NCCL_PROTO=Simple,LL128 \
-        --env NCCL_TUNER_CONFIG_PATH=/var/lib/tcpxo/lib64/a3plus_tuner_config_ll128.textproto \
-        --env NCCL_SHIMNET_GUEST_CONFIG_CHECKER_CONFIG_FILE=/var/lib/tcpxo/lib64/a3plus_guest_config_ll128.textproto \
         --env OLMO_SHARED_FS=1 \
         --num_nodes 4 \
         --budget ai2/oe-adapt \
-        --gpus 8 -- source /var/lib/tcpxo/lib64/nccl-env-profile.sh \&\& torchrun \
+        --gpus 8 -- torchrun \
         --nnodes=4 \
         --node_rank=\$BEAKER_REPLICA_RANK \
         --master_addr=\$BEAKER_LEADER_REPLICA_HOSTNAME \
@@ -55,10 +49,5 @@ do
         --loss_type dpo_norm \
         --beta 5 \
         --gradient_checkpointing \
-        --with_tracking \
-        --eval_workspace ai2/olmo-instruct \
-        --eval_priority urgent \
-        --oe_eval_max_length 32768 \
-        --oe_eval_gpu_multiplier 2 \
-        --oe_eval_tasks "omega_500:0-shot-chat_deepseek,minerva_math_500::hamish_zs_reasoning_deepseek,livecodebench_codegeneration::tulu-thinker_deepseek_no_think_tags_lite,aime:zs_cot_r1::pass_at_32_2024_deepseek,aime:zs_cot_r1::pass_at_32_2025_deepseek,zebralogic::hamish_zs_reasoning_deepseek,bbh:cot::hamish_zs_reasoning_deepseek_v2,popqa::hamish_zs_reasoning_deepseek,mbppplus:0-shot-chat::tulu-thinker_deepseek,mmlu:cot::hamish_zs_reasoning_deepseek,gpqa:0shot_cot::qwen3-instruct,codex_humanevalplus:0-shot-chat::tulu-thinker_deepseek,alpaca_eval_v3::hamish_zs_reasoning_deepseek,ifeval::hamish_zs_reasoning_deepseek,agi_eval_english:0shot_cot::hamish_zs_reasoning_deepseek"
+        --with_tracking
 done
