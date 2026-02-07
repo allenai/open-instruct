8c5273a8dad4bb176f86fe64e1ab21c2a56f947c
Michael Noukhovitch <michaeln@allenai.org>
2026-02-07 02:26:54 +0000

    updates

--- run_eval.sh
+++ run_eval.sh
@@ -4,28 +4,31 @@
 # wandb_id="n3irz0v8_16k"
 # model_location="gs://ai2-llm/post-training//michaeln//output/olmo3-7b-rlzero-math/checkpoints/${model_name}_checkpoints"
 #
-model_name="qwen3_1.7b_rlzero_math__1__1766123249"
-wandb_id="op75161d"
+# model_name="qwen3_1.7b_rlzero_math__1__1766123249"
+model_name="Qwen/Qwen3-4B-Instruct-2507"
+model_location="/weka/oe-adapt-default/allennlp/.cache/hub/models--Qwen--Qwen3-4B-Instruct-2507/snapshots/cdbee75f17c01a7cc42f958dc650907174af0554"
+wandb_id="qwen3_4b_instruct"
 # model_name="olmo3_7b_rlzero_math__1__1763966683"
 # wandb_id="kn6kewty"
 # model_location="gs://ai2-llm/post-training//michaeln/output/${model_name}_checkpoints"
-model_location="/weka/oe-adapt-default/allennlp/deletable_checkpoint/michaeln/${model_name}_checkpoints"
-
+# model_location="/weka/oe-adapt-default/allennlp/deletable_checkpoint/michaeln/${model_name}_checkpoints"
+# --model-name "${model_name}_step_${step}" \
+# --model-location "${model_location}/step_$step" \
 
 # for step in {100..2000..100}; do
-    uv run bash scripts/eval/oe-eval.sh \
-        --model-name "${model_name}_step_${step}" \
-        --model-location "${model_location}/step_$step" \
-        --beaker-workspace ai2/tulu-3-results \
-        --tasks aime:zs_cot_r1::pass_at_32_2024_rlzero,aime:zs_cot_r1::pass_at_32_2025_rlzero,minerva_math_500::hamish_zs_reasoning_rlzero \
-        --run-id https://wandb.ai/ai2-llm/open_instruct_internal/runs/$wandb_id \
-        --step $step \
-        --wandb-run-path ai2-llm/open_instruct_internal/$wandb_id \
-        --num_gpus 1 \
-        --max-length 32768 \
-        --task-suite NEXT_MODEL_DEV \
-        --priority normal \
-        --process-output r1_style \
-        --beaker-image oe-eval-beaker/oe_eval_auto \
-        --cluster 'ai2/neptune'
-done
+# --step $step \
+uv run bash scripts/eval/oe-eval.sh \
+    --model-name $model_name \
+    --model-location $model_name \
+    --beaker-workspace ai2/oe-adapt-code \
+    --tasks aime:2025::justrl \
+    --run-id https://wandb.ai/ai2-llm/open_instruct_internal/runs/$wandb_id \
+    --wandb-run-path ai2-llm/open_instruct_internal/$wandb_id \
+    --num_gpus 1 \
+    --max-length 32768 \
+    --task-suite NEXT_MODEL_DEV \
+    --priority high \
+    --process-output r1_style \
+    --beaker-image michaeln/oe_eval_internal \
+    --cluster 'ai2/neptune'
+# done
--- scripts/train/qwen/qwen2.5_0.5b_gsm8k.sh
+++ scripts/train/qwen/qwen2.5_0.5b_gsm8k.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 EXP_NAME="qwen3_4b_it_deepmath"
-MODEL_NAME_OR_PATH="Qwen/Qwen3-4B-Instruct-2507"
-DATASETS="mnoukhov/DeepMath-103K-openinstruct 1.0"
+MODEL_NAME_OR_PATH=" Qwen/Qwen2.5-0.5B-Instruct"
+DATASETS="ai2-adapt-dev/rlvr_gsm8k_zs 1.0"
 
-LOCAL_EVALS="allenai/aime2024-25-rlvr 32"
-LOCAL_EVAL_SPLITS="test_2024 test_2024"
+LOCAL_EVALS="mnoukhov/gsm8k-platinum-openinstruct 1.0"
+LOCAL_EVAL_SPLITS="test"
 
-EVALS="aime:2024::justrl,aime:2025::justrl"
+# EVALS="aime:2024::justrl,aime:2025::justrl"
 
 # BEAKER_USER=$(beaker account whoami --format json | jq -r '.[0].name')
 BEAKER_IMAGE="michaeln/open_instruct"
@@ -30,7 +30,7 @@ uv run mason.py \
     --num_nodes 1 \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --env VLLM_ATTENTION_BACKEND="FLASH_ATTN" \
-    --gpus 4 \
+    --gpus 1 \
     --budget ai2/oe-adapt \
     -- source configs/beaker_configs/ray_node_setup.sh \
 \&\& uv run open_instruct/grpo_fast.py \
@@ -39,7 +39,6 @@ uv run mason.py \
     --async_steps 4 \
     --active_sampling \
     --inflight_updates \
-    --no_resampling_pass_rate 0.875 \
     --truncated_importance_sampling_ratio_cap 2.0 \
     --advantage_normalization_type centered \
     --num_samples_per_prompt_rollout 16 \
@@ -51,35 +50,39 @@ uv run mason.py \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list $LOCAL_EVALS \
     --dataset_mixer_eval_list_splits $LOCAL_EVAL_SPLITS \
-    --max_prompt_token_length 2048 \
-    --response_length 24000 \
-    --pack_length 32000 \
+    --max_prompt_token_length 512 \
+    --response_length 2048 \
+    --pack_length 4096 \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --user_prompt_transform "{prompt}\n\nPlease reason step by step, and put your final answer within \\boxed{{}}." \
     --non_stop_penalty False \
     --temperature 1.0 \
     --total_episodes 512000 \
     --deepspeed_stage 2 \
-    --num_learners_per_node 1 \
-    --vllm_num_engines 3 \
-    --vllm_tensor_parallel_size 1 \
     --lr_scheduler_type constant \
     --apply_verifiable_reward true \
     --seed 1 \
-    --local_eval_every 50 \
-    --save_freq 100 \
-    --checkpoint_state_freq 100 \
+    --local_eval_every 100 \
+    --save_freq 200 \
+    --checkpoint_state_freq 200 \
     --gradient_checkpointing \
-    --with_tracking \
     --vllm_enable_prefix_caching \
-    --clip_higher 0.272 \
+    --single_gpu_mode \
+    --vllm_sync_backend gloo \
+    --vllm_gpu_memory_utilization 0.3 \
+    --num_learners_per_node 1 \
+    --vllm_tensor_parallel_size 1 \
+    --clip_higher 0.28 \
     --mask_truncated_completions False \
-    --oe_eval_max_length 32768 \
-    --try_launch_beaker_eval_jobs_on_weka True \
-    --eval_priority normal \
-    --oe_eval_tasks $EVALS \
     --load_ref_policy False \
+    --eval_on_step_0 True \
+    --with_tracking \
     --keep_last_n_checkpoints -1 \
-    --oe_eval_gpu_multiplier 2  \
-    --oe_eval_beaker_image michaeln/oe_eval_internal \
     --push_to_hub False $@
+
+    # --eval_priority normal \
+    # --try_launch_beaker_eval_jobs_on_weka True \
+    # --oe_eval_max_length 32768 \
+    # --oe_eval_gpu_multiplier 2  \
+    # --oe_eval_beaker_image michaeln/oe_eval_internal \
+    # --oe_eval_tasks $EVALS \
