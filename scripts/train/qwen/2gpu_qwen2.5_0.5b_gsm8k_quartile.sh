6d9fe78a37c99af5577f55de863e968058f726cc
Michael Noukhovitch <michaeln@allenai.org>
2026-02-18 22:43:20 +0000

    2gpus quartiles

--- scripts/train/qwen/2gpu_qwen2.5_0.5b_gsm8k_quartile.sh
+++ scripts/train/qwen/2gpu_qwen2.5_0.5b_gsm8k_quartile.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-EXP_NAME="qwen25_05b_it_gsm8k_mathtemplate"
+EXP_NAME="qwen25_05b_it_gsm8k_mathtemplate_pass0"
 MODEL_NAME_OR_PATH=" Qwen/Qwen2.5-0.5B-Instruct"
-DATASETS="mnoukhov/gsm8k-platinum-openinstruct-0.5b-instruct-0 8 mnoukhov/gsm8k-platinum-openinstruct-0.5b-instruct-25 8 mnoukhov/gsm8k-platinum-openinstruct-0.5b-instruct-50 8 mnoukhov/gsm8k-platinum-openinstruct-0.5b-instruct-75 8"
+DATASETS="mnoukhov/gsm8k-platinum-openinstruct-0.5b-instruct-0 8"
 BEAKER_IMAGE="michaeln/open_instruct"
 
 uv run mason.py \
     --task_name ${EXP_NAME} \
-    --cluster ai2/saturn ai2/jupiter \
+    --cluster ai2/prometheus \
     --workspace ai2/oe-adapt-code \
-    --priority high \
+    --priority low \
     --pure_docker_mode \
     --image ${BEAKER_IMAGE} \
     --preemptible \
     --num_nodes 1 \
     --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     --env VLLM_ATTENTION_BACKEND="FLASHINFER" \
-    --gpus 4 \
+    --gpus 2 \
     --budget ai2/oe-adapt \
     -- source configs/beaker_configs/ray_node_setup.sh \
 \&\& uv run --active open_instruct/grpo_fast.py \
     --exp_name ${EXP_NAME} \
     --run_name $EXP_NAME \
     --beta 0.0 \
-    --async_steps 4 \
+    --async_steps 1 \
     --inflight_updates \
     --filter_zero_std_samples False \
     --log_train_solve_rate_metrics True \
     --truncated_importance_sampling_ratio_cap 2.0 \
     --advantage_normalization_type centered \
     --num_samples_per_prompt_rollout 16 \
-    --num_unique_prompts_rollout 32 \
+    --num_unique_prompts_rollout 8 \
     --num_mini_batches 1 \
     --learning_rate 1e-6 \
     --per_device_train_batch_size 1 \
@@ -43,7 +43,7 @@ uv run mason.py \
     --chat_template_name qwen_instruct_math \
     --non_stop_penalty False \
     --temperature 1.0 \
-    --total_episodes 512000 \
+    --total_episodes 256000 \
     --deepspeed_stage 2 \
     --lr_scheduler_type constant \
     --apply_verifiable_reward true \
@@ -52,13 +52,10 @@ uv run mason.py \
     --gradient_checkpointing \
     --vllm_enable_prefix_caching \
     --num_learners_per_node 1 \
-    --vllm_num_engines 3 \
+    --vllm_num_engines 1 \
     --vllm_tensor_parallel_size 1 \
     --clip_higher 0.28 \
     --mask_truncated_completions False \
     --load_ref_policy True \
     --with_tracking \
     --push_to_hub False $@
-
-    # --checkpoint_state_freq 200 \
-    # --keep_last_n_checkpoints -1 \
