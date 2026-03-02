6f5e0254aedd3cd3045ab553bf2d5cf46194e17c
Michael Noukhovitch <michaeln@allenai.org>
2026-03-02 20:51:51 +0000

    new vllm and transformers 5.2.0

--- scripts/train/debug/grpo_fast.sh
+++ scripts/train/debug/grpo_fast.sh
@@ -2,23 +2,22 @@
 export VLLM_ALLOW_INSECURE_SERIALIZATION=1
 export VLLM_DISABLE_COMPILE_CACHE=1
 export VLLM_USE_V1=1
+export TORCH_COMPILE_DISABLE=1
 uv run python open_instruct/grpo_fast.py \
     --dataset_mixer_list ai2-adapt-dev/rlvr_gsm8k_zs 64 \
     --dataset_mixer_list_splits train \
     --dataset_mixer_eval_list ai2-adapt-dev/rlvr_gsm8k_zs 16 \
     --dataset_mixer_eval_list_splits train \
     --max_prompt_token_length 512 \
-    --response_length 512 \
-    --pack_length 1024 \
+    --response_length 2048 \
+    --pack_length 8192 \
     --per_device_train_batch_size 1 \
     --num_unique_prompts_rollout 8 \
     --num_samples_per_prompt_rollout 4 \
-    --model_name_or_path Qwen/Qwen3-0.6B \
-    --stop_strings "</answer>" \
+    --model_name_or_path Qwen/Qwen3.5-0.8B \
     --apply_verifiable_reward true \
-    --temperature 0.7 \
+    --temperature 1.0 \
     --ground_truths_key ground_truth \
-    --chat_template_name r1_simple_chat_postpend_think \
     --learning_rate 3e-7 \
     --total_episodes 200 \
     --deepspeed_stage 2 \
@@ -30,11 +29,8 @@ uv run python open_instruct/grpo_fast.py \
     --local_eval_every 1 \
     --vllm_sync_backend gloo \
     --vllm_gpu_memory_utilization 0.3 \
-    --save_traces \
     --vllm_enforce_eager \
     --gradient_checkpointing \
     --single_gpu_mode \
-    --push_to_hub false \
-    --system_prompt_override_file scripts/train/debug/cute_debug_system_prompt.txt \
-    --active_sampling --async_steps 8
-    # --with_tracking
+    --save_traces \
+    --push_to_hub false
