8c5f5009d324cada315f8d5a123d6f8560632c05
Michael Noukhovitch <michaeln@allenai.org>
2026-03-09 18:56:44 +0000

    deepscaler

---
+++ scripts/train/qwen/qwen3_4b_deepscaler.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+EXP_NAME="qwen3_4b_base_dapo"
+RUN_NAME="${RUN_NAME:-${EXP_NAME}_$(date +%Y%m%d_%H%M%S)}"
+
+MODEL_NAME_OR_PATH="Qwen/Qwen3-4B-Thinking-2507"
+BEAKER_IMAGE="michaeln/open_instruct"
+
+DATASETS="mnoukhov/deepscaler_openinstruct 1.0"
+DATASET_SPLITS="train"
+
+LOCAL_EVALS="mnoukhov/aime_2025_openinstruct 1.0 mnoukhov/brumo_2025_openinstruct 1.0 mnoukhov/hmmt_nov_2025_openinstruct 1.0 mnoukhov/hmmt_feb_2025_openinstruct"
+LOCAL_EVAL_SPLITS="train"
+
+# BEAKER_USER=$(beaker account whoami --format json | jq -r '.[0].name')
+BEAKER_IMAGE="michaeln/open_instruct"
+
+CLUSTER="${CLUSTER:-ai2/saturn ai2/jupiter ai2/neptune}"
+PRIORITY="${PRIORITY:-high}"
+
+uv run mason.py \
+    --task_name ${EXP_NAME} \
+    --cluster ${CLUSTER} \
+    --workspace ai2/oe-adapt-code \
+    --priority ${PRIORITY} \
+    --pure_docker_mode \
+    --image ${BEAKER_IMAGE} \
+    --preemptible \
+    --num_nodes 1 \
+    --env VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
+    --env VLLM_ATTENTION_BACKEND="FLASHINFER" \
+    --gpus 8 \
+    --budget ai2/oe-adapt \
+    -- \
+uv run open_instruct/grpo_fast.py \
+    --run_name "${RUN_NAME}" \
+    --exp_name "${EXP_NAME}" \
+    --eval_pass_at_k 32 \
+    --eval_top_p 0.95 \
+    --vllm_top_p 1.0 \
+    --local_eval_every 100 \
+    --beta 0.0 \
+    --async_steps 2 \
+    --active_sampling \
+    --inflight_updates \
+    --truncated_importance_sampling_ratio_cap 2.0 \
+    --advantage_normalization_type centered \
+    --num_samples_per_prompt_rollout 8 \
+    --num_unique_prompts_rollout 32 \
+    --num_mini_batches 1 \
+    --learning_rate 1e-6 \
+    --per_device_train_batch_size 1 \
+    --dataset_mixer_list $DATASETS \
+    --dataset_mixer_list_splits $DATASET_SPLITS \
+    --dataset_mixer_eval_list $LOCAL_EVALS \
+    --dataset_mixer_eval_list_splits $LOCAL_EVAL_SPLITS \
+    --max_prompt_token_length 2048 \
+    --response_length 16384 \
+    --pack_length 18432 \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --non_stop_penalty False \
+    --temperature 1.0 \
+    --total_episodes 256000 \
+    --deepspeed_stage 2 \
+    --num_learners_per_node 2 \
+    --vllm_num_engines 6 \
+    --vllm_tensor_parallel_size 1 \
+    --lr_scheduler_type constant \
+    --apply_verifiable_reward true \
+    --seed 1 \
+    --local_eval_every 100 \
+    --save_freq 100 \
+    --checkpoint_state_freq 100 \
+    --gradient_checkpointing \
+    --with_tracking \
+    --vllm_enable_prefix_caching \
+    --clip_higher 0.272 \
+    --mask_truncated_completions False \
+    --system_prompt_override_file scripts/train/qwen/math_system_prompt.txt \
+    --load_ref_policy True \
+    --keep_last_n_checkpoints -1 \
+    --push_to_hub False $@
