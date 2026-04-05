70e2ef5e54eb935fe44fed03ea759d4f29b31f38
Jacob Morrison <jacob1morrison@gmail.com>
2026-04-04 17:42:16 -0700

    push eval script

--- submit_eval_jobs.sh
+++ submit_eval_jobs.sh
@@ -1,7 +1,7 @@
 MODEL_PATHS=(
-/weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-4b-sft-100k/step1476-hf
+
 )
-current_evals="alpaca_eval_v3::hamish_zs_reasoning_deepseek,minerva_math_500::hamish_zs_reasoning,ifbench::tulu,livecodebench_codegeneration::tulu-thinker_deepseek_no_think_tags_lite"
+current_evals="alpaca_eval_v3::hamish_zs_reasoning_deepseek,minerva_math_500::hamish_zs_reasoning,ifbench::tulu,livecodebench_codegeneration::tulu-thinker_deepseek_no_think_tags_lite,aime:zs_cot_r1::pass_at_32_2025_deepseek"
 
 for MODEL_PATH in "${MODEL_PATHS[@]}"; do
     BASENAME=$(basename "$MODEL_PATH")
