a90bfcbca74013cc351cae0bc38ee88210c7304a
Andrew Wu <andrew.wu@zettafleet.com>
2026-04-11 00:16:10 +0100

    PR fixes - use open instruct logger not accelerate

--- open_instruct/dpo_tune_cache.py
+++ open_instruct/dpo_tune_cache.py
@@ -71,7 +71,7 @@ from open_instruct.utils import (
     maybe_use_ai2_wandb_entity,
 )
 
-logger = get_logger(__name__)
+logger = logger_utils.setup_logger(__name__)
 
 
 def build_deepspeed_config(
@@ -674,7 +674,8 @@ def main(args: dpo_utils.DPOExperimentConfig, tc: TokenizerConfig):
                     metrics_to_log["perf/tokens_per_second_step"] = step_tokens_per_second
                     metrics_to_log["perf/tokens_per_second_total"] = total_tokens_per_second
 
-                    logger.info(logger_str)
+                    if accelerator.is_main_process:
+                        logger.info(logger_str)
                     if args.with_tracking:
                         accelerator.log(metrics_to_log, step=completed_steps)
                     if accelerator.is_main_process:
