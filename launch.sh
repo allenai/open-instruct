a78fb22321d0a54bd646b1810080e75d3ca8e905
Michael Noukhovitch <michaeln@allenai.org>
2026-05-04 00:01:26 -0400

    launch

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
-for i in 1 2 3; do
+for i in 5; do 
 # i=4
-    export NAME="ngu_64r"
+    export NAME="ngu_comp4_age_4"
 
-# WORKSPACE=ai2/olmo-instruct \
-# PRIORITY=urgent \
 
+    WORKSPACE=ai2/olmo-instruct \
+    PRIORITY=urgent \
     BEAKER_IMAGE=michaeln/open-instruct-integration-test-michaeln-merge \
     EXP="${NAME}_seed${i}" \
     bash scripts/train/qwen/qwen2.5_0.5b_gsm8k_buckets.sh \
@@ -17,7 +17,9 @@ for i in 1 2 3; do
     --max_grad_norm 25 \
     --active_sampling \
     --never_give_up 0.95 \
+    --num_response_tokens_rollout 50000 \
     --maintain_pending_ngu_completions True \
-    --maintain_pending_ngu_counts False
+    --maintain_pending_ngu_counts False \
+    --maintain_pending_ngu_age 4
 
 done
