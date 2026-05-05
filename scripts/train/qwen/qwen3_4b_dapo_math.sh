f6398f4e3c77fa05de46b20245c2c13bbc7b3e48
Finbarr Timbers <finbarrtimbers@gmail.com>
2026-05-05 09:31:57 -0600

    Even more narrow IcePop

--- scripts/train/qwen/qwen3_4b_dapo_math.sh
+++ scripts/train/qwen/qwen3_4b_dapo_math.sh
@@ -38,8 +38,8 @@ uv run open_instruct/grpo_fast.py \
     --inflight_updates \
     --truncated_importance_sampling_ratio_cap 0.0 \
     --use_icepop \
-    --icepop_lower_bound 0.5 \
-    --icepop_upper_bound 2.0 \
+    --icepop_lower_bound 0.75 \
+    --icepop_upper_bound 1.25 \
     --advantage_normalization_type centered \
     --num_samples_per_prompt_rollout 16 \
     --num_unique_prompts_rollout 8 \
