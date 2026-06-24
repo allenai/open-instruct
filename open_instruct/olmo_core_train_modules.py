cd9e7535376239c5e0e8d79fcdc5e6a0133b99bb
Michael Noukhovitch <michaeln@allenai.org>
2026-06-24 17:43:26 -0400

    fix ratio

--- open_instruct/olmo_core_train_modules.py
+++ open_instruct/olmo_core_train_modules.py
@@ -544,8 +544,11 @@ class GRPOTrainModule(TransformerTrainModule):
 
                 advantages = data_BT.advantages[sample_idx]
 
-                log_ratio = new_logprobs - old_logprob
-                ratio = torch.exp(log_ratio)
+                if self.grpo_config.num_epochs == 1 and not self.grpo_config.use_vllm_logprobs:
+                    ratio = torch.ones_like(new_logprobs)
+                else:
+                    log_ratio = new_logprobs - old_logprob
+                    ratio = torch.exp(log_ratio)
 
                 rho = grpo_utils.compute_rho_correction(
                     old_logprob, vllm_logprobs, response_mask, advantages[:, 1:], self.grpo_config
