a0e540fd3f870bbcdf385c79181c3feead1f845b
Kevin Farhat <kevinfarhat@allenai.org>
2026-09-06 18:10:16 -0700

    Use a 15-minute guarantee for 9B math smoke

--- scripts/general_agent/terminal/rl/qwen35_9b_verifier_dppo_math_4node.sh
+++ scripts/general_agent/terminal/rl/qwen35_9b_verifier_dppo_math_4node.sh
@@ -52,7 +52,7 @@ case "$RUN_MODE" in
         SAVE_FREQ=-1
         CHECKPOINT_STATE_FREQ=-1
         MASON_CHECKPOINT_STATE_DIR=""
-        MIN_RUNTIME=30m
+        MIN_RUNTIME=15m
         TIMEOUT=2h
         ;;
     full)
